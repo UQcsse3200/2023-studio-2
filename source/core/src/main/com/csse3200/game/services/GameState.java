39315e954875bf44d21ba361be02ea548c628e6d
Thomas Day <git@tday.au>
2023-08-18 15:45:54 +1000

    Support for multiple callbacks in the state service

--- source/core/src/main/com/csse3200/game/services/GameState.java
+++ source/core/src/main/com/csse3200/game/services/GameState.java
@@ -1,7 +1,10 @@
 package com.csse3200.game.services;
 
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 /**
  * Represents the current state of the game and facilitates state transitions
@@ -15,20 +18,17 @@ public class GameState {
     // Holds the current state data
     private Map<String, Object> stateData = new HashMap<>();
 
-    // Callback listener for state changes
-    private StateChangeListener stateChangeListener;
+    // Callback listeners for state changes
+    private List<StateChangeListener> stateChangeListeners = new CopyOnWriteArrayList<>();
 
     /**
-     * Initializes a new GameState instance with a state change listener.
-     *
-     * @param stateChangeListener Listener to be informed of state changes.
+     * Initializes a new GameState instance.
      */
-    public GameState(StateChangeListener stateChangeListener) {
-        this.stateChangeListener = stateChangeListener;
+    public GameState() {
     }
 
     /**
-     * Sets the new state data and triggers the state change callback.
+     * Sets the new state data and triggers the state change callbacks.
      *
      * @param newData New state data to be set.
      */
@@ -36,17 +36,46 @@ public class GameState {
         synchronized (lock) {
             stateData = new HashMap<>(newData);
         }
-        stateChangeListener.onStateChange(this); // Inform about state change
+        notifyStateChangeListeners(); // Inform about state change
     }
 
     /**
-     * Retrieves a safe copy of the current state data.
+     * Retrieves a safe, immutable copy of the current state data.
      *
-     * @return A copy of the current state data.
+     * @return An immutable copy of the current state data.
      */
     public Map<String, Object> getStateData() {
         synchronized (lock) {
-            return new HashMap<>(stateData);
+            return Collections.unmodifiableMap(new HashMap<>(stateData));
+        }
+    }
+
+    /**
+     * Registers a state change listener.
+     *
+     * @param listener The listener to be registered.
+     */
+    public void registerStateChangeListener(StateChangeListener listener) {
+        stateChangeListeners.add(listener);
+    }
+
+    /**
+     * Unregisters a state change listener.
+     *
+     * @param listener The listener to be unregistered.
+     */
+    public void unregisterStateChangeListener(StateChangeListener listener) {
+        stateChangeListeners.remove(listener);
+    }
+
+    private void notifyStateChangeListeners() {
+        // No need for synchronization here, using CopyOnWriteArrayList
+        Map<String, Object> stateCopy;
+        synchronized (lock) {
+            stateCopy = new HashMap<>(stateData);
+        }
+        for (StateChangeListener listener : stateChangeListeners) {
+            listener.onStateChange(stateCopy);
         }
     }
 
@@ -59,9 +88,8 @@ public class GameState {
         /**
          * Callback method triggered when the state changes.
          *
-         * @param newState The new state after the change.
+         * @param newStateData The new state data after the change.
          */
-        void onStateChange(GameState newState);
+        void onStateChange(Map<String, Object> newStateData);
     }
 }
-

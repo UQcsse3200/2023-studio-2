27897d25450ae64752407f2ac8c208613fd5f394
Kushagra Agrawal <agrawalkushagra647@gmail.com>
2023-08-21 15:45:50 +1000

    Added Multiple Input Detection

--- source/core/src/main/com/csse3200/game/components/player/KeyboardPlayerInputComponent.java
+++ source/core/src/main/com/csse3200/game/components/player/KeyboardPlayerInputComponent.java
@@ -3,6 +3,7 @@ package com.csse3200.game.components.player;
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.scenes.scene2d.actions.DelayAction;
 import com.csse3200.game.input.InputComponent;
 import com.csse3200.game.utils.math.Vector2Utils;
 
@@ -12,6 +13,11 @@ import com.csse3200.game.utils.math.Vector2Utils;
  */
 public class KeyboardPlayerInputComponent extends InputComponent {
   private final Vector2 walkDirection = Vector2.Zero.cpy();
+  private int flagW = 0;
+  private int flagA = 0;
+  private int flagS = 0;
+  private int flagD = 0;
+
 
   public KeyboardPlayerInputComponent() {
     super(5);
@@ -27,23 +33,32 @@ public class KeyboardPlayerInputComponent extends InputComponent {
   public boolean keyDown(int keycode) {
     switch (keycode) {
       case Keys.W:
+        flagW = 1;
         walkDirection.add(Vector2Utils.UP);
         triggerWalkEvent();
         return true;
       case Keys.A:
+        flagA = 1;
         walkDirection.add(Vector2Utils.LEFT);
         triggerWalkEvent();
         return true;
       case Keys.S:
+        flagS = 1;
         walkDirection.add(Vector2Utils.DOWN);
         triggerWalkEvent();
         return true;
       case Keys.D:
+        flagD = 1;
         walkDirection.add(Vector2Utils.RIGHT);
         triggerWalkEvent();
         return true;
       case Keys.SPACE:
         entity.getEvents().trigger("attack");
+        System.out.println("Attack");
+        if (flagW == 1) {
+          System.out.println("Multiple");
+      }
+        
         return true;
       default:
         return false;
@@ -60,18 +75,22 @@ public class KeyboardPlayerInputComponent extends InputComponent {
   public boolean keyUp(int keycode) {
     switch (keycode) {
       case Keys.W:
-        walkDirection.sub(Vector2Utils.UP);
-        triggerWalkEvent();
-        return true;
+          flagW = 0;
+          walkDirection.sub(Vector2Utils.UP);
+          triggerWalkEvent();          
+          return true;
       case Keys.A:
+        flagA = 0;
         walkDirection.sub(Vector2Utils.LEFT);
         triggerWalkEvent();
         return true;
       case Keys.S:
+        flagS = 0;
         walkDirection.sub(Vector2Utils.DOWN);
         triggerWalkEvent();
         return true;
       case Keys.D:
+        flagD = 0;
         walkDirection.sub(Vector2Utils.RIGHT);
         triggerWalkEvent();
         return true;
--- source/core/src/main/com/csse3200/game/utils/math/Vector2Utils.java
+++ source/core/src/main/com/csse3200/game/utils/math/Vector2Utils.java
@@ -10,6 +10,7 @@ public class Vector2Utils {
   public static final Vector2 RIGHT = new Vector2(1f, 0f);
   public static final Vector2 UP = new Vector2(0f, 1f);
   public static final Vector2 DOWN = new Vector2(0f, -1f);
+  public static final Vector2 UP_LEFT = new Vector2(0f, 5f);
 
   public static final Vector2 ONE = new Vector2(1f, 1f);
   public static final Vector2 MAX = new Vector2(Float.MAX_VALUE, Float.MAX_VALUE);

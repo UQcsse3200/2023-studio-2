b006aa6e12b5238fcb18933501d0a1a3655778fc
mcarthur <mcarthur.alford@proton.me>
2023-08-21 22:55:09 +1000

    Implemented ProductionComponent that lets an entity produce/send resources.

---
+++ source/core/src/main/com/csse3200/game/components/resources/ProductionComponent.java
@@ -0,0 +1,34 @@
+package com.csse3200.game.components.resources;
+
+import com.csse3200.game.components.Component;
+import com.csse3200.game.services.GameTime;
+
+public class ProductionComponent extends Component {
+    GameTime timer;
+    long tickRate;
+    int tickSize;
+    long lastTime;
+    Resource produces;
+
+    public ProductionComponent(Resource produces, long tickRate, int tickSize) {
+        this.timer = new GameTime();
+        this.produces = produces;
+        this.tickRate = tickRate;
+        this.tickSize = tickSize;
+        this.lastTime = timer.getTime();
+    }
+
+    @Override
+    public void create() {
+        super.create();
+    }
+
+    @Override
+    public void update() {
+        super.update();
+        while (this.timer.getTimeSince(this.lastTime) >= this.tickRate) {
+            this.getEntity().getEvents().trigger("produceResource", this.produces, this.tickSize);
+            this.lastTime += this.tickRate;
+        }
+    }
+}

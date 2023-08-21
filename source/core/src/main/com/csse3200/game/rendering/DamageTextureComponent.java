b4424cf74fdcbe52a652b65d33be703a0a34df91
Isaac Robinson <isaac@irobinson.org>
2023-08-21 15:53:06 +1000

    Improved documentation for DamageTextureComponent and removed unneccessary checks

--- source/core/src/main/com/csse3200/game/rendering/DamageTextureComponent.java
+++ source/core/src/main/com/csse3200/game/rendering/DamageTextureComponent.java
@@ -13,23 +13,47 @@ import com.csse3200.game.services.ServiceLocator;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * A Rendering component used to switch between different textures based on the current entity's health.
+ */
 public class DamageTextureComponent extends RenderComponent {
     Map<Integer, Texture> thresholds = new HashMap<>();
-    Sprite sprite = new Sprite();
+    Texture texture = null;
 
+    /**
+     * Creates a DamageTextureComponent with the given texture
+     * @param texturePath path to texture to be displayed
+     */
     public DamageTextureComponent(String texturePath) {
         this(GetTexture(texturePath));
     }
 
+    /**
+     * Creates a DamageTextureComponent with the given texture
+     * @param texture Texture object to be displayed
+     */
     public DamageTextureComponent(Texture texture) {
-        sprite.setTexture(texture);
         thresholds.put(Integer.MAX_VALUE, texture);
     }
 
+    /**
+     * Add a texture with a given threshold such that if
+     * health <= threshold then the texture will show.
+     * @param threshold Threshold to show texture at
+     * @param texturePath Path to texture to be displayed
+     * @return current DamageTextureComponent to allow for chaining
+     */
     public DamageTextureComponent addTexture(int threshold, String texturePath) {
         return addTexture(threshold, GetTexture(texturePath));
     }
 
+    /**
+     * Add a texture with a given threshold such that if
+     * health <= threshold then the texture will show.
+     * @param threshold Threshold to show texture at
+     * @param texture Texture to be displayed
+     * @return current DamageTextureComponent to allow for chaining
+     */
     public DamageTextureComponent addTexture(int threshold, Texture texture) {
         thresholds.put(threshold, texture);
         return this;
@@ -43,23 +67,25 @@ public class DamageTextureComponent extends RenderComponent {
     protected void draw(SpriteBatch batch) {
         Vector2 position = entity.getPosition();
         Vector2 scale = entity.getScale();
-        sprite.setTexture(getHealthTexture());
-        batch.draw(sprite.getTexture(), position.x, position.y, scale.x, scale.y);
+        texture = getHealthTexture();
+        batch.draw(texture, position.x, position.y, scale.x, scale.y);
     }
 
+    /**
+     * Calculates the correct texture to display based on the thresholds and current health
+     * @return Texture to be displayed for entity
+     */
     private Texture getHealthTexture() {
         CombatStatsComponent healthStats = entity.getComponent(CombatStatsComponent.class);
         Texture texture = null;
         int prevThreshold = Integer.MAX_VALUE;
+
+        //Will never be empty as constructor initialises with 1 texture with max threshold
         for (Map.Entry<Integer, Texture> entry : thresholds.entrySet()) {
             if (healthStats.getHealth() <= entry.getKey() && entry.getKey() <= prevThreshold) {
                 texture = entry.getValue();
                 prevThreshold = entry.getKey();
             }
-        }
-
-        if (texture == null) {
-            texture = thresholds.values().stream().findFirst().get();
         }
 
         return texture;

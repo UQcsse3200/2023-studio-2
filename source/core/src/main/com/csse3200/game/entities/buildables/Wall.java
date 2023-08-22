13d5f31e32c708c1e36ae67423175035fdb76903
NotRaghavAggarwal <aggarwalraghav29@gmail.com>
2023-08-23 09:42:33 +1000

    Added WallStatDisplay

--- source/core/src/main/com/csse3200/game/entities/buildables/Wall.java
+++ source/core/src/main/com/csse3200/game/entities/buildables/Wall.java
@@ -1,16 +1,16 @@
 package com.csse3200.game.entities.buildables;
 
-import com.badlogic.gdx.math.Vector2;
+//import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.csse3200.game.components.CombatStatsComponent;
 import com.csse3200.game.entities.Entity;
 import com.csse3200.game.entities.configs.WallConfig;
-import com.csse3200.game.input.InputComponent;
+//import com.csse3200.game.input.InputComponent;
 import com.csse3200.game.physics.PhysicsLayer;
 import com.csse3200.game.physics.components.ColliderComponent;
 import com.csse3200.game.physics.components.PhysicsComponent;
 import com.csse3200.game.rendering.TextureRenderComponent;
-import com.csse3200.game.services.ServiceLocator;
+//import com.csse3200.game.services.ServiceLocator;
 
 public class Wall extends Entity {
     public Wall(WallConfig config) {

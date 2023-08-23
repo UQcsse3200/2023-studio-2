983412051c602759ab9c40e497b297acf51c0af3
Rowan Gray <github@rowangray.au>
2023-08-23 22:49:33 +1000

    Added WALL physics layer to seperate it from obstacle.

--- source/core/src/main/com/csse3200/game/physics/PhysicsLayer.java
+++ source/core/src/main/com/csse3200/game/physics/PhysicsLayer.java
@@ -8,6 +8,8 @@ public class PhysicsLayer {
   public static final short OBSTACLE = (1 << 2);
   // NPC (Non-Playable Character) colliders
   public static final short NPC = (1 << 3);
+
+  public static final short WALL = (1 << 4);
   public static final short ALL = ~0;
 
   public static boolean contains(short filterBits, short layer) {

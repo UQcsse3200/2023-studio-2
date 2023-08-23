fb82416e388667a318d9569dfe702ec5e05acb38
kk4w4i <kintarokawai@gmail.com>
2023-08-24 02:48:13 +1000

    Created enemy types and behaviour enums

--- source/core/src/main/com/csse3200/game/entities/configs/EnemyConfig.java
+++ source/core/src/main/com/csse3200/game/entities/configs/EnemyConfig.java
@@ -6,4 +6,5 @@ package com.csse3200.game.entities.configs;
 public class EnemyConfig extends BaseEntityConfig {
   // Load enemy information here
   public int speed = 1;
+  public String atlas = ".atlas";
 }
---
+++ source/core/src/main/com/csse3200/game/entities/enemies/EnemyType.java
@@ -0,0 +1,6 @@
+package com.csse3200.game.entities.enemies;
+
+public enum EnemyType {
+    Melee,
+    Ranged
+}

9c0be40eac9c64cf526bdcb4628432109c8f5809
mcarthur <mcarthur.alford@proton.me>
2023-08-23 12:43:03 +1000

    Pushing resources to the global store works

--- source/core/src/main/com/csse3200/game/components/resources/ProductionComponent.java
+++ source/core/src/main/com/csse3200/game/components/resources/ProductionComponent.java
@@ -1,7 +1,13 @@
 package com.csse3200.game.components.resources;
 
 import com.csse3200.game.components.Component;
+import com.csse3200.game.services.GameState;
 import com.csse3200.game.services.GameTime;
+import com.csse3200.game.services.ServiceLocator;
+
+import java.security.Provider;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ProductionComponent extends Component {
     GameTime timer;
@@ -28,6 +34,9 @@ public class ProductionComponent extends Component {
         super.update();
         while (this.timer.getTimeSince(this.lastTime) >= this.tickRate) {
             this.getEntity().getEvents().trigger("produceResource", this.produces, this.tickSize);
+            Map<String, Object> gameStateData = ServiceLocator.getGameStateService().getStateData();
+            int newAmount = (int) gameStateData.getOrDefault("resource/" + this.produces.toString(), 0.0) + this.tickSize;
+            gameStateData.put("resource/" + this.produces.toString(), newAmount);
             this.lastTime += this.tickRate;
         }
     }

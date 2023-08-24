6a9f4d2bad79eb5f9f76e1f680767e6465c84235
BuildTools <unconfigured@null.spigotmc.org>
2023-08-24 18:34:34 +1000

    "Un-reverts  "Implements dodge movement for player""
    This reverts commit a0c8f0876a3d6b846dc1c4e705fc36d4cc810939.

--- source/core/src/main/com/csse3200/game/utils/math/Vector2Utils.java
+++ source/core/src/main/com/csse3200/game/utils/math/Vector2Utils.java
@@ -10,7 +10,10 @@ public class Vector2Utils {
   public static final Vector2 RIGHT = new Vector2(1f, 0f);
   public static final Vector2 UP = new Vector2(0f, 1f);
   public static final Vector2 DOWN = new Vector2(0f, -1f);
-
+  public static final Vector2 DODGE_UP = new Vector2(0f, 7f);
+  public static final Vector2 DODGE_LEFT = new Vector2(-7f, 0f);
+  public static final Vector2 DODGE_DOWN = new Vector2(0f, -7f);
+  public static final Vector2 DODGE_RIGHT = new Vector2(7f, 0f);
   public static final Vector2 ONE = new Vector2(1f, 1f);
   public static final Vector2 MAX = new Vector2(Float.MAX_VALUE, Float.MAX_VALUE);
   public static final Vector2 MIN = new Vector2(Float.MIN_VALUE, Float.MIN_VALUE);

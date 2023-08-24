27ac1a557038e1cc40245778b89259d62b571051
BuildTools <unconfigured@null.spigotmc.org>
2023-08-24 12:06:16 +1000

    Implements initial powerup component class

---
+++ source/core/src/main/com/csse3200/game/components/PowerupComponent.java
@@ -0,0 +1,20 @@
+package com.csse3200.game.components;
+
+/**
+ * Represents a power-up component within the game. This will be extended
+ * further to specify the type of power-up or its effects.
+ */
+public class PowerupComponent extends Component {
+
+    /**
+     * Constructor for the PowerupComponent.
+     */
+    public PowerupComponent() {
+
+    }
+
+    // public void applyEffect(Player player) {
+    //     System.out.println("Applying effect");
+    // }
+}
+

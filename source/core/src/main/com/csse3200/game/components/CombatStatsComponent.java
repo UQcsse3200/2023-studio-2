530b52923eae4a2661c35735908ed0fed5d2b21c
Damian Bellew <damian.bellew@gmail.com>
2023-08-25 15:00:08 +1000

    Added function documentation to CombatStatsComponent.java

--- source/core/src/main/com/csse3200/game/components/CombatStatsComponent.java
+++ source/core/src/main/com/csse3200/game/components/CombatStatsComponent.java
@@ -88,29 +88,63 @@ public class CombatStatsComponent extends Component {
     }
   }
 
+  /**
+   * Returns the entity's attack multiplier.
+   *
+   * @return attack multiplier
+   */
   public int getAttackMultiplier() {
     return attackMultiplier;
   }
 
+  /**
+   * Sets the entity's attack multiplier. Attack multiplier has a minimum bound of 0.
+   *
+   * @param attackMultiplier attack multiplier
+   */
   public void setAttackMultiplier(int attackMultiplier) {
-    this.attackMultiplier = attackMultiplier;
+    if (attackMultiplier >= 0) {
+      this.attackMultiplier = attackMultiplier;
+    } else {
+      logger.error("Can not set base attack multiplier to a negative attack value");
+    }
   }
 
+  /**
+   * Returns the entity's immunity status.
+   *
+   * @return immunity status
+   */
   public Boolean getImmunity() {
     return isImmune;
   }
 
+  /**
+   * Sets the entity's immunity status.
+   *
+   * @param isImmune immunity status
+   */
   public void setImmunity(boolean isImmune) {
     this.isImmune = isImmune;
   }
 
+  /**
+   * Inverts the entity's current immunity status.
+   */
   public void changeImmunityStatus() {
     this.isImmune = !this.getImmunity();
   }
 
+  /**
+   * Returns the entity's attack damage. This is the base damage scaled by
+   *  the entity's attack multiplier.
+   *
+   * @return attack damage
+   */
   public int getAttack() {
     return getBaseAttack() * getAttackMultiplier();
   }
+
 
   public void hit(CombatStatsComponent attacker) {
     if (getImmunity()) {

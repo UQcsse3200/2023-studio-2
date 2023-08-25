10f23a73a907b7fb940b6f037af6add2847cc5cb
Daksh2607 <dakshjain2607@gmail.com>
2023-08-25 10:14:31 +0530

    companion team commit 1

--- source/core/src/main/com/csse3200/game/input/TouchInputFactory.java
+++ source/core/src/main/com/csse3200/game/input/TouchInputFactory.java
@@ -1,6 +1,7 @@
 package com.csse3200.game.input;
 
 import com.csse3200.game.components.player.TouchPlayerInputComponent;
+import com.csse3200.game.components.Companion.TouchCompanionInputComponent;
 import com.csse3200.game.ui.terminal.TouchTerminalInputComponent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,6 +18,16 @@ public class TouchInputFactory extends InputFactory{
   public InputComponent createForPlayer() {
     logger.debug("Creating player input handler");
     return new TouchPlayerInputComponent();
+  }
+  /**
+   * Creates an input handler for the Companion
+   *
+   * @return Companion input handler
+   */
+  @Override
+  public InputComponent createForCompanion() {
+    logger.debug("Creating Companion input handler");
+    return new TouchCompanionInputComponent();
   }
 
   /**

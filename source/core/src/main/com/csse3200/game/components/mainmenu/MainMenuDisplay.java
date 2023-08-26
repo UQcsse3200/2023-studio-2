500bb1e6e68103318f0cd57c3e7578e8fad31737
Arsh <arsh.upadhyaya123@gmail.com>
2023-08-26 14:43:57 +1000

    Created new screen for minigame, accessible from main menu screen for now as well as a minigamearea

--- source/core/src/main/com/csse3200/game/components/mainmenu/MainMenuDisplay.java
+++ source/core/src/main/com/csse3200/game/components/mainmenu/MainMenuDisplay.java
@@ -38,6 +38,7 @@ public class MainMenuDisplay extends UIComponent {
     TextButton loadBtn = new TextButton("Load", skin);
     TextButton settingsBtn = new TextButton("Settings", skin);
     TextButton exitBtn = new TextButton("Exit", skin);
+    TextButton miniBtn = new TextButton("mini", skin);
 
     // Triggers an event when the button is pressed
     startBtn.addListener(
@@ -76,6 +77,14 @@ public class MainMenuDisplay extends UIComponent {
             entity.getEvents().trigger("exit");
           }
         });
+      miniBtn.addListener(
+              new ChangeListener() {
+                  @Override
+                  public void changed(ChangeEvent changeEvent, Actor actor) {
+                      logger.debug("Settings button clicked");
+                      entity.getEvents().trigger("mini");
+                  }
+              });
 
     table.add(title);
     table.row();
@@ -86,6 +95,8 @@ public class MainMenuDisplay extends UIComponent {
     table.add(settingsBtn).padTop(15f);
     table.row();
     table.add(exitBtn).padTop(15f);
+    table.row();
+    table.add(miniBtn).padTop(15f);
 
     stage.addActor(table);
   }

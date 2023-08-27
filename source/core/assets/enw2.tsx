c16762583e16f4e938791be61b0d32480d50daab
rike568 <140146232+rike568@users.noreply.github.com>
2023-08-27 15:28:41 +1000

    Added all files required for base.tmx file to be opened for use on other user's computer.

---
+++ source/core/assets/enw2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="enw2" tilewidth="16" tileheight="16" tilecount="192" columns="16">
+ <image source="../Dropbox/2023/CSSE3200/Tiles/tmw_desert_spacing.png" width="265" height="199"/>
+</tileset>

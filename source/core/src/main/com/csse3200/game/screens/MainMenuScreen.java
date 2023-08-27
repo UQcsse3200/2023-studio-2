764a7dba87b9959e8906f61c6b3aba6bc323b995
YashMitttal <140570495+YashMitttal@users.noreply.github.com>
2023-08-28 01:58:13 +1000

    Displayed complete story on pressing Start Button and game starts on clicking OK. Also changed in-game sounds

--- source/core/src/main/com/csse3200/game/screens/MainMenuScreen.java
+++ source/core/src/main/com/csse3200/game/screens/MainMenuScreen.java
@@ -24,7 +24,7 @@ import static com.csse3200.game.ui.UIComponent.skin;
  * The game screen containing the main menu.
  */
 public class MainMenuScreen extends ScreenAdapter {
-  private static final Logger logger = LoggerFactory.getLogger(MainMenuScreen.class);
+  public static final Logger logger = LoggerFactory.getLogger(MainMenuScreen.class);
   private final GdxGame game;
   private final Renderer renderer;
   private static final String[] mainMenuTextures = {"images/escape-earth2.png"};
--- source/core/src/main/com/csse3200/game/ui/TitleBox.java
+++ source/core/src/main/com/csse3200/game/ui/TitleBox.java
@@ -1,35 +1,66 @@
 package com.csse3200.game.ui;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.csse3200.game.GdxGame;
+import com.csse3200.game.components.Component;
+import com.csse3200.game.entities.Entity;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static com.csse3200.game.screens.MainMenuScreen.logger;
+
 
 public class TitleBox extends Dialog {
-
-    public TitleBox(String title, Skin skin) {
+    private GdxGame game;
+    public TitleBox( GdxGame game, String title, Skin skin) {
         super(title, skin);
+        this.game = game;
         setMovable(false);
-        setResizable(false);
+        setResizable(true);
 
         // Create a label with your story text
-        String storyText = "Once upon a time...\n\nThis is the story of your adventure.";
+        String storyText = "Earth has become a desolate wasteland ravaged by a deadly virus. \nCivilisation as we know  has crumbled, and humanity's last hope lies among the stars." +
+                " \nYou are one of the few survivors who have managed to secure a spot on a spaceship built with the hopes of finding a cure or a new home on distant planets. " +
+                "\nThe spaceship belongs to Dr Emily Carter, a brilliant scientist determined to find a cure for the virus and make the earth habitable again. " +
+                "\nBut the cosmos is a vast and dangerous place, filled with unknown challenges and mysteries, from alien encounters to unexpected phenomena. " +
+                "\nYour journey begins now as you board the spaceship \"Aurora\" and venture into the unknown.\n";
         text(storyText);
 
         // Create a "Start" button to close the dialog
-        TextButton startButton = new TextButton("Start", skin);
-        button(startButton, true); // The 'true' argument makes this button close the dialog when clicked.
+        TextButton startButton = new TextButton("OK", skin);
+        button(startButton, true);
+        Entity entity = new Entity();
+        entity.getEvents().addListener("ok", this::onOK);
+        startButton.addListener(
+                new ChangeListener() {
+                    @Override
+                    public void changed(ChangeEvent changeEvent, Actor actor) {
+                        logger.debug("OK button clicked");
+                        entity.getEvents().trigger("ok");
+                    }
+                });
+
 
         // Lay out the dialog's components
         getContentTable().row();
         getContentTable().add(startButton).pad(20f).center();
 
         // Size and positioning of the dialog
-        setSize(400f, 300f); // Adjust the size as needed
+        setSize(1920f, 1200f); // Adjust the size as needed
         setPosition((Gdx.graphics.getWidth() - getWidth()) / 2, (Gdx.graphics.getHeight() - getHeight()) / 2);
     }
 
     public void showDialog(Stage stage) {
         stage.addActor(this);
     }
+    private void onOK() {
+        logger.info("Start game");
+        game.setScreen(GdxGame.ScreenType.MAIN_GAME);
+    }
+
 }

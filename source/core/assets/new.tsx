a923fc21348efcbb431ff1c8c30c4625c801f22a
rike568 <140146232+rike568@users.noreply.github.com>
2023-08-27 15:39:48 +1000

    Created map folder in assets and stored all required map files in that folder, for better file management. Removed all unnecessary files.

--- source/core/assets/new.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="new" tilewidth="16" tileheight="16" tilecount="120" columns="8">
- <image source="../Dropbox/2023/CSSE3200/Tiles/TopDownFantasy_Forest_v1/TopDownFantasy-Forest/Tiles/Tileset.png" width="128" height="240"/>
-</tileset>
--- source/core/src/main/com/csse3200/game/areas/terrain/TerrainFactory.java
+++ source/core/src/main/com/csse3200/game/areas/terrain/TerrainFactory.java
@@ -57,7 +57,7 @@ public class TerrainFactory {
   public TerrainComponent createTerrain() {
     ResourceService resourceService = ServiceLocator.getResourceService();
     TmxMapLoader mapLoader = new TmxMapLoader();
-    TiledMap tiledMap = mapLoader.load(Gdx.files.internal("base.tmx").file().getAbsolutePath());
+    TiledMap tiledMap = mapLoader.load(Gdx.files.internal("map/base.tmx").file().getAbsolutePath());
     TiledMapRenderer renderer = createRenderer(tiledMap, 0.5f / 16);
     return new TerrainComponent(camera, tiledMap, renderer, orientation, 0.5f);
   }

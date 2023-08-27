162870659b03465b5e3e385b3fd844434284caef
NotRaghavAggarwal <aggarwalraghav29@gmail.com>
2023-08-27 13:58:21 +1000

    Wall Merge issue

--- source/core/src/main/com/csse3200/game/services/StructurePlacementService.java
+++ source/core/src/main/com/csse3200/game/services/StructurePlacementService.java
@@ -4,8 +4,13 @@ import com.badlogic.gdx.math.GridPoint2;
 import com.csse3200.game.entities.Entity;
 import com.csse3200.game.events.EventHandler;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class StructurePlacementService {
     EventHandler handler;
+    private final Map<GridPoint2, Entity> placedStructures = new HashMap<>();
+
 
     public StructurePlacementService(EventHandler handler) {
         this.handler = handler;
@@ -17,6 +22,11 @@ public class StructurePlacementService {
 
     public void PlaceStructureAt(Entity entity, GridPoint2 tilePos, boolean centerX, boolean centerY) {
         handler.trigger("placeStructureAt", new PlaceStructureAtArgs(entity, tilePos, centerX, centerY));
+        placedStructures.put(tilePos, entity);
+
+    }
+    public Entity getStructureAt(GridPoint2 position) {
+        return placedStructures.get(position);
     }
 
     public static class PlaceStructureAtArgs {

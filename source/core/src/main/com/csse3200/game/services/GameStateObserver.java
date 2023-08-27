c76b7e8cf91ace85bd634b51c3bcede250f82dff
henrybatt <hbatt1545@gmail.com>
2023-08-27 19:02:17 +1000

    GameStateObserver EventHandling added to register events

--- source/core/src/main/com/csse3200/game/services/GameStateObserver.java
+++ source/core/src/main/com/csse3200/game/services/GameStateObserver.java
@@ -1,8 +1,26 @@
 package com.csse3200.game.services;
 
 
+import com.csse3200.game.events.EventHandler;
+
 
 public class GameStateObserver {
+
+    GameState gameState = new GameState();
+    private final EventHandler eventHandler;
+
+    public EventHandler getEvents() {
+        return eventHandler;
+    }
+
+
+    public GameStateObserver() {
+        eventHandler = new EventHandler();
+
+    }
+
+
+
 
 
 }

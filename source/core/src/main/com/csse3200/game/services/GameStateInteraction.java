66ab6fd8d95b747aac7834c8afdaf6d09a284d31
henrybatt <hbatt1545@gmail.com>
2023-08-27 19:38:26 +1000

    Remove GameState from global ServiceLocator. GameState can now only be changed through the GameStateObserver

--- source/core/src/main/com/csse3200/game/services/GameStateInteraction.java
+++ source/core/src/main/com/csse3200/game/services/GameStateInteraction.java
@@ -1,6 +1,30 @@
 package com.csse3200.game.services;
 
+import java.util.Map;
 
 public class GameStateInteraction {
 
+    private GameState gameState = new GameState();
+
+    public void put(String key, Object value) {
+        gameState.put(key, value);
+    }
+
+    public Object get(String key) {
+        return gameState.get(key);
+    }
+//
+    public Map<String, Object> getStateData() {
+        return gameState.getStateData();
+    }
+
+    public void updateResource(String resourceKey, int changeAmount){
+        Object value = gameState.get(resourceKey);
+        int amount = value == null ? 0 : (int) value;
+        gameState.put(resourceKey, amount + changeAmount);
+    }
+
+
 }
+
+

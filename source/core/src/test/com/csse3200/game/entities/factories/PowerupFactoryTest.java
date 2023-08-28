d7a9e07205626b2329be4d2fba1cc38972363a65
connor-golin <c.golin@uqconnect.edu.au>
2023-08-29 02:17:17 +1000

    Implements PowerupFactory final Sprint 1 tests

--- source/core/src/test/com/csse3200/game/entities/factories/PowerupFactoryTest.java
+++ source/core/src/test/com/csse3200/game/entities/factories/PowerupFactoryTest.java
@@ -5,8 +5,12 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Matrix4;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.csse3200.game.areas.ForestGameArea;
+import com.csse3200.game.areas.GameArea;
 import com.csse3200.game.components.PowerupComponent;
 import com.csse3200.game.components.PowerupType;
+import com.csse3200.game.components.gamearea.GameAreaDisplay;
 import com.csse3200.game.entities.Entity;
 import com.csse3200.game.entities.EntityService;
 import com.csse3200.game.extensions.GameExtension;
@@ -15,6 +19,7 @@ import com.csse3200.game.physics.PhysicsLayer;
 import com.csse3200.game.physics.PhysicsService;
 import com.csse3200.game.rendering.DebugRenderer;
 import com.csse3200.game.rendering.RenderService;
+import com.csse3200.game.rendering.TextureRenderComponent;
 import com.csse3200.game.services.ResourceService;
 import com.csse3200.game.services.ServiceLocator;
 import org.junit.jupiter.api.BeforeEach;
@@ -26,31 +31,39 @@ import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
 
 @ExtendWith(MockitoExtension.class)
 @ExtendWith(GameExtension.class)
 public class PowerupFactoryTest {
 
-    @Mock
-    PhysicsService physicsService;
-
-    @Mock
-    ShapeRenderer shapeRenderer;
-    @Mock
-    Box2DDebugRenderer physicsRenderer;
-    DebugRenderer debugRenderer;
     @BeforeEach
     void setUp() {
-        debugRenderer = new DebugRenderer(physicsRenderer, shapeRenderer);
         ServiceLocator.registerEntityService(new EntityService());
         ServiceLocator.registerPhysicsService(new PhysicsService());
-        ServiceLocator.registerInputService(new InputService());
-        ServiceLocator.registerResourceService(new ResourceService());
-        ServiceLocator.registerRenderService(new RenderService());
+        ResourceService resourceService = new ResourceService();
+        ServiceLocator.registerResourceService(resourceService);
+        String[] powerupTextures = {"images/healthpowerup.png", "images/speedpowerup.png"};
+        resourceService.loadTextures(powerupTextures);
+        resourceService.loadAll();
     }
 
     @Test
-    public void testCreateHealthBoostPowerup() {
+    public void testCreateHealthPowerup() {
+        Entity powerup = PowerupFactory.createHealthPowerup();
+        PowerupType type = powerup.getComponent(PowerupComponent.class).getType();
 
+        assertNotNull(powerup);
+        assertEquals(type, PowerupType.HEALTH_BOOST);
+    }
+
+    @Test
+    public void testCreateSpeedPowerup() {
+        Entity powerup = PowerupFactory.createSpeedPowerup();
+        PowerupType type = powerup.getComponent(PowerupComponent.class).getType();
+
+        assertNotNull(powerup);
+        assertEquals(type, PowerupType.SPEED_BOOST);
     }
 }

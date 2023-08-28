1559db2c561b7dc76d7b18f8590d33458d513dfe
Rachel Leathers <rach.leathers@gmail.com>
2023-08-28 21:51:29 +1000

    fixed image error for player skin

--- source/core/src/main/com/csse3200/game/areas/ForestGameArea.java
+++ source/core/src/main/com/csse3200/game/areas/ForestGameArea.java
@@ -42,7 +42,7 @@ public class ForestGameArea extends GameArea {
           "images/elixir_collector.png", //TODO: Replace these images with copyright free images - these are just for testing purposes!!
           "images/broken_elixir_collector.png",
           "images/box_boy_leaf.png",
-          "images/blank.png",
+          "images/player_blank.png",
           "images/tree.png",
           "images/wall.png",
           "images/wall2.png",

26d0af65f98e689cbfc955d2c4aeed9c0dd9ad41
aman-86 <aman.gupta@uqconnect.edu.au>
2023-08-28 13:40:16 +1000

    added .atlas file in ForestGameArea.java

--- source/core/src/main/com/csse3200/game/areas/ForestGameArea.java
+++ source/core/src/main/com/csse3200/game/areas/ForestGameArea.java
@@ -69,7 +69,7 @@ public class ForestGameArea extends GameArea {
           "images/playerSS_6.png",
   };
   private static final String[] forestTextureAtlases = {
-    "images/terrain_iso_grass.atlas", "images/ghost.atlas", "images/ghostKing.atlas"
+    "images/terrain_iso_grass.atlas", "images/ghost.atlas", "images/ghostKing.atlas", "playerSS.atlas"
   };
   private static final String[] forestSounds = {"sounds/Impact4.ogg"};
   private static final String backgroundMusic = "sounds/BGM_03_mp3.mp3";

6d824ca0f53e8018dda8601e6ef618dee6f67362
Rowan Gray <github@rowangray.au>
2023-08-28 23:02:46 +1000

    Update ForestGameArea.java
    Fixed the syntax error.

--- source/core/src/main/com/csse3200/game/areas/ForestGameArea.java
+++ source/core/src/main/com/csse3200/game/areas/ForestGameArea.java
@@ -75,7 +75,7 @@ public class ForestGameArea extends GameArea {
     "images/rangeEnemy.png",
     "images/stone_wall.png",
     "images/healthpowerup.png", // Free to use - https://merchant-shade.itch.io/16x16-mixed-rpg-icons
-    "images/speedpowerup.png" // Free to use - https://merchant-shade.itch.io/16x16-mixed-rpg-icons
+    "images/speedpowerup.png", // Free to use - https://merchant-shade.itch.io/16x16-mixed-rpg-icons
     "images/Ship.png",
     "images/stone_wall.png"
   };

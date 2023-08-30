9d9a7518336bc2b9b33f06c2b447e339b4192c7d
Theodore Kim <tkim0476@gmail.com>
2023-08-30 19:54:56 +1000

    EnemyFactory testing the creation of Melee PTE, Melee Ranged, Melee DTE and Boss Entities.

--- source/core/src/test/com/csse3200/game/entities/factories/EnemyFactoryTest.java
+++ source/core/src/test/com/csse3200/game/entities/factories/EnemyFactoryTest.java
@@ -1,44 +1,137 @@
 package com.csse3200.game.entities.factories;
 
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.csse3200.game.components.CombatStatsComponent;
 import com.csse3200.game.entities.Entity;
 
 import com.csse3200.game.entities.EntityService;
+import com.csse3200.game.entities.enemies.EnemyBehaviour;
+import com.csse3200.game.entities.enemies.EnemyType;
 import com.csse3200.game.extensions.GameExtension;
 import com.csse3200.game.input.InputService;
+import com.csse3200.game.physics.PhysicsLayer;
 import com.csse3200.game.physics.PhysicsService;
+import com.csse3200.game.physics.components.HitboxComponent;
+import com.csse3200.game.rendering.DebugRenderer;
 import com.csse3200.game.rendering.RenderService;
 import com.csse3200.game.services.ResourceService;
 import com.csse3200.game.services.ServiceLocator;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.Mock;
 
-@ExtendWith(MockitoExtension.class)
+import java.util.ArrayList;
+
+import static com.csse3200.game.entities.factories.EnemyFactory.createEnemy;
+
 @ExtendWith(GameExtension.class)
 public class EnemyFactoryTest {
-
+    @Mock
+    ShapeRenderer shapeRenderer;
+    @Mock
+    Box2DDebugRenderer physicsRenderer;
+    DebugRenderer debugRenderer;
     @BeforeEach
     void setUp() {
+        debugRenderer = new DebugRenderer(physicsRenderer, shapeRenderer);
         ServiceLocator.registerEntityService(new EntityService());
         ServiceLocator.registerPhysicsService(new PhysicsService());
-        ResourceService resourceService = new ResourceService();
-        ServiceLocator.registerResourceService(resourceService);
         ServiceLocator.registerInputService(new InputService());
+        ServiceLocator.registerResourceService(new ResourceService());
         ServiceLocator.registerRenderService(new RenderService());
-        String[] textures = {"images/box_boy_leaf.png"};
+    }
+    /**
+     * Melee PTE Enemy distinguished by stat traits
+     * "meleeEnemyPTE":
+     *  - "health": 20,
+     *  - "baseAttack": 10,
+     *  - "speed": 5,
+     *  - "behaviour": "PTE",
+     *  - "atlas": "images/rangeEnemy.atlas"
+     *  Passes Test if created enemy contains the correct health and base attack
+     */
+    @Test
+    void createEnemyPTETest(){
+        ArrayList<Entity> targetList = new ArrayList<>();
+        Entity newPlayer = new Entity().addComponent(new HitboxComponent().setLayer(PhysicsLayer.PLAYER));
+        targetList.add(newPlayer);
+        // List should contain one empty Entity
+        Entity enemy = createEnemy(targetList, EnemyType.Melee, EnemyBehaviour.PTE);
 
-        // These will be the player images once feature/player pushes to main :)
-        // String[] textures = {"images/player_blank.png"};
-        // String[] atlases = {"images/playerSS.atlas"};
-        // resourceService.loadTextureAtlases(atlases);
-        resourceService.loadTextures(textures);
-        resourceService.loadAll();
+        // Checking Health matches PTE Melee Enemy
+        Assertions.assertEquals(enemy.getComponent(CombatStatsComponent.class).getHealth(), 20);
+        // Checking Base Attack matches PTE Melee Enemy
+        Assertions.assertEquals(enemy.getComponent(CombatStatsComponent.class).getBaseAttack(), 10);
     }
 
+    /**
+     * Melee DTE Enemy distinguished by stat traits
+     * "meleeEnemyDTE":
+     *     "health": 50,
+     *     "baseAttack": 5,
+     *     "speed": 5,
+     *     "behaviour": "DTE",
+     *     "atlas": "images/troll_enemy.atlas"
+     * Passes test if correct stats are returned
+     */
     @Test
-    void createEnemyTypeTest(){
-        Entity enemy = new Entity();
-        Entity player = PlayerFactory.createPlayer();
+    void createEnemyDTETest() {
+        ArrayList<Entity> targetList = new ArrayList<>();
+        Entity structure = new Entity().addComponent(new HitboxComponent().setLayer(PhysicsLayer.STRUCTURE));
+        targetList.add(structure);
+        // List should contain one empty Entity
+        Entity enemy = createEnemy(targetList, EnemyType.Melee, EnemyBehaviour.DTE);
+        // Checking Health matches PTE Melee Enemy
+        Assertions.assertEquals(enemy.getComponent(CombatStatsComponent.class).getHealth(), 50);
+        // Checking Base Attack matches PTE Melee Enemy
+        Assertions.assertEquals(enemy.getComponent(CombatStatsComponent.class).getBaseAttack(), 5);
+    }
+
+    /**
+     * Ranged PTE Enemy Test
+     * "rangeEnemyPTE":
+     *     "health": 40,
+     *     "baseAttack": 10,
+     *     "speed": 5,
+     *     "behaviour": "PTE",
+     *     "atlas": "images/base_enemy.atlas"
+     * Passes if successfully creates Entity with proposed traits
+     */
+    @Test
+    void createEnemyRangePTETest() {
+        ArrayList<Entity> targetList = new ArrayList<>();
+        Entity structure = new Entity().addComponent(new HitboxComponent().setLayer(PhysicsLayer.STRUCTURE));
+        targetList.add(structure);
+        // List should contain one empty Entity
+        Entity enemy = createEnemy(targetList, EnemyType.Ranged, EnemyBehaviour.PTE);
+        // Checking Health matches PTE Melee Enemy
+        Assertions.assertEquals(enemy.getComponent(CombatStatsComponent.class).getHealth(), 40);
+        // Checking Base Attack matches PTE Melee Enemy
+        Assertions.assertEquals(enemy.getComponent(CombatStatsComponent.class).getBaseAttack(), 10);
+    }
+    /**
+     * Creates new boss with the following stats:
+     * "meleeBossPTE":
+     *     "health": 100,
+     *     "baseAttack": 25,
+     *     "speed": 8,
+     *     "specialAttack": 40,
+     *     "atlas": "images/boss_enemy.atlas"
+     * Passes if stats created in new entity equal the proposed health and base attack
+     */
+    @Test
+    void createBossEnemyPTETest() {
+        ArrayList<Entity> targetList = new ArrayList<>();
+        Entity structure = new Entity().addComponent(new HitboxComponent().setLayer(PhysicsLayer.PLAYER));
+        targetList.add(structure);
+        // List should contain one empty Entity
+        Entity boss = createEnemy(targetList, EnemyType.BossMelee, EnemyBehaviour.PTE);
+        // Checking Health matches PTE Melee Enemy
+        Assertions.assertEquals(boss.getComponent(CombatStatsComponent.class).getHealth(), 100);
+        // Checking Base Attack matches PTE Melee Enemy
+        Assertions.assertEquals(boss.getComponent(CombatStatsComponent.class).getBaseAttack(), 25);
     }
 }

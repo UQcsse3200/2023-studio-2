01da88223ad189785389a4726c64e5751d5a3929
mhUnt3rS <huntermilo40@gmail.com>
2023-08-31 01:53:32 +1000

    Changed aim and shoot task so bullet is spawned in different area depending on target location

--- source/core/src/main/com/csse3200/game/components/tasks/ShootTask.java
+++ source/core/src/main/com/csse3200/game/components/tasks/ShootTask.java
@@ -34,9 +34,17 @@ public class ShootTask extends DefaultTask {
         super.start();
         this.spawn = owner.getEntity().getPosition();
         hasShot = false;
-        Entity bullet = ProjectileFactory.createEnemyBullet(target.getPosition(), owner.getEntity());
-        ServiceLocator.getStructurePlacementService().SpawnEntityAtVector(bullet, spawn);
+        fireProjectile(target.getPosition());
         hasShot = true;
+    }
+
+    public void fireProjectile(Vector2 position) {
+        Vector2 currentPos = owner.getEntity().getPosition();
+        Entity bullet = ProjectileFactory.createEnemyBullet(target.getPosition(), owner.getEntity());
+        if (currentPos.y < position.y) {
+            spawn.y += 1;
+        }
+        ServiceLocator.getStructurePlacementService().SpawnEntityAtVector(bullet, spawn);
     }
 
     @Override

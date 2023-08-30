c65b30765d9636af7747bb503b2612c500550931
mcarthur <mcarthur.alford@proton.me>
2023-08-30 22:09:44 +1000

    Added documentatiaon to resourcedisplay

--- source/core/src/main/com/csse3200/game/components/resources/ResourceDisplay.java
+++ source/core/src/main/com/csse3200/game/components/resources/ResourceDisplay.java
@@ -14,7 +14,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * A ui component for displaying player stats, e.g. health.
+ * A ui component for displaying resources in the bottom right corner of the screen.
  */
 public class ResourceDisplay extends UIComponent {
     Table table;
@@ -25,6 +25,10 @@ public class ResourceDisplay extends UIComponent {
     int scale = 5;
     int maxResource = 1000; // TODO make this a reference to gamestate and actually work
 
+    /**
+     * Constructor for the ResourceDisplay.
+     * Initializes the table and maps used for storing resource bar data.
+     */
     public ResourceDisplay() {
         this.table = new Table();
         this.resourceBars = new HashMap<>();
@@ -41,15 +45,26 @@ public class ResourceDisplay extends UIComponent {
         addActors();
     }
 
+    /**
+     * Adjusts the width of the resource bar based on a given percentage.
+     *
+     * @param resource The type of resource to adjust.
+     * @param pct The percentage to which the bar should be filled.
+     */
     void setWidth(Resource resource, double pct) {
         TextureRegion region = this.resourceBars.get(resource.toString());
         float originalWidth = this.barWidths.get(resource.toString());
         region.setRegionWidth((int) (pct * originalWidth));
         Image resourceBarImage = this.resourceBarImages.get(resource.toString());
         resourceBarImage.setSize((float) (originalWidth * pct * 5.0), resourceBarImage.getImageHeight());
-//        region.setRegion(0, 0, (int) (pct * originalWidth), region.getRegionHeight());
     }
 
+    /**
+     * Adds a resource type to the display.
+     *
+     * @param resource The type of resource to add.
+     * @return Returns the updated ResourceDisplay object.
+     */
     public ResourceDisplay withResource(Resource resource) {
         Image barForegroundImage= new Image(ServiceLocator.getResourceService().getAsset("images/resourcebar_foreground.png", Texture.class));
         Image barBackgroundImage= new Image(ServiceLocator.getResourceService().getAsset("images/resourcebar_background.png", Texture.class));
@@ -91,6 +106,13 @@ public class ResourceDisplay extends UIComponent {
         stage.addActor(table);
     }
 
+
+    /**
+     * Draws the resource bars on the screen.
+     * The drawing is managed by the stage.
+     *
+     * @param batch The SpriteBatch used for drawing.
+     */
     @Override
     public void draw(SpriteBatch batch)  {
         // draw is handled by the stage

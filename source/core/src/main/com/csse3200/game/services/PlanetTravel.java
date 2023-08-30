1d64a45cb473acf53df0df30daafd641dabe9fde
Vy Vu Khanh Ho <116183364+VuKhanhVyHo@users.noreply.github.com>
2023-08-30 19:20:23 +1000

    Update the correct method and testcase

--- source/core/src/main/com/csse3200/game/services/PlanetTravel.java
+++ source/core/src/main/com/csse3200/game/services/PlanetTravel.java
@@ -1,6 +1,8 @@
 package com.csse3200.game.services;
 import com.csse3200.game.GdxGame;
 import com.csse3200.game.services.GameState;
+import com.csse3200.game.services.ServiceLocator;
+import com.csse3200.game.services.GameStateObserver;
 
     /**
      * Responsible for travel between planets and stored the number of planets remaining
@@ -10,12 +12,14 @@ import com.csse3200.game.services.GameState;
          * The variable to set screen for the minigame
          */
         private final GdxGame game;
-
+        private final GameStateObserver state;
         /**
          * Constructor to set the current game state for using
          */
         public PlanetTravel(GdxGame game) {
             this.game = game; // Set the game object
+            this.state = new GameStateObserver();
+            ServiceLocator.getGameStateObserverService();
         }
 
         /**
@@ -24,12 +28,14 @@ import com.csse3200.game.services.GameState;
          */
         public void moveToNextPlanet(Object planet) {
             game.setScreen(GdxGame.ScreenType.SPACE_MAP);
-            ServiceLocator.getGameStateObserverService().trigger("setCurrentPlanet", planet);
+            state.trigger("setCurrentPlanet", "planet", planet);
         }
 
         /**
          * Return the current planet where player are in
          * @return current planet
          */
-        
+        public Object returnCurrent() {
+            return state.getStateData("planet");
+        }
     }
--- source/core/src/test/com/csse3200/game/services/PlanetTravelTest.java
+++ source/core/src/test/com/csse3200/game/services/PlanetTravelTest.java
@@ -2,38 +2,40 @@ package com.csse3200.game.services;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.math.Plane;
 import com.csse3200.game.GdxGame;
 import com.csse3200.game.input.InputFactory;
 import com.csse3200.game.input.InputService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 public class PlanetTravelTest {
-    private GameState gameState;
-    private PlanetTravel planetTravel;
-    private GdxGame mockGame; // Mock GdxGame
-
+    PlanetTravel planetTravel;
+    GameStateObserver stateObserver;
+    GdxGame mockGame;
+    GameStateInteraction stateInteraction;
     @BeforeEach
     public void setUp() {
         InputFactory inputFactoryMock = mock(InputFactory.class);
         Input inputMock = mock(Input.class);
         Gdx.input = inputMock;
         mockGame = mock(GdxGame.class);
-        InputService inputService = new InputService(inputFactoryMock);
-        gameState = new GameState();
-        planetTravel = new PlanetTravel(gameState, mockGame);  // Modify constructor to accept GdxGame as a parameter
+        stateInteraction = new GameStateInteraction();
+        stateObserver = new GameStateObserver(stateInteraction);
+        planetTravel = new PlanetTravel(mockGame);
     }
 
     @Test
     public void PlanetTravelTest() {
-        gameState.put("planet", "Earth");  // Modifying the same gameState that planetTravel has
+        planetTravel.moveToNextPlanet("Earth");
         assertEquals("Earth", planetTravel.returnCurrent(), "The state data should match the set data.");
         //Skip the minigame because we are responsible for testing it.
-        doNothing().when(mockGame).setScreen(any(GdxGame.ScreenType.class)); // Stub the setScreen method
         planetTravel.moveToNextPlanet("Mars");
-        assertEquals("Mars", gameState.get("planet"), "The state data should match the set data.");
+        assertEquals("Mars", planetTravel.returnCurrent(), "The state data should match the set data.");
     }
 }

1ac9ac32ccbe114d1d293bb95f08f54b57fff0eb
aman-86 <aman.gupta@uqconnect.edu.au>
2023-08-30 22:53:31 +1000

    Implemented Tests of checking whether ArrayList adding Entities in EntityServiceTest

--- source/core/src/test/com/csse3200/game/entities/EntityServiceTest.java
+++ source/core/src/test/com/csse3200/game/entities/EntityServiceTest.java
@@ -1,13 +1,30 @@
 package com.csse3200.game.entities;
 
+import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.fail;
+import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.anyList;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
+import com.badlogic.gdx.utils.Array;
+import com.csse3200.game.components.*;
+import com.csse3200.game.components.gamearea.PerformanceDisplay;
+import com.csse3200.game.components.player.InventoryComponent;
+import com.csse3200.game.components.player.KeyboardPlayerInputComponent;
+import com.csse3200.game.components.player.PlayerActions;
+import com.csse3200.game.components.player.PlayerStatsDisplay;
 import com.csse3200.game.extensions.GameExtension;
+import com.csse3200.game.ui.UIComponent;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+
+import java.awt.event.ComponentAdapter;
+
 
 @ExtendWith(GameExtension.class)
 class EntityServiceTest {
@@ -49,4 +66,55 @@ class EntityServiceTest {
     entityService.dispose();
     verify(entity).dispose();
   }
+
+  /*
+  Creating a test for array list in EntityService Class.
+   */
+  @Test
+  void testGetEntitiesByComponent() {
+    EntityService entityService = new EntityService();
+// Checking whether the below components are in list
+    Assertions.assertEquals(entityService.getEntitiesByComponent(PowerupComponent.class).size, 0);
+    Assertions.assertEquals(entityService.getEntitiesByComponent(InventoryComponent.class).size, 0);
+    Assertions.assertEquals(entityService.getEntitiesByComponent(KeyboardPlayerInputComponent.class).size, 0);
+    Assertions.assertEquals(entityService.getEntitiesByComponent(PlayerActions.class).size, 0);
+    Assertions.assertEquals(entityService.getEntitiesByComponent(PlayerStatsDisplay.class).size, 0);
+    Assertions.assertEquals(entityService.getEntitiesByComponent(CameraComponent.class).size, 0);
+    Assertions.assertEquals(entityService.getEntitiesByComponent(CombatStatsComponent.class).size, 0);
+    Assertions.assertEquals(entityService.getEntitiesByComponent(TouchAttackComponent.class).size, 0);
+    Assertions.assertEquals(entityService.getEntitiesByComponent(PerformanceDisplay.class).size, 0);
+
+    Array<Entity> entity =  entityService.getEntitiesByComponent(CombatStatsComponent.class);
+    assertEquals(0, entity.size);
+
+  }
+// Creates test as in general
+  private Component componentA,componentB;
+  private Entity entityA,entityB;
+  private EntityService entityService;
+
+  //Creating objects for Entity and Component
+
+  @BeforeEach
+  void setup() {
+    entityService = new EntityService();
+    entityA = new Entity();
+    entityB = new Entity();
+    componentA = new Component();
+    componentB = new Component();
+  }
+  @Test
+ public void isTrueGetEntitiesByComponents() {
+
+    entityA.addComponent(componentA);
+    entityB.addComponent(componentB);
+
+    entityService.register(entityA);
+    entityService.register(entityB);
+
+    Array<Entity> test =  entityService.getEntitiesByComponent(componentA.getClass());
+    assertEquals(2, test.size);
+
+  }
+
 }

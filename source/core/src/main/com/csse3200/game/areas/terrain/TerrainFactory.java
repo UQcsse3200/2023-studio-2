705a2a98cbfc7c12873e428bdbd914d0c84098b4
Isaac Robinson <isaac@irobinson.org>
2023-08-31 10:52:25 +1000

    Fix map load path error

--- source/core/src/main/com/csse3200/game/areas/terrain/TerrainFactory.java
+++ source/core/src/main/com/csse3200/game/areas/terrain/TerrainFactory.java
@@ -60,15 +60,16 @@ public class TerrainFactory {
     TiledMap tiledMap = null;
 
     try {
-      
-      tiledMap = mapLoader.load(Gdx.files.internal("map/base.tmx").file().getAbsolutePath());
-    } catch (Exception ex) {
-      
-      // Handle the exception (e.g., print an error message)
-      System.err.println("Error loading TiledMap: " + ex.getMessage());
-    }
-    
+      tiledMap = mapLoader.load(Gdx.files.internal("source/core/assets/map/base.tmx").file().getAbsolutePath());
+    } catch (Exception e) {
 
+      try {
+        tiledMap = mapLoader.load(Gdx.files.internal("map/base.tmx").file().getAbsolutePath());
+      } catch (Exception ex) {
+        // Handle the exception (e.g., print an error message)
+        System.err.println("Error loading TiledMap: " + ex.getMessage());
+      }
+    }
 
     TiledMapRenderer renderer = createRenderer(tiledMap, 0.5f / 16);
 

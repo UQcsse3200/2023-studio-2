d47461bd701533213516abf37a04229d4b1c4069
kk4w4i <kintarokawai@gmail.com>
2023-08-31 12:30:36 +1000

    Changed MelleDTE prio

--- source/core/src/main/com/csse3200/game/entities/factories/EnemyFactory.java
+++ source/core/src/main/com/csse3200/game/entities/factories/EnemyFactory.java
@@ -148,7 +148,7 @@ public class EnemyFactory {
         if (target.getComponent(HitboxComponent.class).getLayer() == PhysicsLayer.STRUCTURE) {
           aiTaskComponent.addTask(new ChaseTask(target, 10, 3f, 4f));
         } else {
-          aiTaskComponent.addTask(new ChaseTask(target, 5, 3f, 4f));
+          aiTaskComponent.addTask(new ChaseTask(target, 0, 3f, 4f));
         }
       }
     }

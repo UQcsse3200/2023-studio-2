69e299c4014b75a9b25c27c92172ba522c6163c0
Jordan <rike568@gmail.com>
2023-08-31 10:48:59 +1000

    Fixed EarthGameAreaTest

--- source/core/src/test/com/csse3200/game/areas/EarthGameAreaTest.java
+++ source/core/src/test/com/csse3200/game/areas/EarthGameAreaTest.java
@@ -1,8 +1,8 @@
 package com.csse3200.game.areas;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-
+import com.badlogic.gdx.maps.MapLayers;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.csse3200.game.areas.terrain.TerrainComponent;
 import com.csse3200.game.areas.terrain.TerrainFactory;
 import com.csse3200.game.entities.Entity;
@@ -12,8 +12,29 @@ import com.csse3200.game.services.ServiceLocator;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
-public class EarthGameAreaTest {
-    
-    void spawnEnvironmentTest(){
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import static org.mockito.Mockito.*;
+@ExtendWith(GameExtension.class)
+class EarthGameAreaTest {
+
+    @Test
+    void spawnEnvironmentTest() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        TerrainFactory terrainFactory = mock(TerrainFactory.class);
+        TiledMapTileLayer tileLayer = mock(TiledMapTileLayer.class);
+        TiledMap tiledMap = mock(TiledMap.class);
+        MapLayers mapLayers = mock(MapLayers.class); // Mock MapLayers
+
+        EarthGameArea earthGameArea = new EarthGameArea(terrainFactory);
+        earthGameArea.terrain = mock(TerrainComponent.class);
+        when(earthGameArea.terrain.getMap()).thenReturn(tiledMap);
+        when(tiledMap.getLayers()).thenReturn(mapLayers); // Return the mock MapLayers
+        when(mapLayers.get("Tree Base")).thenReturn(tileLayer); // Return the mock TileLayer
+
+        EarthGameArea spyEarthGameArea = spy(earthGameArea);
+        Method spawnEnvironmentMethod = EarthGameArea.class.getDeclaredMethod("spawnEnvironment");
+        spawnEnvironmentMethod.setAccessible(true);
+        spawnEnvironmentMethod.invoke(spyEarthGameArea);
     }
 }

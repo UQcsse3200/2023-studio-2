7894350a560e4c949dfb0819606fc72e4843fff7
Rowan Gray <github@rowangray.au>
2023-09-05 21:38:17 +1000

    Created PlaceableEntity class

---
+++ source/core/src/main/com/csse3200/game/entities/PlaceableEntity.java
@@ -0,0 +1,47 @@
+package com.csse3200.game.entities;
+
+import com.csse3200.game.services.GameStateInteraction;
+import com.csse3200.game.services.GameStateObserver;
+import com.csse3200.game.services.ServiceLocator;
+
+import javax.xml.crypto.dsig.keyinfo.KeyValue;
+import java.util.Map;
+
+public class PlaceableEntity extends Entity {
+    private final Map<String, Integer> cost;
+
+    public PlaceableEntity(Map<String, Integer> cost) {
+        super();
+
+        this.cost = cost;
+    }
+
+    /**
+     * This function is called when a PlaceableEntity is placed via the structure
+     * placement service. Override this method to implement specific functionality
+     * when placed.
+     */
+    public void placed() {
+        for (var elementCost : cost.entrySet()) {
+            GameStateObserver stateObserver = ServiceLocator.getGameStateObserverService();
+
+            stateObserver.trigger("resourceAdd", elementCost.getKey(), -elementCost.getValue());
+        }
+    }
+
+    /**
+     * This function is called when a PlaceableEntity is removed via the structure
+     * placement service. Override this method to implement specific functionality
+     * when removed.
+     */
+    public void removed() {
+
+    }
+
+    /**
+     * @return the cost of the entity.
+     */
+    public Map<String, Integer> getCost() {
+        return cost;
+    }
+}

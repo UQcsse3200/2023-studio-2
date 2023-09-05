205136fa5fd8abdf0b70956237ba628b86a1d984
Manvi Narang <65333992+ManviNarang01@users.noreply.github.com>
2023-09-05 22:14:04 +1000

    Update EarthGameArea.java
    Added spawner grid location

--- source/core/src/main/com/csse3200/game/areas/EarthGameArea.java
+++ source/core/src/main/com/csse3200/game/areas/EarthGameArea.java
@@ -50,7 +50,7 @@ public class EarthGameArea extends GameArea {
     private static final int NUM_POWERUPS = 3;
     private static final GridPoint2 PLAYER_SPAWN = new GridPoint2(10, 10);
     private static final GridPoint2 COMPANION_SPAWN = new GridPoint2(8, 8);
-    private static final GridPoint2 SPAWNER_SPAWN = new GridPoint2(30, 2);
+    private static final GridPoint2 SPAWNER_SPAWN = new GridPoint2(0, 50);
     private static final GridPoint2 BOX_SPAWN = new GridPoint2(10, 10);
     private static final GridPoint2 SHIP_SPAWN = new GridPoint2(10, 10);
     private static final float WALL_WIDTH = 0.1f;

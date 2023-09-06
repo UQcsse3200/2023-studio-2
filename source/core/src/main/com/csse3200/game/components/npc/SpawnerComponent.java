1b14d99c9d2fe978840a0ba87b77e9574953dd7a
Manvi Narang <65333992+ManviNarang01@users.noreply.github.com>
2023-09-06 17:09:30 +1000

    Incorporated spawnDelay mechanism to SpawnerComponent.java
    Adversaries now appear in organized waves of 20 seconds gap with each enemy spawning at 1 second interval.

--- source/core/src/main/com/csse3200/game/components/npc/SpawnerComponent.java
+++ source/core/src/main/com/csse3200/game/components/npc/SpawnerComponent.java
@@ -1,8 +1,7 @@
 package com.csse3200.game.components.npc;
+
 import com.badlogic.gdx.math.Vector2;
-import com.csse3200.game.components.CombatStatsComponent;
 import com.csse3200.game.components.Component;
-import com.csse3200.game.components.resources.Resource;
 import com.csse3200.game.entities.Entity;
 import com.csse3200.game.entities.enemies.EnemyBehaviour;
 import com.csse3200.game.entities.enemies.EnemyType;
@@ -13,37 +12,16 @@ import com.csse3200.game.services.ServiceLocator;
 import java.util.ArrayList;
 
 public class SpawnerComponent extends Component {
-    // Timer used to track time since last tick
-    GameTime timer;
+    private GameTime timer;
+    private long lastTime;
+    private ArrayList<Entity> targets;
+    private int currentWave;
+    private long waveDelay = 10000;
+    private boolean isSpawning;
+    private long spawnDelay = 1000; // 1 second delay between enemy spawns
+    private int enemiesToSpawn;
+    private int enemiesSpawned;
 
-    // The desired amount of time (seconds) between each tick
-    long tickRate;
-
-    // The time of the last tick
-    long lastTime;
-
-    // The enemy this spawns
-    ArrayList<Entity> targets;
-
-    EnemyType type;
-
-    EnemyBehaviour behaviour;
-
-    int count;
-
-    int spawnedAmount = 0;
-
-    private int currentWave = 0;
-
-    private long spawnDelay = 0;
-
-    long waveDelay = 10000;
-    /**
-     * SpawnerComponent allows an entity to spawn enemies on some real time interval and send them to
-     * the gameState and event handler.
-     *
-     * @param targets the list of enemy targets
-     */
     public SpawnerComponent(ArrayList<Entity> targets) {
         this.timer = new GameTime();
         this.lastTime = timer.getTime();
@@ -55,45 +33,11 @@ public class SpawnerComponent extends Component {
         super.create();
     }
 
-    /**
-     * Spawns a specified number of melee enemies.
-     *
-     * @param count The number of melee enemies to spawn.
-     */
-    private void spawnMeleeEnemies(int count) {
-        for (int i = 0; i < count; i++) {
-            spawnEnemy(EnemyType.Melee, EnemyBehaviour.PTE);
-        }
+    private void spawnEnemies(int meleeCount, int rangedCount) {
+        isSpawning = true;
+        enemiesToSpawn = meleeCount + rangedCount;
+        enemiesSpawned = 0;
     }
-
-    /**
-     * Spawns a specified number of ranged enemies.
-     *
-     * @param count The number of ranged enemies to spawn.
-     */
-    private void spawnRangedEnemies(int count) {
-        for (int i = 0; i < count; i++) {
-            spawnEnemy(EnemyType.Melee, EnemyBehaviour.DTE);
-        }
-    }
-
-//    private void spawnBoss() {
-//        switch (currentWave) {
-//            case 2:
-//                spawnEnemy(EnemyType.BossMelee);
-//                break;
-//            case 3:
-//                spawnEnemy(EnemyType.BossRanged);
-//                break;
-//        }
-//    }
-    
-    /**
-     * Spawns an enemy with the specified type and behavior.
-     *
-     * @param enemyType  The type of enemy to spawn.
-     * @param behaviour  The behavior of the enemy.
-     */
 
     private void spawnEnemy(EnemyType enemyType, EnemyBehaviour behaviour) {
         Vector2 worldPos = entity.getCenterPosition();
@@ -104,26 +48,35 @@ public class SpawnerComponent extends Component {
     @Override
     public void update() {
         super.update();
-        if (this.timer.getTimeSince(this.lastTime) >= waveDelay) {
-               switch (currentWave) {
+        long currentTime = timer.getTime();
+
+        if (!isSpawning && currentTime - lastTime >= waveDelay) {
+            switch (currentWave) {
                 case 0:
-                    spawnMeleeEnemies(10);
+                    spawnEnemies(10, 0);
                     break;
                 case 1:
-                    spawnMeleeEnemies(10);
-                    spawnRangedEnemies(5);
+                    spawnEnemies(10, 5);
                     break;
                 case 2:
-                    spawnMeleeEnemies(10);
-                    spawnRangedEnemies(10);
+                    spawnEnemies(10, 10);
                     break;
             }
-            
-            currentWave++;
-            this.lastTime += waveDelay;
-            
 
-          
+            currentWave++;
+            lastTime = currentTime;
+        }
+
+        if (isSpawning && enemiesSpawned < enemiesToSpawn && currentTime - lastTime >= spawnDelay) {
+            spawnEnemy(EnemyType.Melee, EnemyBehaviour.PTE);
+            enemiesSpawned++;
+            lastTime = currentTime;
+        }
+
+        if (enemiesSpawned >= enemiesToSpawn) {
+            isSpawning = false;
+            enemiesToSpawn = 0;
+            enemiesSpawned = 0;
         }
     }
 }

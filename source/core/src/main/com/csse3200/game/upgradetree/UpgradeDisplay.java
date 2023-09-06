e94912049c6ce00be8fef8601ba52819443a5574
connor-golin <c.golin@uqconnect.edu.au>
2023-09-07 04:32:17 +1000

    Refactors UpgradeDisplay to implement a Tree data structure, allowing easy add/removals from the Upgrade Tree. Also implements dimming/brightening of assets depending on the unlocked status.

--- source/core/src/main/com/csse3200/game/upgradetree/UpgradeDisplay.java
+++ source/core/src/main/com/csse3200/game/upgradetree/UpgradeDisplay.java
@@ -3,30 +3,26 @@ package com.csse3200.game.upgradetree;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureAtlas;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Group;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
-import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
-import com.badlogic.gdx.utils.Scaling;
-import com.csse3200.game.ExtractorMinigameWindow;
-import com.csse3200.game.components.CombatStatsComponent;
 import com.csse3200.game.components.Weapons.WeaponType;
+import com.csse3200.game.components.resources.Resource;
 import com.csse3200.game.entities.Entity;
 import com.csse3200.game.input.InputOverrideComponent;
-import com.csse3200.game.rendering.TextureRenderComponent;
+import com.csse3200.game.services.GameStateObserver;
 import com.csse3200.game.services.ServiceLocator;
-import com.csse3200.game.ui.UIComponent;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
-import java.util.HashMap;
-
-// todo: refactor and reduce hardcode
 /**
 * A GUI component for the UpgradeTree
 */
@@ -37,12 +33,19 @@ public class UpgradeDisplay extends Window {
     private static final int MATERIAL_LABEL_Y = 650;
     private static final int EXIT_BUTTON_X = 1450;
     private static final int EXIT_BUTTON_Y = 620;
+    private static final float SIZE = 64f;
     private static final String SKIN_PATH = "flat-earth/skin/flat-earth-ui.json";
     private static final String MATERIALS_FORMAT = "Materials: %d";
     private final InputOverrideComponent inputOverrideComponent;
     private final Entity upgradeBench;
-
     private Label materialsLabel;
+
+    // Tree stuff
+    private final List<UpgradeNode> trees = new ArrayList<>();
+    private final ShapeRenderer shapeRenderer = new ShapeRenderer();
+    private static final float VERTICAL_SPACING = 200f;
+    private static final float HORIZONTAL_SPACING = 150f;
+
 
     public static UpgradeDisplay createUpgradeDisplay(Entity upgradeBench) {
         Texture background =
@@ -69,6 +72,114 @@ public class UpgradeDisplay extends Window {
         // Override all normal user input
         inputOverrideComponent = new InputOverrideComponent();
         ServiceLocator.getInputService().register(inputOverrideComponent);
+
+    }
+
+    /**
+     * Stores melee, ranged, and building trees inside a tree data structure :)
+     */
+    private void buildTrees() {
+        // todo: reduce imagePath hardcode
+        // Melee Tree
+        UpgradeNode meleeRoot = new UpgradeNode(WeaponType.STICK, "images/upgradetree/stick.png");
+        UpgradeNode swordNode = new UpgradeNode(WeaponType.KATANA, "images/upgradetree/sword.png");
+        meleeRoot.addChild(swordNode);
+        trees.add(meleeRoot);
+
+        // Ranged Tree
+        UpgradeNode rangedRoot = new UpgradeNode(WeaponType.ELEC_WRENCH, "images/wrench.png");
+        UpgradeNode wrenchNode = new UpgradeNode(WeaponType.THROW_ELEC_WRENCH, "images/speedpowerup.png");
+        UpgradeNode wrenchNode2 = new UpgradeNode(WeaponType.LASERGUN, "images/meteor.png");
+        rangedRoot.addChild(wrenchNode);
+        rangedRoot.addChild(wrenchNode2);
+        wrenchNode2.addChild((new UpgradeNode(WeaponType.SLING_SHOT, "images/meteor.png")));
+        wrenchNode2.addChild((new UpgradeNode(WeaponType.SLING_SHOT, "images/meteor.png")));
+        trees.add(rangedRoot);
+
+        // Build Tree
+        UpgradeNode buildRoot = new UpgradeNode(WeaponType.WOODHAMMER, "images/upgradetree/hammer1.png");
+        UpgradeNode hammer2 = new UpgradeNode(WeaponType.STONEHAMMER, "images/upgradetree/hammer2.png");
+        UpgradeNode hammer3 = new UpgradeNode(WeaponType.LASERGUN, "images/upgradetree/hammer2.png");
+        UpgradeNode x = new UpgradeNode(WeaponType.THROW_ELEC_WRENCH, "images/upgradetree/hammer2.png");
+        buildRoot.addChild(hammer2);
+        buildRoot.addChild(hammer3);
+        hammer2.addChild(x);
+        trees.add(buildRoot);
+    }
+
+    private Group createUpgradeButtons() {
+        Group group = new Group();
+
+        buildTrees();
+
+        for (UpgradeNode treeRoot : trees) {
+            float treeX = (trees.indexOf(treeRoot) + 1) * getWidth() / (trees.size() + 1);
+            float startY = getHeight() - 200;  // start near the top
+            createAndPositionNodes(treeRoot, treeX, startY, group);
+        }
+
+        return group;
+    }
+
+    private void createAndPositionNodes(UpgradeNode node, float x, float y, Group group) {
+
+        // base case
+        if (node == null) {
+            return;
+        }
+
+        node.x = x;
+        node.y = y;
+
+
+        ImageButton button = createWeaponButtons(node, SIZE, x, y);
+        group.addActor(button);
+
+        int childCount = node.children.size();
+        float totalWidth = childCount * HORIZONTAL_SPACING;
+        float currentX = x - totalWidth / 2 + HORIZONTAL_SPACING / 2;
+
+        for (UpgradeNode child : node.children) {
+            float childX = currentX;
+            float childY = y - VERTICAL_SPACING;
+
+            createAndPositionNodes(child, childX, childY, group);
+            currentX += HORIZONTAL_SPACING;
+        }
+    }
+
+    private void drawBoxBackground(UpgradeNode node) {
+        if (node == null) {
+            return;
+        }
+
+        // draws an outline around the weapon texture
+        float dx = 20;
+        float rectSize = SIZE + dx;
+        float offsetX = this.getX() - (dx / 2);
+        float offsetY = this.getY() - (dx / 2);
+
+        shapeRenderer.setColor(Color.GRAY);
+        shapeRenderer.rect(node.x + offsetX, node.y + offsetY, rectSize, rectSize);
+
+        for (UpgradeNode child : node.children) {
+            drawBoxBackground(child);
+        }
+    }
+
+    private void drawLines(UpgradeNode node, Batch batch) {
+        if (node == null || node.children.isEmpty()) {
+            return;
+        }
+
+        Vector2 parentPos = localToStageCoordinates(new Vector2(node.x + SIZE/2, node.y + SIZE/2));
+
+        for (UpgradeNode child : node.children) {
+            Vector2 childPos = localToStageCoordinates(new Vector2(child.x + SIZE/2, child.y + SIZE/2));
+            shapeRenderer.rectLine(parentPos, childPos, 5);
+
+            drawLines(child, batch);
+        }
     }
 
     private void setupWindowDimensions() {
@@ -101,46 +212,27 @@ public class UpgradeDisplay extends Window {
 
     private Label createMaterialsLabel() {
         Skin skin = new Skin(Gdx.files.internal(SKIN_PATH));
-        String materials = String.format(MATERIALS_FORMAT, upgradeBench.getComponent(UpgradeTree.class).getMaterials());
-        this.materialsLabel = new Label(materials, skin);
+        int materials = upgradeBench.getComponent(UpgradeTree.class).getMaterials();
+
+        String str = String.format(MATERIALS_FORMAT, materials);
+        this.materialsLabel = new Label(str, skin);
         materialsLabel.setPosition(MATERIAL_LABEL_X, MATERIAL_LABEL_Y);
         return materialsLabel;
     }
 
-    private Group createUpgradeButtons() {
-        Group group = new Group();
-
-        // sorry
-        ImageButton meleeOne = createImageButton("images/upgradetree/stick.png", 64f, 534, 512, WeaponType.STICK);
-        ImageButton rangedOne = createImageButton("images/wrench.png", 64f, 534 + 204, 512, WeaponType.ELEC_WRENCH);
-        ImageButton meleeTwo = createImageButton("images/upgradetree/sword.png", 64f, 534, 512 - 154, WeaponType.KATANA);
-        ImageButton rangedTwo = createImageButton("images/wrench.png", 64f, 534 + 204, 512 - 154, WeaponType.THROW_ELEC_WRENCH);
-        ImageButton tierThree = createImageButton("images/upgradetree/sword.png", 64f, 534 + 102, 512 - 385, WeaponType.LASERGUN);
-        ImageButton hammerOne = createImageButton("images/upgradetree/hammer1.png", 64f, 534 + 2 * 204, 512, WeaponType.WOODHAMMER);
-        ImageButton hammerTwo = createImageButton("images/upgradetree/hammer2.png", 64f, 534 + 2 * 204, 512 - 154, WeaponType.STONEHAMMER);
-
-        group.addActor(meleeOne);
-        group.addActor(meleeTwo);
-        group.addActor(rangedOne);
-        group.addActor(rangedTwo);
-        group.addActor(tierThree);
-        group.addActor(hammerOne);
-        group.addActor(hammerTwo);
-
-        return group;
-    }
-
-    private ImageButton createImageButton(String imagePath, float size, float posX, float posY, WeaponType weaponType) {
-        TextureRegionDrawable drawable = createTextureRegionDrawable(imagePath, size);
+    // todo: make this less gross
+    private ImageButton createWeaponButtons(UpgradeNode node, float size, float posX, float posY) {
+        TextureRegionDrawable drawable = createTextureRegionDrawable(node.imagePath, size);
         ImageButton button = new ImageButton(drawable);
 
         UpgradeTree stats = upgradeBench.getComponent(UpgradeTree.class);
-        Image lock = null;
 
-        if (!stats.isWeaponUnlocked(weaponType)) {
+        Image lock = null;
+        if (!stats.isWeaponUnlocked(node.weaponType)) {
             lock = new Image(new Texture("images/upgradetree/lock.png"));
             lock.setSize(size, size);
             button.addActor(lock);
+            button.setColor(0.5f,0.5f,0.5f,0.5f);
         }
         button.setPosition(posX, posY);
 
@@ -148,13 +240,14 @@ public class UpgradeDisplay extends Window {
         button.addListener(new ChangeListener() {
             @Override
             public void changed(ChangeEvent event, Actor actor) {
-                int requiredMaterials = 50;
+                int requiredMaterials = 50;  // todo: make each level cost more
 
-                // Try to unlock a new weapon
-                if (!stats.isWeaponUnlocked(weaponType)) {
+                // Try to unlock a new weapon if the node is locked
+                if (!stats.isWeaponUnlocked(node.weaponType)) {
                     if (stats.getMaterials() >= requiredMaterials) {
                         stats.subtractMaterials(requiredMaterials);
-                        stats.unlockWeapon(weaponType);
+                        stats.unlockWeapon(node.weaponType);
+                        button.setColor(1f,1f,1f,1f);
 
                         materialsLabel.setText(String.format("Materials: %d", stats.getMaterials()));
 
@@ -181,5 +274,36 @@ public class UpgradeDisplay extends Window {
         //Stop overriding input when exiting minigame
         ServiceLocator.getInputService().unregister(inputOverrideComponent);
         return super.remove();
+    }
+
+    @Override
+    protected void drawBackground(Batch batch, float parentAlpha, float x, float y) {
+        // Draw order: Window -> Lines -> ImageButtons
+        super.drawBackground(batch, parentAlpha, x, y);
+
+        batch.end();
+
+        // Draws lines connecting parents to children :)
+        shapeRenderer.setProjectionMatrix(batch.getProjectionMatrix());
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+        shapeRenderer.setColor(Color.BLACK);
+        for (UpgradeNode treeRoot : trees) {
+            drawLines(treeRoot, batch);
+        }
+        shapeRenderer.end();
+
+        shapeRenderer.setProjectionMatrix(batch.getProjectionMatrix());
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+        for (UpgradeNode treeRoot : trees) {
+            drawBoxBackground(treeRoot);
+        }
+        shapeRenderer.end();
+
+        batch.begin();
+    }
+
+    @Override
+    public void draw(Batch batch, float parentAlpha) {
+        super.draw(batch, parentAlpha);
     }
 }

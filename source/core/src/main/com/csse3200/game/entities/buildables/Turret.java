715eb97f9de452bad6f44ede7e108739a73f9079
NotRaghavAggarwal <aggarwalraghav29@gmail.com>
2023-09-06 20:26:23 +1000

    Added Turrets, TurretConfig, TurretConfigs,TurretTypes and rudimentary turret images, and placed them on the map

--- source/core/src/main/com/csse3200/game/entities/buildables/Turret.java
+++ source/core/src/main/com/csse3200/game/entities/buildables/Turret.java
@@ -1,2 +1,43 @@
-package com.csse3200.game.entities.buildables;public class Turret {
+package com.csse3200.game.entities.buildables;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.csse3200.game.components.*;
+import com.csse3200.game.components.joinable.JoinLayer;
+import com.csse3200.game.entities.Entity;
+import com.csse3200.game.entities.configs.TurretConfig;
+import com.csse3200.game.entities.configs.TurretConfigs;
+import com.csse3200.game.files.FileLoader;
+import com.csse3200.game.physics.PhysicsLayer;
+import com.csse3200.game.physics.components.ColliderComponent;
+import com.csse3200.game.physics.components.HitboxComponent;
+import com.csse3200.game.physics.components.PhysicsComponent;
+import com.csse3200.game.components.joinable.JoinableComponent;
+import com.csse3200.game.rendering.TextureRenderComponent;
+import com.csse3200.game.services.ServiceLocator;
+
+public class Turret extends Entity {
+
+    private final boolean showHealthBar = true;
+
+    private static final TurretConfigs turretConfigs =
+            FileLoader.readClass(TurretConfigs.class, "configs/turrets.json");
+
+    TurretType type;
+
+    public Turret(TurretType type, Entity player) {
+        super();
+        this.type = type;
+
+        TurretConfig turretConfig = turretConfigs.GetTurretConfig(type);
+        var texture = ServiceLocator.getResourceService().getAsset(turretConfig.texture, Texture.class);
+
+        addComponent(new PhysicsComponent().setBodyType(BodyDef.BodyType.StaticBody));
+        addComponent(new ColliderComponent().setLayer(PhysicsLayer.TURRET));
+        addComponent(new HitboxComponent().setLayer(PhysicsLayer.STRUCTURE));
+        addComponent(new CombatStatsComponent(turretConfig.health, 0, 0, false));
+        addComponent(new HealthBarComponent(true));
+        addComponent(new TextureRenderComponent(texture));
+    }
 }

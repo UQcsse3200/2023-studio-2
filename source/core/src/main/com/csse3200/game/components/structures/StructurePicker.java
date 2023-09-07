12a8e5b9527dd41c535b82308ca19bda47c33d09
Rowan Gray <github@rowangray.au>
2023-09-07 11:08:54 +1000

    Begun implementation of structure picker

---
+++ source/core/src/main/com/csse3200/game/components/structures/StructurePicker.java
@@ -0,0 +1,11 @@
+package com.csse3200.game.components.structures;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.csse3200.game.ui.UIComponent;
+
+public class StructurePicker extends UIComponent {
+    @Override
+    protected void draw(SpriteBatch batch) {
+
+    }
+}

c9977c7ae2bfdcf1418286c1c8d2dbce1b047344
atharvagupta2003 <atharvagupta2003@gmail.com>
2023-09-08 04:33:19 +1000

    added functionality of picking up potions

--- source/core/src/main/com/csse3200/game/entities/factories/PotionFactory.java
+++ source/core/src/main/com/csse3200/game/entities/factories/PotionFactory.java
@@ -3,10 +3,13 @@ package com.csse3200.game.entities.factories;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.csse3200.game.components.InteractableComponent;
+import com.csse3200.game.components.ItemPickupComponent;
 import com.csse3200.game.components.PotionComponent;
 import com.csse3200.game.components.PotionType;
 import com.csse3200.game.entities.Entity;
+import com.csse3200.game.physics.PhysicsLayer;
 import com.csse3200.game.physics.components.ColliderComponent;
+import com.csse3200.game.physics.components.HitboxComponent;
 import com.csse3200.game.physics.components.PhysicsComponent;
 import com.csse3200.game.rendering.TextureRenderComponent;
 
@@ -15,6 +18,8 @@ public class PotionFactory {
     public static Entity createPotion(PotionType type, Entity companionEntity ,Entity laboratoryEntity){
         Entity Potion = new Entity()
                         .addComponent(new PhysicsComponent().setBodyType(BodyDef.BodyType.StaticBody))
+                        .addComponent(new HitboxComponent().setLayer(PhysicsLayer.COMPANION))
+                        .addComponent(new ItemPickupComponent(PhysicsLayer.COMPANION))
                         .addComponent(new PotionComponent(type));
         Potion.setScale(0.4f,0.4f);
         switch (type){

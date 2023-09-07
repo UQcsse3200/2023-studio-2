5a1c81d69e6a6af85bf16cd38ea90f307c6b2591
abhijithvarma03 <abhijithvarmag@gmail.com>
2023-09-07 13:06:00 +1000

    Added a simple button for exiting the interaction popup with the ship and just some text in there. Looking to make the popup look more fancy now

--- source/core/src/main/com/csse3200/game/components/ShipInteractionPopup.java
+++ source/core/src/main/com/csse3200/game/components/ShipInteractionPopup.java
@@ -8,6 +8,7 @@ import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.ui.Window;
@@ -22,14 +23,40 @@ import com.csse3200.game.services.ServiceLocator;
 public class ShipInteractionPopup extends Window {
     private final InputOverrideComponent inputOverrideComponent;
 
+    private Label descriptionLabel;
+
     public ShipInteractionPopup() {
         super("", new Window.WindowStyle(new BitmapFont(), Color.BLACK, getBrownBackgroundStatic()));
 
         Stage stage = ServiceLocator.getRenderService().getStage();
-        setHeight((float) (stage.getHeight()*0.5));
-        setWidth((float) (stage.getWidth()*0.3));
-        setPosition(stage.getWidth()/2 - getWidth()/2 * getScaleX(), stage.getHeight()/2 - getHeight()/2 * getScaleY());
+        float popupWidth = (float) (stage.getWidth() * 0.3);  //setting the width of the popup
+        float popupHeight = (float) (stage.getHeight() * 0.5);  //setting the height of the popup
+        setPosition(stage.getWidth() / 2 - popupWidth / 2, stage.getHeight() / 2 - popupHeight / 2);
+        setSize(popupWidth, popupHeight); //setting the popup size at once using width and height variables
+
         Skin skin = new Skin(Gdx.files.internal("flat-earth/skin/flat-earth-ui.json"));
+        Label.LabelStyle labelStyle = skin.get(Label.LabelStyle.class);
+
+        Label titleLabel = new Label("ship", labelStyle);
+        titleLabel.setColor(Color.RED);
+        add(titleLabel).padTop(20).center().top().expandX();
+        row();
+
+        descriptionLabel = new Label("This is the interaction popup for the ship entity", labelStyle);
+        descriptionLabel.setWidth(popupWidth * 0.9f);  // Using 90% of the popup width
+        descriptionLabel.setWrap(true); //helps the text to stay within the bounds of the popup
+        add(descriptionLabel).width(popupWidth * 0.9f).padTop(20).padLeft(popupWidth * 0.05f).expand().fill();
+
+        TextButton okButton = new TextButton("OK", skin);
+        okButton.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                ShipInteractionPopup.this.remove(); // Remove the popup from the stage
+            }
+        });
+        okButton.setSize(100, 50);
+        okButton.setPosition((getWidth() - okButton.getWidth()) / 2, 20);
+        this.addActor(okButton);
 
         inputOverrideComponent = new InputOverrideComponent();
 

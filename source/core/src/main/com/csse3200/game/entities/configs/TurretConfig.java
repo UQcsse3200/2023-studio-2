f64a14413910fff4a7c348f412875441a8ebb8f3
Khushi Jain <khushijain0302@gmail.com>
2023-09-07 20:45:12 +1000

    implemented maxAmmo and damage config into turrents

--- source/core/src/main/com/csse3200/game/entities/configs/TurretConfig.java
+++ source/core/src/main/com/csse3200/game/entities/configs/TurretConfig.java
@@ -3,5 +3,7 @@ package com.csse3200.game.entities.configs;
 public class TurretConfig {
     public int health = 0;
     public String texture = "images/TurretOne.png";
+    public int maxAmmo = 0;
+    public int damage = 0;
     }
 

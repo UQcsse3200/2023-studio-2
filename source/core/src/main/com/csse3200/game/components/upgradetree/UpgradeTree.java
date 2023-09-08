d47deb19f8ecb73239c3983a93b7e8074c470432
connor-golin <c.golin@uqconnect.edu.au>
2023-09-08 12:32:35 +1000

    Implements Javadocs for UpgradeTree class

--- source/core/src/main/com/csse3200/game/components/upgradetree/UpgradeTree.java
+++ source/core/src/main/com/csse3200/game/components/upgradetree/UpgradeTree.java
@@ -9,11 +9,20 @@ import com.csse3200.game.services.ServiceLocator;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Represents the upgrade tree component for the game, which manages weapon upgrades.
+ */
 public class UpgradeTree extends Component {
 
+    /** List of unlocked weapons for the player. */
     private final List<WeaponType> unlockedWeapons;
-    private int materials = 100;
 
+    /** The starting number of materials available to the player for upgrades. */
+    private int materials = 0;
+
+    /**
+     * Constructs a new UpgradeTree with default weapons unlocked.
+     */
     public UpgradeTree() {
         unlockedWeapons = new ArrayList<WeaponType>();
 
@@ -23,20 +32,38 @@ public class UpgradeTree extends Component {
         unlockedWeapons.add(WeaponType.STICK);
     }
 
+    /**
+     * Returns a list of all unlocked weapons.
+     * @return The list of unlocked weapons.
+     */
     public List<WeaponType> getUnlockedWeapons() {
         return unlockedWeapons;
     }
 
+    /**
+     * Unlocks a specified weapon type, adding it to the list of unlocked weapons if it's not already there.
+     * @param weapon The type of weapon to unlock.
+     */
     public void unlockWeapon(WeaponType weapon) {
         if (!isWeaponUnlocked(weapon)) {
             unlockedWeapons.add(weapon);
         }
     }
 
+    /**
+     * Checks if a given weapon type is already unlocked.
+     * @param weapon The type of weapon to check.
+     * @return True if the weapon is unlocked, false otherwise.
+     */
     public boolean isWeaponUnlocked(WeaponType weapon) {
         return unlockedWeapons.contains(weapon);
     }
 
+    /**
+     * Retrieves the current number of materials available to the player.
+     * It fetches this value from the game's state observer, updating the local materials value.
+     * @return The number of materials.
+     */
     public int getMaterials() {
         GameStateObserver gameStateOb = ServiceLocator.getGameStateObserverService();
         String resourceKey = "resource/" + Resource.Nebulite;
@@ -46,6 +73,11 @@ public class UpgradeTree extends Component {
         return materials;
     }
 
+    /**
+     * Subtracts a given amount from the available materials.
+     * The subtraction only happens if the materials available are more than or equal to the given amount.
+     * @param amount The amount of materials to subtract.
+     */
     public void subtractMaterials(int amount) {
         if (materials >= amount) {
             materials -= amount;

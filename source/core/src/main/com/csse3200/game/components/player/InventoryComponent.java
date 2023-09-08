a0c6c64f685d97a28542b25661e400d492e6a5ca
Kushagra Agrawal <agrawalkushagra647@gmail.com>
2023-09-08 15:43:41 +1000

    Deleted rougue file that broke the branch

--- source/core/src/main/com/csse3200/game/components/player/InventoryComponent.java
+++ source/core/src/main/com/csse3200/game/components/player/InventoryComponent.java
@@ -1,34 +1,72 @@
 package com.csse3200.game.components.player;
 
 import com.csse3200.game.components.Component;
+import com.csse3200.game.components.Weapons.WeaponType;
+
+import java.util.HashMap;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
  * A component intended to be used by the player to track their inventory.
- *
- * Currently only stores the gold amount but can be extended for more advanced
- * functionality such as storing items.
+ * Player can switch between weapons and weapons can be updated
  * Can also be used as a more generic component for other entities.
  */
 public class InventoryComponent extends Component {
   private static final Logger logger = LoggerFactory.getLogger(InventoryComponent.class);
   private int equipped = 1;
+  private HashMap<Integer, WeaponType> equippedWMap = new HashMap<Integer, WeaponType>();
 
+  public void create() {
+    equippedWMap.put(1, WeaponType.ELEC_WRENCH);
+    equippedWMap.put(2, WeaponType.SLING_SHOT);
+    equippedWMap.put(3, WeaponType.WOODHAMMER);
+  }
+
+  public InventoryComponent() {
+    create();
+  }
+
+  /**
+   * @return int - the equipped weapon
+   */
   public int getEquipped() {
     return equipped;
   }
 
+  /**
+   * @param i - the weapon to be equipped
+   */
   public void setEquipped(int i) {
     this.equipped = i;
   }
 
+  /**
+   * Cycles between equiped weapons
+   */
   public void cycleEquipped() {
     int equiped = getEquipped();
     if (equiped == 3) {
       this.equipped = 1;
     } else
       this.equipped++;
+  }
+
+  /**
+   * @return WeaponType - Type of cureently equiped weapon
+   */
+  public WeaponType getEquippedType() {
+    return this.equippedWMap.get(getEquipped());
+  }
+
+  /**
+   * @param weaponType - Type of the new weapon
+   */
+  public void changeEquipped(WeaponType weaponType) {
+    WeaponType equippedType = getEquippedType();
+    this.equippedWMap.remove(equippedType);
+    this.equippedWMap.put(getEquipped(), weaponType);
   }
 
 }

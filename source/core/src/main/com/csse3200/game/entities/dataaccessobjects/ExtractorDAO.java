ab34383194d63c7b734303bc7536e1deb1db55c1
Manvi Narang <65333992+ManviNarang01@users.noreply.github.com>
2023-09-09 17:57:14 +1000

    Created Data Access Object for Extractor Entity ExtractorDAO.java
    Created Data Access Object for Extractor Entity under DataAccessObject. .

---
+++ source/core/src/main/com/csse3200/game/entities/dataaccessobjects/ExtractorDAO.java
@@ -0,0 +1,9 @@
+package com.csse3200.game.entities.dataaccessobjects;
+
+import com.csse3200.game.components.resources.Resource;
+public class ExtractorDAO extends EntityDAO {
+    public Resource resource;
+    public int health;
+    public long tickRate;
+    public int tickSize;
+}

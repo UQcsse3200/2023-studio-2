4f0572b8a144dfae5037f979c0c84d31c0de5835
connor-golin <c.golin@uqconnect.edu.au>
2023-09-10 19:38:23 +1000

    Fixes Upgrade tree node tests

--- source/core/src/test/com/csse3200/game/components/upgradetree/UpgradeNodeTest.java
+++ source/core/src/test/com/csse3200/game/components/upgradetree/UpgradeNodeTest.java
@@ -1,34 +1,59 @@
 package com.csse3200.game.components.upgradetree;
 
+import com.badlogic.gdx.Files;
+import com.badlogic.gdx.Gdx;
 import com.csse3200.game.components.Weapons.WeaponType;
+import com.csse3200.game.entities.configs.WeaponConfig;
+import com.csse3200.game.entities.configs.WeaponConfigs;
+import com.csse3200.game.files.FileLoader;
+import com.csse3200.game.services.ServiceLocator;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class UpgradeNodeTest {
 
     private UpgradeNode node;
+    private WeaponConfigs weaponConfigs;
 
     @BeforeEach
     public void setUp() {
-        node = new UpgradeNode(WeaponType.STICK, "images/upgradetree/stick.png");
+        Gdx.files = mock(Files.class);
+        weaponConfigs = mock(WeaponConfigs.class);
+
+        WeaponConfig stickConfig = mock(WeaponConfig.class);
+        when(weaponConfigs.GetWeaponConfig(WeaponType.STICK)).thenReturn(stickConfig);
+        WeaponConfig woodHammerConfig = mock(WeaponConfig.class);
+        when(weaponConfigs.GetWeaponConfig(WeaponType.WOODHAMMER)).thenReturn(woodHammerConfig);
+        WeaponConfig katanaConfig = mock(WeaponConfig.class);
+        when(weaponConfigs.GetWeaponConfig(WeaponType.KATANA)).thenReturn(katanaConfig);
+        WeaponConfig elecWrenchConfig = mock(WeaponConfig.class);
+        when(weaponConfigs.GetWeaponConfig(WeaponType.ELEC_WRENCH)).thenReturn(elecWrenchConfig);
+
+        node = new UpgradeNode(stickConfig, WeaponType.STICK);
     }
 
     @Test
     public void testAddChild() {
-        UpgradeNode child = new UpgradeNode(WeaponType.WOODHAMMER, "images/upgradetree/hammer1.png");
+        UpgradeNode child = new UpgradeNode(weaponConfigs.GetWeaponConfig(WeaponType.WOODHAMMER), WeaponType.WOODHAMMER);
         node.addChild(child);
         assertTrue(node.getChildren().contains(child));
     }
 
     @Test
     public void testMultipleChildren() {
-        UpgradeNode child1 = new UpgradeNode(WeaponType.STONEHAMMER, "images/upgradetree/hammer2.png");
-        UpgradeNode child2 = new UpgradeNode(WeaponType.ELEC_WRENCH, "images/wrench.png");
-        UpgradeNode child3 = new UpgradeNode(WeaponType.KATANA, "images/upgradetree/katana.png");
+        WeaponConfig woodhammerConfig = weaponConfigs.GetWeaponConfig(WeaponType.WOODHAMMER);
+        WeaponConfig elecWrenchConfig = weaponConfigs.GetWeaponConfig(WeaponType.ELEC_WRENCH);
+        WeaponConfig katanaConfig = weaponConfigs.GetWeaponConfig(WeaponType.KATANA);
+
+        UpgradeNode child1 = new UpgradeNode(woodhammerConfig, WeaponType.STONEHAMMER);
+        UpgradeNode child2 = new UpgradeNode(elecWrenchConfig, WeaponType.ELEC_WRENCH);
+        UpgradeNode child3 = new UpgradeNode(katanaConfig, WeaponType.KATANA);
 
         node.addChild(child1);
         node.addChild(child2);
@@ -57,10 +82,5 @@ public class UpgradeNodeTest {
     @Test
     public void testGetWeaponType() {
         assertEquals(WeaponType.STICK, node.getWeaponType());
-    }
-
-    @Test
-    public void testGetImagePath() {
-        assertEquals("images/upgradetree/stick.png", node.getImagePath());
     }
 }

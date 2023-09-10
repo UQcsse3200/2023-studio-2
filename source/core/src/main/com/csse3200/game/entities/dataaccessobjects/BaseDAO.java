215cf64f6a09d3fc5ac7172f1c2374049c7574ac
Manvi Narang <65333992+ManviNarang01@users.noreply.github.com>
2023-09-10 17:42:49 +1000

    Created DAO for  Base

---
+++ source/core/src/main/com/csse3200/game/entities/dataaccessobjects/BaseDAO.java
@@ -0,0 +1,9 @@
+package com.csse3200.game.entities.dataaccessobjects;
+
+public class BaseDAO extends EntityDAO{
+
+    public int health;
+    public int baseAttack;
+    public int attackMultiplier;
+    public Boolean isImmune;
+}

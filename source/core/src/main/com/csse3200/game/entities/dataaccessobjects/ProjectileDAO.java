d8fd7cd3f1fde82991791512285d4de33bd9a009
Manvi Narang <65333992+ManviNarang01@users.noreply.github.com>
2023-09-10 17:47:13 +1000

    Created Data Access Object for Projectile

---
+++ source/core/src/main/com/csse3200/game/entities/dataaccessobjects/ProjectileDAO.java
@@ -0,0 +1,7 @@
+package com.csse3200.game.entities.dataaccessobjects;
+import com.csse3200.game.entities.configs.EnemyBulletConfig;
+
+public class ProjectileDAO extends EntityDAO {
+    public EnemyBulletConfig bullet;
+
+}

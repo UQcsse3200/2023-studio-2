4a2497ab6e5005e4d33ad30d6b70f42bd4064ae4
Manvi Narang <65333992+ManviNarang01@users.noreply.github.com>
2023-09-10 17:42:08 +1000

    Created DAO for Gate

---
+++ source/core/src/main/com/csse3200/game/entities/dataaccessobjects/GateDAO.java
@@ -0,0 +1,7 @@
+package com.csse3200.game.entities.dataaccessobjects;
+
+public class GateDAO extends EntityDAO {
+    public int health;
+    public String openTextureAtlas;
+    public String closedTextureAtlas;
+}

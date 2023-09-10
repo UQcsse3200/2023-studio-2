7f07e81f485e849096906f1c13d69eb82b8451e7
Manvi Narang <65333992+ManviNarang01@users.noreply.github.com>
2023-09-10 17:41:06 +1000

    Created Data access object for NPC for efficient retrieval.

---
+++ source/core/src/main/com/csse3200/game/entities/dataaccessobjects/NPCDAO.java
@@ -0,0 +1,22 @@
+package com.csse3200.game.entities.dataaccessobjects;
+
+import com.csse3200.game.entities.configs.BotanistConfig;
+import com.csse3200.game.entities.configs.BaseEntityConfig;
+import com.csse3200.game.entities.configs.EnemyConfig;
+import com.csse3200.game.entities.configs.BossConfig;
+
+public class NPCDAO extends EntityDAO {
+  public BaseEntityConfig ghost;
+  public BotanistConfig botanist;
+  
+  public EnemyConfig meleeEnemyPTE;
+  public EnemyConfig meleeEnemyDTE;
+  public EnemyConfig rangeEnemyPTE;
+  public EnemyConfig rangeEnemyDTE;
+  public BossConfig meleeBossPTE;
+  public BossConfig meleeBossDTE;
+  public BossConfig rangeBossPTE;
+  public BossConfig rangeBossDTE;
+
+
+}

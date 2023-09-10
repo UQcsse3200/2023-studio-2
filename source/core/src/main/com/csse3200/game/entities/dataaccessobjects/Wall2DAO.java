0f05a30ce1892c8247df607bee8d93a3eca65426
SamarthDevVaish <sammydv24@gmail.com>
2023-09-10 17:49:00 +1000

    Fixed a bug in the Wall2DAO function.

--- source/core/src/main/com/csse3200/game/entities/dataaccessobjects/Wall2DAO.java
+++ source/core/src/main/com/csse3200/game/entities/dataaccessobjects/Wall2DAO.java
@@ -1,6 +1,6 @@
 package com.csse3200.game.entities.dataaccessobjects;
 
-import com.csse3200.game.entities.WallConfig;
+import com.csse3200.game.entities.configs.WallConfig;
 
 
 public class Wall2DAO extends EntityDAO {

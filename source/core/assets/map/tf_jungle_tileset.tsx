65e425d61f1116d18e4bd098777e55b21a974f0a
Armando Liebenberg <34469720+ArmandoLiebenberg@users.noreply.github.com>
2023-09-10 20:01:28 +1000

    Revert "jungle tree tileset"
    This reverts commit b718f1b438aaf6115b6e9c3acec042fa453940f1.

--- source/core/assets/map/tf_jungle_tileset.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="tf_jungle_tileset" tilewidth="16" tileheight="16" tilecount="462" columns="22">
- <image source="tf_jungle/tf_jungle_tileset.png" width="352" height="336"/>
-</tileset>

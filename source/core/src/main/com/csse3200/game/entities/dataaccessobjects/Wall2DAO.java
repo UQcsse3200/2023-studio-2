ad99080ea3e74eeb24b0d6b4a2fe641bda5833e2
Manvi Narang <65333992+ManviNarang01@users.noreply.github.com>
2023-09-10 17:40:05 +1000

    Created Wall2DAO.java
    Created a Data Access Object (DAO) for the Wall 2 entity for efficient retrieval.

---
+++ source/core/src/main/com/csse3200/game/entities/dataaccessobjects/Wall2DAO.java
@@ -0,0 +1,12 @@
+package com.csse3200.game.entities.dataaccessobjects;
+
+import com.csse3200.game.entities.WallConfig;
+
+
+public class Wall2DAO extends EntityDAO {
+    public WallConfig basic;
+    public WallConfig intermediate;
+    public WallConfig gate;
+
+
+}

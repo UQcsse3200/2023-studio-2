d50c6f2c9768d6921e8b6dd4e46935dec8eb22eb
abhijithvarma03 <abhijithvarmag@gmail.com>
2023-09-12 02:33:23 +1000

    Added the interaction prompt message functionality to all interactable entities

--- source/core/src/main/com/csse3200/game/components/InteractLabel.java
+++ source/core/src/main/com/csse3200/game/components/InteractLabel.java
@@ -18,8 +18,6 @@ public class InteractLabel extends Label {
         Stage stage = ServiceLocator.getRenderService().getStage();
         this.setPosition((stage.getWidth() - this.getWidth()) / 2, 10);
         this.setVisible(false);
-
-        this.addAction(Actions.repeat(RepeatAction.FOREVER, Actions.sequence(Actions.fadeOut(0.5f), Actions.fadeIn(0.5f)))); //this provides a blinking effect to the prompt message
     }
 }
 

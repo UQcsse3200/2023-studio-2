820aa39aef15221c4790798f820a2c89b40bd862
henrybatt <hbatt1545@gmail.com>
2023-09-12 00:11:32 +1000

    Connected PlanetTravel to next Planet GameArea spawning.

--- source/core/src/main/com/csse3200/game/services/PlanetTravel.java
+++ source/core/src/main/com/csse3200/game/services/PlanetTravel.java
@@ -1,5 +1,6 @@
 package com.csse3200.game.services;
 import com.csse3200.game.GdxGame;
+import com.csse3200.game.screens.PlanetScreen;
 import com.csse3200.game.services.GameState;
 import com.csse3200.game.services.ServiceLocator;
 import com.csse3200.game.services.GameStateObserver;
@@ -18,13 +19,26 @@ public class PlanetTravel {
      */
     public PlanetTravel(GdxGame game) {
         this.game = game; // Set the game object
+        ServiceLocator.registerGameStateObserverService(new GameStateObserver()); // TODO: Can be removed once map button is removed from main menu
         this.state = ServiceLocator.getGameStateObserverService();
     }
 
+    /**
+     * Begin transitioning to the new planet from the current one. Displaying all
+     * intermediate gameplay in between
+     *
+     * @param targetPlanetName  The name of the planet to transition to.
+     */
     public void beginPlanetTransition(String targetPlanetName) {
-        game.setScreen(GdxGame.ScreenType.MAIN_GAME);
-        System.out.println(targetPlanetName);
+        // Begin the transition.
 
+        // Play mini-game.
+
+        // End transition period.
+
+        // Finally, go to planet.
+        PlanetScreen currentPlanet = (PlanetScreen) ServiceLocator.getGameStateObserverService().getStateData("currentPlanet");
+        game.setScreen(currentPlanet.getNextPlanet());
     }
 
     /**
@@ -41,6 +55,6 @@ public class PlanetTravel {
      * @return current planet
      */
     public Object returnCurrent() {
-        return state.getStateData("planet");
+        return state.getStateData("currentPlanet");
     }
 }

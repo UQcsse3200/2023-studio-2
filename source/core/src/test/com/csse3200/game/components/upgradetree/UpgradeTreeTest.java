cf520578b20daf38b3185c471db5d3ded3140ae5
connor-golin <c.golin@uqconnect.edu.au>
2023-09-11 15:43:30 +1000

    Completes tests for the upgrade tree using extractor resources.

--- source/core/src/test/com/csse3200/game/components/upgradetree/UpgradeTreeTest.java
+++ source/core/src/test/com/csse3200/game/components/upgradetree/UpgradeTreeTest.java
@@ -1,34 +1,63 @@
 package com.csse3200.game.components.upgradetree;
 
 import com.csse3200.game.components.Weapons.WeaponType;
+import com.csse3200.game.components.resources.Resource;
+import com.csse3200.game.extensions.GameExtension;
+import com.csse3200.game.services.GameStateObserver;
+import com.csse3200.game.services.ServiceLocator;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import java.util.ArrayList;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
+@ExtendWith(MockitoExtension.class)
+@ExtendWith(GameExtension.class)
 public class UpgradeTreeTest {
 
     private UpgradeTree upgradeTree;
+    GameStateObserver gameStateObserver;
+    String resourceKey = "resource/" + Resource.Nebulite;
+    private static final int MATERIALS = 1000;
+    ArrayList<WeaponType> defaultWeapons = new ArrayList<>();
 
     @BeforeEach
     public void setUp() {
         upgradeTree = new UpgradeTree();
+        gameStateObserver = mock(GameStateObserver.class);
+        mock(ServiceLocator.class);
+        ServiceLocator.registerGameStateObserverService(gameStateObserver);
+        defaultWeapons.add(WeaponType.WOODHAMMER);
+        defaultWeapons.add(WeaponType.SLING_SHOT);
+        defaultWeapons.add(WeaponType.STICK);
     }
 
     @Test
-    public void testGetUnlockedWeapons() {
+    public void testGetUnlockedWeaponsSize() {
         assertEquals(3, upgradeTree.getUnlockedWeapons().size());
     }
 
     @Test
-    public void testDefaultWeapons() {
+    public void testIsUnlocked() {
         assertTrue(upgradeTree.isWeaponUnlocked(WeaponType.WOODHAMMER));
+        assertFalse(upgradeTree.isWeaponUnlocked(WeaponType.STEELHAMMER));
+        assertTrue(upgradeTree.isWeaponUnlocked(WeaponType.STICK));;
+        assertFalse(upgradeTree.isWeaponUnlocked(WeaponType.THROW_ELEC_WRENCH));
         assertTrue(upgradeTree.isWeaponUnlocked(WeaponType.SLING_SHOT));
-        assertTrue(upgradeTree.isWeaponUnlocked(WeaponType.STICK));
+    }
+
+    @Test
+    public void testDefaultWeapons() {
+        // ensure the tree contains only the default weapons
+        assertEquals(defaultWeapons, upgradeTree.getUnlockedWeapons());
+        assertEquals(3, upgradeTree.getUnlockedWeapons().size());
     }
 
     @Test
     public void testUnlockWeapon() {
+        // Check a weapon is locked, unlock it, then check its unlocked
         assertFalse(upgradeTree.isWeaponUnlocked(WeaponType.STONEHAMMER));
         upgradeTree.unlockWeapon(WeaponType.STONEHAMMER);
         assertTrue(upgradeTree.isWeaponUnlocked(WeaponType.STONEHAMMER));
@@ -36,9 +65,23 @@ public class UpgradeTreeTest {
 
     @Test
     public void testGetStartingMaterials() {
-        int materials  = upgradeTree.getMaterials();
-        assertEquals(0, materials);
+        // Check it returns the starting materials
+        when(gameStateObserver.getStateData(resourceKey)).thenReturn(MATERIALS);
+        assertEquals(MATERIALS, (int) gameStateObserver.getStateData(resourceKey));
     }
 
-    // todo: write testSubtractMaterials based on getting extractor resources
+    @Test
+    public void testSubtractMaterials() {
+        int subtractionAmount = 50;
+
+        // Setup mock
+        when(gameStateObserver.getStateData(resourceKey)).thenReturn(MATERIALS);
+
+        upgradeTree.subtractMaterials(subtractionAmount);
+
+        // Update mock
+        when(gameStateObserver.getStateData(resourceKey)).thenReturn(MATERIALS - subtractionAmount);
+
+        assertEquals(upgradeTree.getMaterials(), MATERIALS - subtractionAmount);
+    }
 }

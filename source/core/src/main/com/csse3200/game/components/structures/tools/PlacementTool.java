f3327701489ece67715b1093bd70b1edb4409fb9
Rowan Gray <github@rowangray.au>
2023-09-11 16:40:01 +1000

    Fixed issue allowing multiple structures to be placed in the same grid position

--- source/core/src/main/com/csse3200/game/components/structures/tools/PlacementTool.java
+++ source/core/src/main/com/csse3200/game/components/structures/tools/PlacementTool.java
@@ -8,10 +8,20 @@ import com.csse3200.game.services.ServiceLocator;
 public abstract class PlacementTool extends Tool {
     @Override
     public void interact(Entity player, GridPoint2 position) {
-        PlaceableEntity newStructure = createEntity(player);
+        var existingStructure = ServiceLocator.getStructurePlacementService().getStructureAt(position);
 
-        ServiceLocator.getStructurePlacementService().PlaceStructureAt(newStructure, position, false, false);
+        if (existingStructure == null) {
+            PlaceableEntity newStructure = createEntity(player);
+
+            ServiceLocator.getStructurePlacementService().PlaceStructureAt(newStructure, position, false, false);
+        } else {
+            modifyExistingEntity(player, existingStructure);
+        }
     }
 
     public abstract PlaceableEntity createEntity(Entity player);
+
+    public void modifyExistingEntity(Entity player, PlaceableEntity existingStructure) {
+        // default implementation is to do nothing
+    };
 }

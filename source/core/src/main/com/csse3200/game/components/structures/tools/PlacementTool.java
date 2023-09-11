6acca13c6495d8b9393dbf03f0c811314f7af6ea
Rowan Gray <github@rowangray.au>
2023-09-11 16:44:49 +1000

    Implemented isPositionValid method in the PlacementTool class

--- source/core/src/main/com/csse3200/game/components/structures/tools/PlacementTool.java
+++ source/core/src/main/com/csse3200/game/components/structures/tools/PlacementTool.java
@@ -8,20 +8,20 @@ import com.csse3200.game.services.ServiceLocator;
 public abstract class PlacementTool extends Tool {
     @Override
     public void interact(Entity player, GridPoint2 position) {
-        var existingStructure = ServiceLocator.getStructurePlacementService().getStructureAt(position);
-
-        if (existingStructure == null) {
-            PlaceableEntity newStructure = createEntity(player);
-
-            ServiceLocator.getStructurePlacementService().PlaceStructureAt(newStructure, position, false, false);
-        } else {
-            modifyExistingEntity(player, existingStructure);
+        if (!isPositionValid(position)) {
+            return;
         }
+
+        PlaceableEntity newStructure = createEntity(player);
+
+        ServiceLocator.getStructurePlacementService().PlaceStructureAt(newStructure, position, false, false);
     }
 
     public abstract PlaceableEntity createEntity(Entity player);
 
-    public void modifyExistingEntity(Entity player, PlaceableEntity existingStructure) {
-        // default implementation is to do nothing
-    };
+    public boolean isPositionValid(GridPoint2 position) {
+        ServiceLocator.getStructurePlacementService().getStructureAt(position);
+
+        return position != null;
+    }
 }

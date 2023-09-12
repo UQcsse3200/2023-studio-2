3e9c78efdfacaa3d2267e02ed0b0437bfd9a92b8
Vy Vu Khanh Ho <116183364+VuKhanhVyHo@users.noreply.github.com>
2023-09-13 09:58:39 +1000

    Add button for check as requested

--- source/core/src/main/com/csse3200/game/components/mainmenu/MainMenuDisplay.java
+++ source/core/src/main/com/csse3200/game/components/mainmenu/MainMenuDisplay.java
@@ -59,6 +59,7 @@ public class MainMenuDisplay extends UIComponent {
         TextButton miniBtn = new TextButton("Space Minigame", skin);
         TextButton extractorBtn = new TextButton("Extractor Minigame", skin);
         TextButton spaceMapBtn = new TextButton("Space Map", skin);
+        TextButton upgradeShip = new TextButton("Upgrade Ship", skin);
         // Attach listeners to buttons
         startBtn.addListener(
                 new ChangeListener() {
@@ -120,6 +121,14 @@ public class MainMenuDisplay extends UIComponent {
                         entity.getEvents().trigger("exit");
                     }
                 });
+        upgradeShip.addListener(
+                new ChangeListener() {
+                    @Override
+                    public void changed(ChangeEvent changeEvent, Actor actor) {
+                        logger.debug("Upgrade Ship button clicked");
+                        entity.getEvents().trigger("upgrade shop");
+                    }
+                });
 
         // Arrange UI elements in a table layout
         table.add(titleImage);
@@ -136,7 +145,10 @@ public class MainMenuDisplay extends UIComponent {
         table.row();
         table.add(spaceMapBtn).padTop(15f).padLeft(1200f);
         table.row();
+
         table.add(exitBtn).padTop(15f).padLeft(1200f);
+        table.row();
+        table.add(upgradeShip).padTop(15f).padLeft(1200f);
         table.row();
         stage.addActor(titleImage);
         stage.addActor(table);

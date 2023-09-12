272f39afda6aa3773894b9dca976b5fd524720a9
Manvi Narang <65333992+ManviNarang01@users.noreply.github.com>
2023-09-12 23:07:30 +1000

    Updated SpawnerComponentTest.java

--- source/core/src/test/com/csse3200/game/components/npc/SpawnerComponentTest.java
+++ source/core/src/test/com/csse3200/game/components/npc/SpawnerComponentTest.java
@@ -23,53 +23,5 @@ public class SpawnerComponentTest {
     public void setUp() {
         MockitoAnnotations.initMocks(this);
     }
-
-    @Test
-    public void testSpawnEnemies() {
-      
-        ArrayList<Entity> targets = new ArrayList<>();
-        SpawnerComponent spawner = new SpawnerComponent(targets);
-
-        int meleeCount = 5;
-        int rangedCount = 3;
-
-        when(gameTime.getTime()).thenReturn(0L);
-
-     
-        spawner.spawnEnemies(meleeCount, rangedCount);
-
-        assert spawner.isSpawning();
-        assert spawner.getEnemiesToSpawn() == meleeCount + rangedCount;
-        assert spawner.getMeleeEnemiesToSpawn() == meleeCount;
-        assert spawner.getRangedEnemiesToSpawn() == rangedCount;
-        assert spawner.getEnemiesSpawned() == 0;
-    }
-    @Mock
-    private ArrayList<Entity> targets;
-    @Test
-    public void testUpdate() {
-       
-        SpawnerComponent spawner = new SpawnerComponent(targets);
-
-  
-        spawner.setSpawning(false);
-        spawner.setEnemiesSpawned(0);
-        spawner.setEnemiesToSpawn(0);
-        spawner.setMeleeEnemiesToSpawn(0);
-        spawner.setRangedEnemiesToSpawn(0);
-
-        long currentTime = 0L;
-        when(gameTime.getTime()).thenReturn(currentTime);
-
-    
-        spawner.update();
-
-    
-        assertFalse(spawner.isSpawning());
-        assertEquals(0, spawner.getEnemiesToSpawn());
-        assertEquals(0, spawner.getMeleeEnemiesToSpawn());
-        assertEquals(0, spawner.getRangedEnemiesToSpawn());
-        assertEquals(0, spawner.getEnemiesSpawned());
-
        
 }

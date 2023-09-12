944f5d83070ce58354591785ceda44568388867e
maxwell-schroder <s4641402@student.uq.edu.au>
2023-09-12 13:23:16 +1000

    Added docstrings to InteractionControllerComponent to make it clear

--- source/core/src/main/com/csse3200/game/components/player/InteractionControllerComponent.java
+++ source/core/src/main/com/csse3200/game/components/player/InteractionControllerComponent.java
@@ -7,12 +7,20 @@ import com.csse3200.game.components.InteractableComponent;
 import com.csse3200.game.entities.Entity;
 import com.csse3200.game.services.ServiceLocator;
 
+/**
+ * InteractionControllerComponent
+ * It allows entities to interact with each other through the service locator and the interact() method
+ */
 public class InteractionControllerComponent extends Component {
     private boolean effectAll;
     public InteractionControllerComponent(boolean effectAll) {
         this.effectAll = effectAll;
     }
 
+    /**
+     * Interact will use the service locator to interact to enumerate through all entities that
+     * have interact component, and will call interact() with between the two entities.
+     */
     public void interact() {
         Array<Entity> entities = ServiceLocator.getEntityService().getEntitiesByComponent(InteractableComponent.class);
         float minDist = Float.MAX_VALUE;
@@ -29,7 +37,8 @@ public class InteractionControllerComponent extends Component {
                 closest = target;
             }
         }
-
+        //If you are not interacting with ALL entities, and you have a closest entity, make sure you interact with
+        // whatever entity is closest to this entity
         if (!effectAll && closest != null) {
             closest.getComponent(InteractableComponent.class).interact(entity);
         }

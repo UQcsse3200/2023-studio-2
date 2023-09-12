97f8465b66003038d71b91344fe5289ab1a8e23c
Manvi Narang <65333992+ManviNarang01@users.noreply.github.com>
2023-09-12 23:05:24 +1000

    Created spawnEnemies and SpawnUpdate tests for SpawnerComponent

--- source/core/src/test/com/csse3200/game/components/npc/SpawnerComponentTest.java
+++ source/core/src/test/com/csse3200/game/components/npc/SpawnerComponentTest.java
@@ -9,6 +9,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import static org.junit.Assert.*;
 
 import java.util.ArrayList;
 
@@ -23,5 +24,52 @@ public class SpawnerComponentTest {
         MockitoAnnotations.initMocks(this);
     }
 
+    @Test
+    public void testSpawnEnemies() {
+      
+        ArrayList<Entity> targets = new ArrayList<>();
+        SpawnerComponent spawner = new SpawnerComponent(targets);
 
+        int meleeCount = 5;
+        int rangedCount = 3;
+
+        when(gameTime.getTime()).thenReturn(0L);
+
+     
+        spawner.spawnEnemies(meleeCount, rangedCount);
+
+        assert spawner.isSpawning();
+        assert spawner.getEnemiesToSpawn() == meleeCount + rangedCount;
+        assert spawner.getMeleeEnemiesToSpawn() == meleeCount;
+        assert spawner.getRangedEnemiesToSpawn() == rangedCount;
+        assert spawner.getEnemiesSpawned() == 0;
+    }
+    @Mock
+    private ArrayList<Entity> targets;
+    @Test
+    public void testUpdate() {
+       
+        SpawnerComponent spawner = new SpawnerComponent(targets);
+
+  
+        spawner.setSpawning(false);
+        spawner.setEnemiesSpawned(0);
+        spawner.setEnemiesToSpawn(0);
+        spawner.setMeleeEnemiesToSpawn(0);
+        spawner.setRangedEnemiesToSpawn(0);
+
+        long currentTime = 0L;
+        when(gameTime.getTime()).thenReturn(currentTime);
+
+    
+        spawner.update();
+
+    
+        assertFalse(spawner.isSpawning());
+        assertEquals(0, spawner.getEnemiesToSpawn());
+        assertEquals(0, spawner.getMeleeEnemiesToSpawn());
+        assertEquals(0, spawner.getRangedEnemiesToSpawn());
+        assertEquals(0, spawner.getEnemiesSpawned());
+
+       
 }

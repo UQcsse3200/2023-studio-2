1822b5fea4846d3df7d08b15ed807a770bd2cb95
Armando Liebenberg <34469720+ArmandoLiebenberg@users.noreply.github.com>
2023-09-13 19:22:09 +1000

    Fixed Documentation

--- source/core/src/main/com/csse3200/game/entities/Portal.java
+++ source/core/src/main/com/csse3200/game/entities/Portal.java
@@ -1,22 +1,8 @@
 package com.csse3200.game.entities;
 import com.badlogic.gdx.physics.box2d.BodyDef;
-import com.csse3200.game.components.CombatStatsComponent;
-import com.csse3200.game.components.HealthBarComponent;
 import com.csse3200.game.components.ProximityActivationComponent;
-import com.csse3200.game.components.joinable.JoinLayer;
-import com.csse3200.game.components.joinable.JoinableComponent;
-import com.csse3200.game.components.joinable.JoinableComponentShapes;
-import com.csse3200.game.entities.Entity;
-import com.csse3200.game.entities.configs.GateConfig;
-import com.csse3200.game.entities.configs.WallConfig;
-import com.csse3200.game.entities.configs.WallConfigs;
-import com.csse3200.game.files.FileLoader;
-import com.csse3200.game.physics.PhysicsLayer;
-import com.csse3200.game.physics.components.ColliderComponent;
 import com.csse3200.game.physics.components.PhysicsComponent;
-import com.csse3200.game.rendering.AtlasRenderComponent;
 import com.csse3200.game.rendering.TextureRenderComponent;
-import com.csse3200.game.services.ServiceLocator;
 
 public class Portal extends Entity {
     public Portal(Entity player) {
@@ -28,9 +14,9 @@ public class Portal extends Entity {
     }
 
     /**
-     * Changes the texture to resemble a closed gate and enables the collision.
+     * Teleports the player to a fixed destination
      *
-     * @param player - the player who opened the gate
+     * @param player - the player entering the portal
      */
     public void teleport(Entity player) {
         player.setPosition(5, 5);
--- source/core/src/main/com/csse3200/game/entities/factories/PortalFactory.java
+++ source/core/src/main/com/csse3200/game/entities/factories/PortalFactory.java
@@ -1,17 +1,17 @@
 package com.csse3200.game.entities.factories;
-
-import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.csse3200.game.entities.Entity;
-import com.csse3200.game.physics.PhysicsLayer;
-import com.csse3200.game.physics.components.ColliderComponent;
-import com.csse3200.game.physics.components.PhysicsComponent;
-import com.csse3200.game.rendering.TextureRenderComponent;
 import com.csse3200.game.entities.Portal;
 
 public class PortalFactory {
 
+    /**
+     * Teleports the player to a fixed destination
+     *
+     * @param width - the width of the portal object
+     * @param height - the height of the portal object
+     * @param player - the player instantiated in the current game area
+     */
     public static Entity createPortal(float width, float height, Entity player) {
-
         Entity portal = new Portal(player);
         portal.setScale(width, height);
         return portal;

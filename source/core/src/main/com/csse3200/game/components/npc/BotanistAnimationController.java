30e252105648b6a7aac9ddc476b70361239abc8f
DRG31 <dev.gupta@uqconnect.edu.au>
2023-09-14 08:58:24 +1000

    Modified BotanistAnimationController, removed irrelevant lines of code

--- source/core/src/main/com/csse3200/game/components/npc/BotanistAnimationController.java
+++ source/core/src/main/com/csse3200/game/components/npc/BotanistAnimationController.java
@@ -12,11 +12,6 @@ public class BotanistAnimationController extends Component {
     private AnimationRenderComponent animator;
     private TextureAtlas atlas;
 
-    private Animation<TextureAtlas.AtlasRegion> leftAnimation;
-    private Animation<TextureAtlas.AtlasRegion> rightAnimation;
-    private Animation<TextureAtlas.AtlasRegion> bottomAnimation;
-    private Animation<TextureAtlas.AtlasRegion> upAnimation;
-
     private float animationTimer = 0f;
     private float animationDuration = 10f; // Adjust this to control animation speed.
 
@@ -33,25 +28,6 @@ public class BotanistAnimationController extends Component {
         assetManager.load("images/botanist.atlas", TextureAtlas.class);
         assetManager.finishLoading();
         atlas = assetManager.get("images/botanist.atlas");
-
-        TextureAtlas.AtlasRegion left1 = atlas.findRegion("row-1-column-1");
-        TextureAtlas.AtlasRegion  left2 = atlas.findRegion("row-1-column-2");
-
-        TextureAtlas.AtlasRegion right1 = atlas.findRegion("row-1-column-3");
-        TextureAtlas.AtlasRegion right2 = atlas.findRegion("row-1-column-4");
-
-        TextureAtlas.AtlasRegion bottom1 = atlas.findRegion("row-1-column-5");
-        TextureAtlas.AtlasRegion bottom2 = atlas.findRegion("row-1-column-6");
-
-        TextureAtlas.AtlasRegion top1 = atlas.findRegion("row-1-column-7");
-        TextureAtlas.AtlasRegion top2 = atlas.findRegion("row-1-column-8");
-
-
-        // Initialize the animation sequences.
-        leftAnimation = new Animation<TextureAtlas.AtlasRegion>(1f , left1,left2);
-        rightAnimation = new Animation<TextureAtlas.AtlasRegion>(1f , right1, right2);
-        bottomAnimation = new Animation<TextureAtlas.AtlasRegion>(1f , bottom1,bottom2);
-        upAnimation = new Animation<TextureAtlas.AtlasRegion>(1f ,top1,top2 );
 
         // Start with the default animation.
         animator.startAnimation("row-1-column-1");

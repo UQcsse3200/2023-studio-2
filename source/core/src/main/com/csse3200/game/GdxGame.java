742064cde52a90eb4a285735327844a472924ae1
Rachit Chaurasia <rachitchaurasia2701@gmail.com>
2023-09-15 08:32:53 +1000

    Added ControlsScreen feature

--- source/core/src/main/com/csse3200/game/GdxGame.java
+++ source/core/src/main/com/csse3200/game/GdxGame.java
@@ -103,13 +103,15 @@ public class GdxGame extends Game {
         return new SpaceNavigationScreen(this);
       case UPGRADE_SHOP:
         return new UpgradeShopScreen(this);
+      case CONTROL_SCREEN:
+        return new ControlsScreen(this);
       default:
         return null;
     }
   }
 
   public enum ScreenType {
-    MAIN_MENU, MAIN_GAME, SETTINGS, TITLE_SCREEN, SPACE_MAP, EXTRACTOR_GAME,  GAME_STORY, PLAYER_DEATH,COMPANION_DEATH, NAVIGATION_SCREEN ,INITIAL_SCREEN, SPACEMINI_SCREEN, UPGRADE_SHOP
+    MAIN_MENU, MAIN_GAME, SETTINGS, TITLE_SCREEN, SPACE_MAP, CONTROL_SCREEN, EXTRACTOR_GAME,  GAME_STORY, PLAYER_DEATH,COMPANION_DEATH, NAVIGATION_SCREEN ,INITIAL_SCREEN, SPACEMINI_SCREEN, UPGRADE_SHOP
 
   }
   /**

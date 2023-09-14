2643d11556984f4e1232a0d2b75a7da43079906d
Rachel Leathers <rach.leathers@gmail.com>
2023-09-14 12:07:26 +1000

    fixed player movement after death

--- source/core/src/main/com/csse3200/game/components/player/KeyboardPlayerInputComponent.java
+++ source/core/src/main/com/csse3200/game/components/player/KeyboardPlayerInputComponent.java
@@ -51,7 +51,12 @@ public class KeyboardPlayerInputComponent extends InputComponent {
     }
     return false;
   }
-
+    public void playerDead() {
+        if (entity.getComponent(CombatStatsComponent.class).isDead()) {
+            keyFlags.clear();
+            triggerWalkEvent();
+        }
+    }
   /**
    *
    */

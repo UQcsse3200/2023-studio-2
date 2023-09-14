2046ad930eece91f59303a17c5b75740aff38ca8
Rachit Chaurasia <rachitchaurasia2701@gmail.com>
2023-09-14 14:34:05 +1000

    changed kenneyui-base to kenney-rpg-expansion with Jura font

--- source/core/src/main/com/csse3200/game/components/player/PlayerStatsDisplay.java
+++ source/core/src/main/com/csse3200/game/components/player/PlayerStatsDisplay.java
@@ -45,7 +45,7 @@ public class PlayerStatsDisplay extends UIComponent {
     // Health text
     int health = entity.getComponent(CombatStatsComponent.class).getHealth();
     CharSequence healthText = String.format("Health: %d", health);
-    healthLabel = new Label(healthText, skin, "large");
+    healthLabel = new Label(healthText, skin, "small");
 
     table.add(heartImage).size(heartSideLength).pad(5);
     table.add(healthLabel);

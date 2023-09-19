d28b46b8b5d6d1f83f992a671348e8d76983c418
atharvagupta2003 <atharvagupta2003@gmail.com>
2023-09-20 00:50:08 +1000

    minor changes

--- source/core/src/test/com/csse3200/game/components/Companion/CompanionAnimationControllerTest.java
+++ source/core/src/test/com/csse3200/game/components/Companion/CompanionAnimationControllerTest.java
@@ -26,49 +26,49 @@ public class CompanionAnimationControllerTest {
     @Test
     public void testAnimateLeft() {
         controller.animateLeft();
-        verify(animator).startAnimation("LEFT");
+        verify(animator).startAnimation("Companion_Left");
     }
 
     @Test
     public void testAnimateRight() {
         controller.animateRight();
-        verify(animator).startAnimation("RIGHT");
+        verify(animator).startAnimation("Companion_Right");
     }
 
     @Test
     public void testAnimateUp() {
         controller.animateUp();
-        verify(animator).startAnimation("UP");
+        verify(animator).startAnimation("Companion_Up");
     }
 
     @Test
     public void testAnimateDown() {
         controller.animateDown();
-        verify(animator).startAnimation("DOWN");
+        verify(animator).startAnimation("Companion_Down");
     }
 
     @Test
     public void testAnimateUpLeft() {
         controller.animateUpLeft();
-        verify(animator).startAnimation("UP_LEFT");
+        verify(animator).startAnimation("Companion_UpLeft");
     }
 
     @Test
     public void testAnimateUpRight() {
         controller.animateUpRight();
-        verify(animator).startAnimation("UP_RIGHT");
+        verify(animator).startAnimation("Companion_UpRight");
     }
 
     @Test
     public void testAnimateDownLeft() {
         controller.animateDownLeft();
-        verify(animator).startAnimation("DOWN_LEFT");
+        verify(animator).startAnimation("Companion_DownLeft");
     }
 
     @Test
     public void testAnimateDownRight() {
         controller.animateDownRight();
-        verify(animator).startAnimation("DOWN_RIGHT");
+        verify(animator).startAnimation("Companion_DownRight");
     }
 
 }

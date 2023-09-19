6cef444706ff6ce645bc2295808fa1f33821c908
aman-86 <aman.gupta@uqconnect.edu.au>
2023-09-19 21:25:47 +1000

    added the audio file of player walking. Implemented the playerWalkSound.java. This class is responsible for managing the walking sound effects. It provides the method such as start, stop and dispose of the sound.

--- source/core/src/main/com/csse3200/game/components/player/playerWalkSound.java
+++ source/core/src/main/com/csse3200/game/components/player/playerWalkSound.java
@@ -3,11 +3,51 @@ package com.csse3200.game.components.player;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.audio.Sound;
 
-public class playerWalkSound {
-    private Sound walkingSound;
-    private boolean isWalking;
+/**
+ * This class is responsible for managing the walking sound effects.
+ * It provides the method such as start, stop and dispose of the sound
+ */
 
-//    public playerWalkSound(String walkSoundFile) {
-//        walkingSound = Gdx.audio.newSound(Gdx.files.internal());
-//    }
+public class playerWalkSound {
+    private Sound walkingSound; // sound object for walking sound effect
+    private boolean isWalking; // Flag to track whether the player is currently walking
+
+    /**
+     *
+     * @param walkSoundFile
+     * this parameter denotes the walking sound audio file
+     */
+    public playerWalkSound(String walkSoundFile) {
+        walkingSound = Gdx.audio.newSound(Gdx.files.internal(walkSoundFile));
+    }
+
+    /**
+     * This startWalking method is checking whether the player is walking or not.
+     * of player is walking, the below method will start playing sound
+     */
+    public void startWalking() {
+        if (!isWalking) {
+            walkingSound.loop();
+            isWalking = true;
+        }
+    }
+
+    /**
+     * This stoptWalking method is checking whether the player is walking or not.
+     * of player is not walking, the below method will stop the sound.
+     */
+    public void stopWalking() {
+        if (isWalking) {
+            walkingSound.stop();
+            isWalking = false;
+        }
+    }
+
+    /**
+     * The dispose method should be called when the sound manager
+     * is no longer needed to release system resources.
+     */
+    public void dispose() {
+        walkingSound.dispose();
+    }
 }

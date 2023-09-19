3486e21c880c0a05d5d22995d5cc6517c45568d4
aman-86 <aman.gupta@uqconnect.edu.au>
2023-09-19 20:54:00 +1000

    Created new java class playerWalkSound.java for to implement walking sound.

---
+++ source/core/src/main/com/csse3200/game/components/player/playerWalkSound.java
@@ -0,0 +1,13 @@
+package com.csse3200.game.components.player;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Sound;
+
+public class playerWalkSound {
+    private Sound walkingSound;
+    private boolean isWalking;
+
+//    public playerWalkSound(String walkSoundFile) {
+//        walkingSound = Gdx.audio.newSound(Gdx.files.internal());
+//    }
+}

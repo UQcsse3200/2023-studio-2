9b95514cba6b67acb61481abfecf64d5cf573870
aman-86 <aman.gupta@uqconnect.edu.au>
2023-09-20 23:03:55 +1000

    added the Player's LowHealth sound file and fully implemented the  sound which makes the user able to know when the Player's health is below 50

--- source/core/src/main/com/csse3200/game/components/playerStatsSound.java
+++ source/core/src/main/com/csse3200/game/components/playerStatsSound.java
@@ -1,5 +1,37 @@
 package com.csse3200.game.components;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Sound;
+
+/**
+ * This class is meant to implement
+ * player's death sound and its low health sound
+ */
 public class playerStatsSound {
+
+    private Sound lowHealthSound; // sound object for low health sound effect
+
+    /**
+     * @param SoundFile
+     * this parameter denotes the Player's Low health audio file
+     */
+    public playerStatsSound(String SoundFile) {
+        lowHealthSound = Gdx.audio.newSound(Gdx.files.internal(SoundFile));
+    }
+
+    /**
+     * playLowHealthSound plays the sound when player's health is low (below 50)
+     */
+    public void playLowHealthSound() {
+        lowHealthSound.play();
+    }
+
+    /**
+     *  The dispose method should be called when the sound manager
+     *  is no longer needed to release system resources.
+     */
+    public void dispose() {
+        lowHealthSound.dispose();
+    }
 
 }

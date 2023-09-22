3b5f63b9bf1da525a688f04a0e2912b8343fdad1
Isaac Robinson <isaac@irobinson.org>
2023-09-22 15:38:05 +1000

    Updated Configs to use HealthEntityConfig.

--- source/core/src/main/com/csse3200/game/entities/configs/PlayerConfig.java
+++ source/core/src/main/com/csse3200/game/entities/configs/PlayerConfig.java
@@ -3,12 +3,8 @@ package com.csse3200.game.entities.configs;
 /**
  * Defines the properties stored in player config files to be loaded by the Player Factory.
  */
-public class PlayerConfig extends BaseEntityConfig  {
+public class PlayerConfig extends HealthEntityConfig  {
   public int speed;
-  public int health;
-  public int baseAttack;
-  public int attackMultiplier;
-  public boolean isImmune;
 
   public PlayerConfig() {
     this.spritePath = "images/player.atlas";
@@ -18,23 +14,17 @@ public class PlayerConfig extends BaseEntityConfig  {
   public boolean equals(Object o) {
     if (this == o) return true;
     if (o == null || getClass() != o.getClass()) return false;
+    if (!super.equals(o)) return false;
 
     PlayerConfig that = (PlayerConfig) o;
 
-    if (speed != that.speed) return false;
-    if (health != that.health) return false;
-    if (baseAttack != that.baseAttack) return false;
-    if (attackMultiplier != that.attackMultiplier) return false;
-    return isImmune == that.isImmune;
+    return speed == that.speed;
   }
 
   @Override
   public int hashCode() {
-    int result = speed;
-    result = 31 * result + health;
-    result = 31 * result + baseAttack;
-    result = 31 * result + attackMultiplier;
-    result = 31 * result + (isImmune ? 1 : 0);
+    int result = super.hashCode();
+    result = 31 * result + speed;
     return result;
   }
 }

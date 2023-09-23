9ee305b28963087c94dd2fba3cc8f4867d97508b
Rowan Gray <github@rowangray.au>
2023-09-23 14:46:07 +1000

    Created SoundComponent and SoundsConfig

---
+++ source/core/src/main/com/csse3200/game/components/SoundComponent.java
@@ -0,0 +1,52 @@
+package com.csse3200.game.components;
+
+import com.badlogic.gdx.audio.Sound;
+import com.csse3200.game.entities.configs.SoundsConfig;
+import com.csse3200.game.files.UserSettings;
+import com.csse3200.game.services.ServiceLocator;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SoundComponent extends Component {
+    private final SoundsConfig soundsConfig;
+    private final Map<String, Sound> sounds;
+
+    public SoundComponent(SoundsConfig soundsConfig) {
+        this.soundsConfig = soundsConfig;
+        sounds = new HashMap<>();
+    }
+
+    @Override
+    public void create() {
+        super.create();
+
+        // loads the sounds and stores them in a map for later access
+        for (var entry : soundsConfig.soundsMap.entries()) {
+            Sound sound = ServiceLocator.getResourceService().getAsset(entry.value, Sound.class);
+
+            sounds.put(entry.key, sound);
+        }
+
+        entity.getEvents().addListener("playSound", this::playSound);
+    }
+
+    public void playSound(String soundName) {
+        var sound = sounds.get(soundName);
+
+        if (sound == null) {
+            return;
+        }
+
+        var settings = UserSettings.get();
+        // TODO change to specific volume setting
+        sound.play(settings.musicVolume);
+    }
+
+    @Override
+    public void dispose() {
+        super.dispose();
+
+        sounds.values().forEach(Sound::dispose);
+    }
+}

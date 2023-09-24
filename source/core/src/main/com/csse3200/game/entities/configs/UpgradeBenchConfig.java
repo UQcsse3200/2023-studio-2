2f7c0cacb2435a4edbe0f5d0f1079b44b650c441
aman-86 <aman.gupta@uqconnect.edu.au>
2023-09-24 23:58:20 +1000

    added component as SoundComponent in StructureFactory.java for UpgradeTree sound

--- source/core/src/main/com/csse3200/game/entities/configs/UpgradeBenchConfig.java
+++ source/core/src/main/com/csse3200/game/entities/configs/UpgradeBenchConfig.java
@@ -1,6 +1,13 @@
 package com.csse3200.game.entities.configs;
 
+/**
+ * Defines the properties stored in UpgradeBeanchConfig files
+ * to be loaded by the Structure Factory.
+ */
+
 public class UpgradeBenchConfig extends BaseEntityConfig {
+
+    public SoundsConfig sounds;
     public UpgradeBenchConfig() {
         this.spritePath = "images/upgradetree/upgradebench.png";
     }

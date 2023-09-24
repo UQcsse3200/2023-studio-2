076569e96e3d8264ef5f5a892f455c6b63fd2b42
connor-golin <connor.golin@gmail.com>
2023-09-24 22:02:30 +1000

    Adds null check to fix tests

--- source/core/src/main/com/csse3200/game/components/PowerupComponent.java
+++ source/core/src/main/com/csse3200/game/components/PowerupComponent.java
@@ -44,7 +44,10 @@ public class PowerupComponent extends Component {
         switch (type) {
             case HEALTH_BOOST ->{
                 playerCombatStats.setHealth(100);
-                entity.getEvents().trigger("playSound", "healthPowerup");
+
+                if (entity != null) {
+                    entity.getEvents().trigger("playSound", "healthPowerup");
+                }
             }
 
 
@@ -56,8 +59,10 @@ public class PowerupComponent extends Component {
 
                 playerActions.setSpeed(5, 5);
                 this.setDuration(1500);
-                entity.getEvents().trigger("playSound", "boostPowerup");
 
+                if (entity != null) {
+                    entity.getEvents().trigger("playSound", "boostPowerup");
+                }
 
                 // Speed up for 1.5 seconds, then return to normal speed
                 Timer.Task speedUp = new Timer.Task() {

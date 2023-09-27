39737505a5d93ecaea90062ecb29268bc9c39752
Yuvraj Singh <yuvi1jan@gmail.com>
2023-09-27 23:16:06 +1000

    Added Level 1 turret

---
+++ source/core/src/main/com/csse3200/game/components/structures/tools/AdvTurretTool.java
@@ -0,0 +1,31 @@
+package com.csse3200.game.components.structures.tools;
+
+import com.badlogic.gdx.utils.ObjectMap;
+import com.csse3200.game.entities.Entity;
+import com.csse3200.game.entities.PlaceableEntity;
+import com.csse3200.game.entities.buildables.TurretType;
+import com.csse3200.game.entities.factories.BuildablesFactory;
+
+/**
+ * A tool which allows the player to place a turret.
+ */
+public class AdvTurretTool extends PlacementTool {
+    /**
+     * Creates a new turret tool with the given cost.
+     * @param cost - the cost of a turret.
+     */
+    public AdvTurretTool(ObjectMap<String, Integer> cost) {
+        super(cost);
+    }
+
+    /**
+     * Creates a new turret.
+     * @param player - the player creating the turret.
+     * @return a new turret.
+     */
+    @Override
+    public PlaceableEntity createStructure(Entity player) {
+        return BuildablesFactory.createCustomTurret(TurretType.LEVEL_TWO, player);
+    }
+
+}

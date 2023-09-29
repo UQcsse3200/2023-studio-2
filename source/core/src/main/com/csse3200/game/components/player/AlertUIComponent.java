3b692a61b0064038a6b8ac124ff85a82359fd4cc
Rowan Gray <github@rowangray.au>
2023-09-29 14:57:48 +1000

    Added javadoc comments to AlertUIComponent, IAlpha, and ColorDrawable.

--- source/core/src/main/com/csse3200/game/components/player/AlertUIComponent.java
+++ source/core/src/main/com/csse3200/game/components/player/AlertUIComponent.java
@@ -1,31 +1,42 @@
 package com.csse3200.game.components.player;
 
 import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Matrix4;
 import com.badlogic.gdx.math.Quaternion;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
-import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.csse3200.game.rendering.ColorDrawable;
 import com.csse3200.game.services.ServiceLocator;
 import com.csse3200.game.ui.UIComponent;
 
+/**
+ * A custom UIComponent used which displays text within a yellow border for alerts.
+ */
 public class AlertUIComponent extends UIComponent implements IAlpha {
-    private static final float scaleFactor = 115;
+    /**
+     * The factor to reduce the scale of the batch projection.
+     */
+    private static final float SCALE_FACTOR = 115;
     private final String alert;
     private float alpha = 1.0f;
     private Table table;
     private ColorDrawable backgroundColor1;
     private ColorDrawable backgroundColor2;
+
+    /**
+     * Creates a new AlertUIComponent containing the given text.
+     * @param alert - the text to display within the component.
+     */
     public AlertUIComponent(String alert) {
         this.alert = alert;
     }
 
+    /**
+     * Creates the UI layout for the component.
+     */
     @Override
     public void create() {
         super.create();
@@ -56,13 +67,19 @@ public class AlertUIComponent extends UIComponent implements IAlpha {
         table.pack();
     }
 
+    /**
+     * Draws the UI Component on to the screen by dividing the batch's projection matrix by
+     * the SCALE_FACTOR to ensure that the UI isn't drawn too large on the screen.
+     *
+     * @param batch Batch to render to.
+     */
     @Override
     protected void draw(SpriteBatch batch) {
         Matrix4 originalMatrix = batch.getProjectionMatrix().cpy();
 
         var newScale = new Vector3();
         newScale = originalMatrix.getScale(newScale);
-        newScale.scl(1/scaleFactor);
+        newScale.scl(1/ SCALE_FACTOR);
 
         var originalPosition = originalMatrix.getTranslation(new Vector3());
         var originalRotation = originalMatrix.getRotation(new Quaternion());
@@ -72,7 +89,7 @@ public class AlertUIComponent extends UIComponent implements IAlpha {
 
 
         var position = new Vector2(entity.getCenterPosition().x, entity.getPosition().y);
-        position.scl(scaleFactor);
+        position.scl(SCALE_FACTOR);
         table.setPosition(position.x, position.y);
         table.draw(batch, alpha);
         batch.setProjectionMatrix(originalMatrix); //revert projection
--- source/core/src/main/com/csse3200/game/rendering/ColorDrawable.java
+++ source/core/src/main/com/csse3200/game/rendering/ColorDrawable.java
@@ -1,6 +1,5 @@
 package com.csse3200.game.rendering;
 
-import com.badlogic.gdx.assets.loaders.AssetLoader;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
@@ -8,29 +7,54 @@ import com.badlogic.gdx.scenes.scene2d.utils.BaseDrawable;
 import com.csse3200.game.components.player.IAlpha;
 import com.csse3200.game.services.ServiceLocator;
 
+/**
+ * Extends BaseDrawable to implement the ability to create a drawable for a custom color.
+ * Taken from a <a href="https://stackoverflow.com/a/54642587">StackOverflow answer</a> by Steve Moseley.
+ */
 public class ColorDrawable extends BaseDrawable implements IAlpha {
-    private float r, g, b, a;
-    private Color savedBatchColor = new Color();
+    private final float red, green, blue;
+    private float alpha;
+    private final Color savedBatchColor = new Color();
 
-    public ColorDrawable(float r, float g, float b, float a) {
-        this.r = r;
-        this.g = g;
-        this.b = b;
-        this.a = a;
+    /**
+     * Creates a new ColorDrawable with the given color (in RGB).
+     * @param red - the fraction of red in the color (0 to 1)
+     * @param green - the fraction of green in the color (0 to 1)
+     * @param blue - the fraction of blue in the color (0 to 1)
+     * @param alpha - the fraction of alpha in the color (0 to 1)
+     */
+    public ColorDrawable(float red, float green, float blue, float alpha) {
+        this.red = red;
+        this.green = green;
+        this.blue = blue;
+        this.alpha = alpha;
     }
 
+    /**
+     * Draws the ColorDrawable to the screen by recoloring a white texture to the defined color.
+     * @param batch - the batch to draw the color to.
+     * @param x - the x position of the drawable
+     * @param y - the y position of the drawable
+     * @param width - the width of the drawable
+     * @param height - the height of the drawable
+     */
     @Override
     public void draw(Batch batch, float x, float y, float width, float height) {
         // Save the batch colour as we are about to change it
         savedBatchColor.set(batch.getColor());
-        batch.setColor(r, g, b, a);
+        batch.setColor(red, green, blue, alpha);
         // Draw a white texture with the current batch colour
-        batch.draw(ServiceLocator.getResourceService().getAsset("images/white_color_texture.png", Texture.class), x, y, width, height);
+        batch.draw(ServiceLocator.getResourceService().getAsset("images/white_color_texture.png", Texture.class),
+                x, y, width, height);
         batch.setColor(savedBatchColor);
     }
 
+    /**
+     * Changes the alpha of the ColorDrawable.
+     * @param alpha - the new alpha to use.
+     */
     @Override
     public void setAlpha(float alpha) {
-        this.a = alpha;
+        this.alpha = alpha;
     }
 }

195280d9ccdcc0de03d0aea6c5e194f3108b21e2
Manandeep Singh <manandeep1500@gmail.com>
2023-09-30 02:53:06 +1000

    added carconfig

--- source/core/src/main/com/csse3200/game/entities/configs/CarConfig.java
+++ source/core/src/main/com/csse3200/game/entities/configs/CarConfig.java
@@ -1,2 +1,7 @@
-package com.csse3200.game.entities.configs;public class Carconfig {
+package com.csse3200.game.entities.configs;
+
+public class CarConfig extends BaseEntityConfig {
+    public CarConfig() {
+        this.spritePath = "images/car.atlas";
+    }
 }

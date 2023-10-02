2b73c9e5c17d4e434db49e4d5c7d0b1cdb862a9c
Manvi Narang <65333992+ManviNarang01@users.noreply.github.com>
2023-10-02 17:57:48 +1000

    Added tilesets used for FireMap

---
+++ source/core/assets/map/CampFiretile.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="CampFire" tilewidth="16" tileheight="16" tilecount="80" columns="20">
+ <image source="../Downloads/CampFire.png" width="320" height="64"/>
+</tileset>

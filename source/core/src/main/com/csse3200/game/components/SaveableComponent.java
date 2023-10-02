291091760d8021a7c543a038505fee3318a2242a
henrybatt <hbatt1545@gmail.com>
2023-10-02 23:52:49 +1000

    Fixed GridPosition shift calculation.

--- source/core/src/main/com/csse3200/game/components/SaveableComponent.java
+++ source/core/src/main/com/csse3200/game/components/SaveableComponent.java
@@ -1,5 +1,6 @@
 package com.csse3200.game.components;
 
+import com.badlogic.gdx.math.GridPoint2;
 import com.csse3200.game.entities.Entity;
 import com.csse3200.game.entities.configs.BaseEntityConfig;
 
@@ -15,7 +16,9 @@ public class SaveableComponent<T extends BaseEntityConfig> extends Component {
     }
 
     public T save() {
-        return saveFunction.apply(entity);
+        BaseEntityConfig config = saveFunction.apply(entity);
+        if (config.position != null) config.position = new GridPoint2((int) (config.position.x / 0.5), (int) (config.position.y /0.5));
+        return (T) config;
     }
 
     public Class<T> getType() {

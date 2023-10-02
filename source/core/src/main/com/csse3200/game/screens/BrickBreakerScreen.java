f278dd7efd941422c4439e686aa408ea82bdaa42
Arsh <arsh.upadhyaya123@gmail.com>
2023-10-03 06:51:32 +1000

    created basic screen for the brick breaker minigame

---
+++ source/core/src/main/com/csse3200/game/screens/BrickBreakerScreen.java
@@ -0,0 +1,96 @@
+package com.csse3200.game.screens;
+
+import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.csse3200.game.GdxGame;
+import com.csse3200.game.components.gamearea.PerformanceDisplay;
+import com.csse3200.game.components.maingame.MainGameActions;
+import com.csse3200.game.components.maingame.MainGameExitDisplay;
+import com.csse3200.game.entities.Entity;
+import com.csse3200.game.entities.factories.RenderFactory;
+import com.csse3200.game.input.InputComponent;
+import com.csse3200.game.input.InputDecorator;
+import com.csse3200.game.rendering.Renderer;
+import com.csse3200.game.services.ResourceService;
+import com.csse3200.game.services.ServiceLocator;
+import com.csse3200.game.ui.terminal.Terminal;
+import com.csse3200.game.ui.terminal.TerminalDisplay;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BrickBreakerScreen extends ScreenAdapter {
+    private final GdxGame game;
+    public static final Logger logger = LoggerFactory.getLogger(BrickBreakerScreen.class);
+    private final Renderer renderer;
+    private static final String[] brickBreakerSounds = {
+
+    };
+    private static final String[] brickBreakerTextures = {};
+    public BrickBreakerScreen(GdxGame game){
+        this.game = game;
+        renderer = RenderFactory.createRenderer();
+        loadAssets();
+        createUI();
+    }
+
+    @Override
+    public void render(float delta) {
+        ServiceLocator.getEntityService().update();
+        renderer.render();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        renderer.resize(width, height);
+        logger.trace("Resized renderer: ({} x {})", width, height);
+    }
+
+
+    @Override
+    public void pause() {
+        logger.info("Game paused");
+    }
+
+    @Override
+    public void resume() {
+        logger.info("Game resumed");
+    }
+
+    @Override
+    public void dispose() {
+        logger.debug("Disposing death screen");
+
+        renderer.dispose();
+        unloadAssets();
+    }
+    private void loadAssets() {
+        logger.debug("Loading assets");
+        ResourceService resourceService = ServiceLocator.getResourceService();
+        resourceService.loadTextures(brickBreakerTextures);
+        resourceService.loadSounds(brickBreakerSounds);
+        ServiceLocator.getResourceService().loadAll();
+    }
+
+    private void unloadAssets() {
+        logger.debug("Unloading assets");
+        ResourceService resourceService = ServiceLocator.getResourceService();
+        resourceService.unloadAssets(brickBreakerTextures);
+    }
+    private void createUI() {
+        logger.debug("Creating ui");
+        Stage stage = ServiceLocator.getRenderService().getStage();
+        InputComponent inputComponent =
+                ServiceLocator.getInputService().getInputFactory().createForTerminal();
+
+        Entity ui = new Entity();
+        ui.addComponent(new InputDecorator(stage, 10))
+                .addComponent(new PerformanceDisplay())
+                .addComponent(new MainGameActions(this.game))
+                .addComponent(new MainGameExitDisplay())
+                .addComponent(new Terminal())
+                .addComponent(inputComponent)
+                .addComponent(new TerminalDisplay());
+
+        ServiceLocator.getEntityService().register(ui);
+    }
+}

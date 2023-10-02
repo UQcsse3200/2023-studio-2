ab1e61969ae89ac1ea92258034599dbfcc7b3db9
Manvi Narang <65333992+ManviNarang01@users.noreply.github.com>
2023-10-03 02:37:28 +1000

    updated tileset

--- source/core/assets/map/spooky_trees.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="spooky_trees" tilewidth="16" tileheight="16" tilecount="126" columns="14">
- <image source="../Downloads/spooky_trees.png" width="238" height="148"/>
-</tileset>

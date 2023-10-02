2041e66e3afd454d6b52790e4132f35c93f71135
Khushi Jain <khushijain0302@gmail.com>
2023-10-02 21:29:55 +1000

    Added basic dialogue box message. Need to set position of text.

--- source/core/src/main/com/csse3200/game/areas/TutorialDialogue.java
+++ source/core/src/main/com/csse3200/game/areas/TutorialDialogue.java
@@ -4,13 +4,14 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.Window;
 import com.csse3200.game.ui.UIComponent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class TutorialDialogue extends UIComponent {
     private static final Logger logger = LoggerFactory.getLogger(TutorialDialogue.class);
-   // private Window window = new Window("Tutorial",skin);
+    private Window window = new Window("Tutorial",skin);
     private Table table = new Table(skin);
     private String message;
 
@@ -27,10 +28,12 @@ public class TutorialDialogue extends UIComponent {
     public void createDialogue () {
         logger.info("Tutorial Dialogue 1");
         message = "Hello Player";
-        table.center().left();
+        //table.center().left();
+        table.pad(40);
+        table.row();
         Label label = new Label(message,skin);
         label.setColor(Color.WHITE);
-        table.pad(50);
+        table.pad(40);
         table.add(label);
         message = "Welcome to the game";
         label = new Label(message,skin);
@@ -38,6 +41,11 @@ public class TutorialDialogue extends UIComponent {
         table.add(label);
         table.setFillParent(true);
         table.padBottom(45f).padRight(45f);
-        stage.addActor(table);
+        window.center().left();
+        window.pad(40,5,5,5);
+        //window.pad(40);
+        window.add(table);
+        window.pack();
+        stage.addActor(window);
     }
 }
--- source/core/src/main/com/csse3200/game/areas/TutorialGameArea.java
+++ source/core/src/main/com/csse3200/game/areas/TutorialGameArea.java
@@ -1,28 +1,10 @@
 package com.csse3200.game.areas;
 
-import com.badlogic.gdx.audio.Music;
-import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
-import com.badlogic.gdx.math.GridPoint2;
-import com.badlogic.gdx.math.Vector2;
 import com.csse3200.game.GdxGame;
-import com.csse3200.game.areas.mapConfig.GameAreaConfig;
-import com.csse3200.game.areas.mapConfig.InvalidConfigException;
-import com.csse3200.game.areas.mapConfig.MapConfigLoader;
 import com.csse3200.game.areas.terrain.TerrainFactory;
 import com.csse3200.game.entities.Entity;
-import com.csse3200.game.entities.TileEntity;
-import com.csse3200.game.entities.factories.EnvironmentFactory;
-import com.csse3200.game.entities.factories.ObstacleFactory;
-import com.csse3200.game.entities.factories.PlayerFactory;
-import com.csse3200.game.files.UserSettings;
-import com.csse3200.game.services.ResourceService;
-import com.csse3200.game.services.ServiceLocator;
-import com.csse3200.game.services.TerrainService;
-import com.csse3200.game.utils.math.GridPoint2Utils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.util.List;
 
 public class TutorialGameArea extends MapGameArea {
     private static final Logger logger = LoggerFactory.getLogger(TutorialGameArea.class);
@@ -42,7 +24,7 @@ public class TutorialGameArea extends MapGameArea {
 
     private void createDialogueUI() {
         Entity ui = new Entity();
-        //ui.addComponent(new TutorialDialogue());
+        ui.addComponent(new TutorialDialogue());
         spawnEntity(ui);
     }
 }

f5ee8716b2ab831cae5e468aea3b9d8f6ca946f9
Arsh <arsh.upadhyaya123@gmail.com>
2023-10-03 11:48:28 +1000

    Fixed the camera position of the game

--- source/core/src/main/com/csse3200/game/screens/BrickBreakerScreen.java
+++ source/core/src/main/com/csse3200/game/screens/BrickBreakerScreen.java
@@ -1,6 +1,7 @@
 package com.csse3200.game.screens;
 
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.csse3200.game.GdxGame;
 import com.csse3200.game.areas.BricKBreakerGameArea;
@@ -13,7 +14,9 @@ import com.csse3200.game.entities.Entity;
 import com.csse3200.game.entities.factories.RenderFactory;
 import com.csse3200.game.input.InputComponent;
 import com.csse3200.game.input.InputDecorator;
+import com.csse3200.game.physics.PhysicsEngine;
 import com.csse3200.game.rendering.Renderer;
+import com.csse3200.game.services.GameStateObserver;
 import com.csse3200.game.services.ResourceService;
 import com.csse3200.game.services.ServiceLocator;
 import com.csse3200.game.ui.terminal.Terminal;
@@ -23,11 +26,19 @@ import org.slf4j.LoggerFactory;
 
 public class BrickBreakerScreen extends ScreenAdapter {
     private final GdxGame game;
+    private final PhysicsEngine physicsEngine;
     public static final Logger logger = LoggerFactory.getLogger(BrickBreakerScreen.class);
     private final Renderer renderer;
+    private static final Vector2 CAMERA_POSITION = new Vector2(15f, 10f);
     public BrickBreakerScreen(GdxGame game){
         this.game = game;
+        physicsEngine = ServiceLocator.getPhysicsService().getPhysics();
+
+
+        //Vector2 shipPos = ServiceLocator.getEntityService().getEntitiesByComponent(ShipActions.class).get(0).getPosition();
         renderer = RenderFactory.createRenderer();
+        renderer.getCamera().getEntity().setPosition(CAMERA_POSITION);
+        renderer.getDebug().renderPhysicsWorld(physicsEngine.getWorld());
         loadAssets();
         createUI();
         TerrainFactory terrainFactory = new TerrainFactory(renderer.getCamera());

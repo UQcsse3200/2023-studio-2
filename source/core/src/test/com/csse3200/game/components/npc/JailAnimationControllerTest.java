51f32ef6f41377e7e0b37f06c23dc8b2f17ee82a
Aniket Gupta <aniketgupta1707@gmail.com>
2023-10-04 04:50:54 +1000

    Added Test classes for JailAnimation and AstroAnimation

---
+++ source/core/src/test/com/csse3200/game/components/npc/JailAnimationControllerTest.java
@@ -0,0 +1,61 @@
+package com.csse3200.game.components.npc;
+
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.csse3200.game.components.Component;
+import com.csse3200.game.entities.Entity;
+import com.csse3200.game.rendering.AnimationRenderComponent;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
+
+class JailAnimationControllerTest {
+
+    @Mock
+    AssetManager assetManager;
+
+    @Mock
+    AnimationRenderComponent animator;
+
+    @Mock
+    TextureAtlas atlas;
+
+    JailAnimationController controller;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+
+        when(assetManager.get("images/Jail/jail.atlas")).thenReturn(atlas);
+
+        controller = new JailAnimationController(assetManager);
+
+        controller.entity = mock(Entity.class);
+
+         when(controller.entity.getComponent(AnimationRenderComponent.class)).thenReturn(animator);
+    }
+
+    @Test
+    void testCreate() {
+        controller.create();
+
+        verify(animator).startAnimation("jail_close");
+        verify(assetManager).load(anyString(), eq(TextureAtlas.class));
+        verify(assetManager).finishLoading();
+    }
+
+    @Test
+    void testUpdate() {
+        controller.animationDuration= 10f;
+
+        when(animator.getCurrentAnimation()).thenReturn("jail_close");
+
+        controller.update();
+    }
+}

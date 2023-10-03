6c0551ebc881298c93f342730c745c95914098f2
Isaac Robinson <isaac@irobinson.org>
2023-10-03 10:05:37 +1000

    Fix stashed changed syntax error

--- source/core/src/main/com/csse3200/game/areas/MapGameArea.java
+++ source/core/src/main/com/csse3200/game/areas/MapGameArea.java
@@ -496,11 +496,6 @@ public class MapGameArea extends GameArea{
         ResourceService resourceService = ServiceLocator.getResourceService();
 
         if (mapConfig == null) return;
-<<<<<<< Updated upstream
-=======
-        resourceService.unloadAssets(mapConfig.getEntityTextures());
-        AssetsConfig assets = mapConfig.assets;
->>>>>>> Stashed changes
 
         if (mapConfig.getEntityTextures() != null)
             resourceService.unloadAssets(mapConfig.getEntityTextures());

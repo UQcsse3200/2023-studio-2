af783773afd114708fe2b8b060d4c0c2a40d5ff9
Arsh <arsh.upadhyaya123@gmail.com>
2023-10-03 11:34:11 +1000

    implemented the gamearea and included random image for now as background tile

--- source/core/src/main/com/csse3200/game/areas/BricKBreakerGameArea.java
+++ source/core/src/main/com/csse3200/game/areas/BricKBreakerGameArea.java
@@ -1,5 +1,7 @@
 package com.csse3200.game.areas;
 
+import com.badlogic.gdx.math.GridPoint2;
+import com.badlogic.gdx.math.Vector2;
 import com.csse3200.game.areas.terrain.TerrainFactory;
 import com.csse3200.game.components.gamearea.GameAreaDisplay;
 import com.csse3200.game.entities.Entity;
@@ -15,6 +17,7 @@ public class BricKBreakerGameArea extends GameArea{
 
     private static final String[] BrickBreakerTextures = {
             "images/minigame/SpaceMiniGameBackground.png",
+            "images/minigame/wormhole.png"
 
     };
 
@@ -31,6 +34,18 @@ public class BricKBreakerGameArea extends GameArea{
     public void create() {
         loadAssets();
         displayUI();
+        spawnTerrain();
+    }
+    private void spawnTerrain() {
+        // Background terrain
+        terrain = terrainFactory.createSpaceTerrain(TerrainFactory.TerrainType.BRICK_DEMO);
+        spawnEntity(new Entity().addComponent(terrain));
+
+        // Terrain walls
+        float tileSize = terrain.getTileSize();
+        GridPoint2 tileBounds = terrain.getMapBounds(0);
+        Vector2 worldBounds = new Vector2(tileBounds.x * tileSize, tileBounds.y * tileSize);
+
     }
     private void displayUI() {
         Entity ui = new Entity();
--- source/core/src/main/com/csse3200/game/screens/BrickBreakerScreen.java
+++ source/core/src/main/com/csse3200/game/screens/BrickBreakerScreen.java
@@ -3,6 +3,9 @@ package com.csse3200.game.screens;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.csse3200.game.GdxGame;
+import com.csse3200.game.areas.BricKBreakerGameArea;
+import com.csse3200.game.areas.SpaceGameArea;
+import com.csse3200.game.areas.terrain.TerrainFactory;
 import com.csse3200.game.components.gamearea.PerformanceDisplay;
 import com.csse3200.game.components.maingame.MainGameActions;
 import com.csse3200.game.components.maingame.MainGameExitDisplay;
@@ -27,6 +30,9 @@ public class BrickBreakerScreen extends ScreenAdapter {
         renderer = RenderFactory.createRenderer();
         loadAssets();
         createUI();
+        TerrainFactory terrainFactory = new TerrainFactory(renderer.getCamera());
+        BricKBreakerGameArea bricKBreakerGameArea= new BricKBreakerGameArea(terrainFactory);
+        bricKBreakerGameArea.create();
     }
 
     @Override

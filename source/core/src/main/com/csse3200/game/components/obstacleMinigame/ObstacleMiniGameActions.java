e97fa6661bd8633cde2312e19a7bef07ed55eff6
Foref <s4679430@student.uq.edu.au>
2023-10-03 17:09:42 +1000

    Added some comments to ObstacleMiniGameActions

--- source/core/src/main/com/csse3200/game/components/obstacleMinigame/ObstacleMiniGameActions.java
+++ source/core/src/main/com/csse3200/game/components/obstacleMinigame/ObstacleMiniGameActions.java
@@ -7,12 +7,23 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import com.csse3200.game.screens.SpaceMapScreen;
 
+/**
+ * This class listens to events relevant to the SpaceGameScreen and does something when events is triggered
+ *
+ * <p>Exit to main menu screen event is Triggered on ReturnToPlanetDisplay</p>
+ */
 public class ObstacleMiniGameActions extends MainGameActions {
     private static final Logger logger = LoggerFactory.getLogger(ObstacleMiniGameActions.class);
     private final GdxGame game;
     private final Stage stage;
     private final SpaceMapScreen screen;
 
+    /**
+     * Creates a ObstacleMiniGameActions that handles events that exit SpaceGameScreen
+     * @param game Current Game
+     * @param stage Current Stage
+     * @param screen Current Screen to be handled
+     */
     public ObstacleMiniGameActions(GdxGame game, Stage stage, SpaceMapScreen screen) {
         super(game);
         this.game = game;
@@ -21,6 +32,9 @@ public class ObstacleMiniGameActions extends MainGameActions {
     }
 
 
+    /**
+     * Creates listeners for specific events
+     */
     @Override
     public void create() {
         entity.getEvents().addListener("exit", this::onExit);

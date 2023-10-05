fd9b1e0cdbebef650931c7dedd07c334d276eae5
Shaivika Anand <shaivika.anand@gmail.com>
2023-10-05 10:33:47 +1000

    made Jdocs for input override component

--- source/core/src/test/com/csse3200/game/input/InputOverrideComponentTest.java
+++ source/core/src/test/com/csse3200/game/input/InputOverrideComponentTest.java
@@ -1,26 +1,43 @@
 package com.csse3200.game.input;
-
 import com.csse3200.game.input.InputOverrideComponent;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * This class contains unit tests for the InputOverrideComponent class.
+ */
 public class InputOverrideComponentTest {
 
+    /**
+     * Test case for the keyDown method.
+     */
     @Test
     public void testKeyDown() {
         InputOverrideComponent inputOverride = new InputOverrideComponent();
-        assertTrue(inputOverride.keyDown(123)); // Any keycode should return true
+
+        // Verify that keyDown method returns true for any keycode.
+        assertTrue(inputOverride.keyDown(123));
     }
 
+    /**
+     * Test case for the keyUp method.
+     */
     @Test
     public void testKeyUp() {
         InputOverrideComponent inputOverride = new InputOverrideComponent();
-        assertTrue(inputOverride.keyUp(456)); // Any keycode should return true
+
+        // Verify that keyUp method returns true for any keycode.
+        assertTrue(inputOverride.keyUp(456));
     }
 
+    /**
+     * Test case for the touchDown method.
+     */
     @Test
     public void testTouchDown() {
         InputOverrideComponent inputOverride = new InputOverrideComponent();
-        assertTrue(inputOverride.touchDown(0, 0, 1, 2)); // Any input values should return true
+
+        // Verify that touchDown method returns true for any input values.
+        assertTrue(inputOverride.touchDown(0, 0, 1, 2));
     }
 }

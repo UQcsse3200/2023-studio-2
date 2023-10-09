fe368b852c4454e2ad6b89128595a03bb17bf172
Gaurika Diwan <diwangaurika@icloud.com>
2023-10-09 06:04:04 +0530

    Added Points to unlock potions

--- source/core/src/main/com/csse3200/game/LabWindow.java
+++ source/core/src/main/com/csse3200/game/LabWindow.java
@@ -11,7 +11,6 @@ import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
-
 import com.csse3200.game.components.PowerupType;
 import com.csse3200.game.entities.Entity;
 import com.csse3200.game.input.InputOverrideComponent;
@@ -21,9 +20,14 @@ import com.csse3200.game.services.ServiceLocator;
  * This is a window that can be added to a stage to pop up for the extractor Laboratory.
  */
 public class LabWindow extends Window {
-    private final InputOverrideComponent inputOverrideComponent;
-    Table buttonTable;
-    Table exit;
+    private InputOverrideComponent inputOverrideComponent;
+    private int points = 100; // Initialize points with your starting value
+    private Label pointsLabel; // Label to display points
+
+    private TextButton[] potionButton=new TextButton[8];
+
+    private Texture potionImage;
+    private String potionName;
 
     public static LabWindow MakeNewLaboratory() {
         Texture background = new Texture("images/companion/lab.png");
@@ -33,7 +37,10 @@ public class LabWindow extends Window {
 
     public LabWindow(Texture background) {
         super("", new WindowStyle(new BitmapFont(), Color.BLACK, new TextureRegionDrawable(background)));
+        create();
+    }
 
+    void create(){
         // Here set up the window to be centered on the stage with 80% width and 65% height.
         Stage stage = ServiceLocator.getRenderService().getStage();
         setWidth((float) (stage.getWidth() * 0.70));
@@ -41,105 +48,43 @@ public class LabWindow extends Window {
         setPosition(stage.getWidth() / 2 - getWidth() / 2 * getScaleX(), stage.getHeight() / 2 - getHeight() / 2 * getScaleY());
         Skin skin = new Skin(Gdx.files.internal("kenney-rpg-expansion/kenneyrpg.json"));
         // Create a Table to hold the buttons and center them within the window
+
         Table buttonTable = new Table();
         buttonTable.setFillParent(true);
         // Fill the entire LabWindow
 
-        Table exit = new Table();
-        Texture deathpotionImage = new Texture("images/powerups/death_potion.png");
-        Texture speedpotionImage = new Texture("images/powerups/speed_boost.png");
-        Texture healthpotionImage = new Texture("images/powerups/health_boost.png");
-        Texture invincibilitypotionImage = new Texture("images/powerups/temp_immunity.png");
-        Texture doubledamageImage = new Texture("images/powerups/double_damage.png");
-        Texture extralifeImage = new Texture("images/powerups/extra_life.png");
-        Texture snapImage = new Texture("images/powerups/snap.png");
-        Texture doublecrossImage = new Texture("images/powerups/double_cross.png");
+        Texture[] potionImages = {
+                new Texture("images/powerups/death_potion.png"),
+                new Texture("images/powerups/speed_potion.png"),
+                new Texture("images/powerups/health_potion.png"),
+                new Texture("images/powerups/temp_immunity.png"),
+                new Texture("images/powerups/double_damage.png"),
+                new Texture("images/powerups/extra_life.png"),
+                new Texture("images/powerups/snap.png"),
+                new Texture("images/powerups/double_cross.png")
+        };
 
+        String[] potionNames = {
+                "Death", "Speed", "Health", "Invincibility", "2x Damage", "Extra Life", "Snap", "Double Cross"
+        };
 
-        Image potion1ImageWidget = new Image(deathpotionImage);
-        Image potion2ImageWidget = new Image(speedpotionImage);
-        Image potion3ImageWidget = new Image(healthpotionImage);
-        Image potion4ImageWidget = new Image(invincibilitypotionImage);
-        Image potion5ImageWidget = new Image(doubledamageImage);
-        Image potion6ImageWidget = new Image(extralifeImage);
-        Image potion7ImageWidget = new Image(snapImage);
-        Image potion8ImageWidget = new Image(doublecrossImage);
+        int[] potionCosts = {
+                50, 60, 30, 40, 70, 80, 90, 100
+        };
+        // Create points label
+        pointsLabel = new Label("Points: " + points, skin);
+        pointsLabel.setFontScale(0.5f); // Adjust font scale as needed
+        pointsLabel.setPosition(40, 550); // Adjust position as needed
+        addActor(pointsLabel);
+        // Create a separate table for the "EXIT" button and position it at the bottom-right
+        Table exitButtonTable = new Table();
+        exitButtonTable.setFillParent(true);
+        exitButtonTable.bottom().right(); // Align the table to the bottom-right corner of the screen
 
-
-        TextButton potion1 = new TextButton("Death", skin);
-        TextButton potion2 = new TextButton("Speed", skin);
-        TextButton potion3 = new TextButton("Health", skin);
-        TextButton potion4 = new TextButton("Invincibility", skin);
-        TextButton potion5 = new TextButton("2x Damage", skin);
-        TextButton potion6 = new TextButton("Extra Life", skin);
-        TextButton potion7 = new TextButton("Snap", skin);
-        TextButton potion8 = new TextButton("Double Cross", skin);
         TextButton returnToGameButton = new TextButton("EXIT", skin);
-
-        float buttonWidth = 150f; // Adjust as needed
-        float buttonHeight = 180f;
-
-
-        potion1.setWidth(buttonWidth);
-        potion1.setHeight(buttonHeight);
-
-        potion2.setWidth(buttonWidth);
-        potion2.setHeight(buttonHeight);
-
-        potion3.setWidth(buttonWidth);
-        potion3.setHeight(buttonHeight);
-
-        potion4.setWidth(buttonWidth);
-        potion4.setHeight(buttonHeight);
-
-        potion5.setWidth(buttonWidth);
-        potion5.setHeight(buttonHeight);
-
-        potion6.setWidth(buttonWidth);
-        potion6.setHeight(buttonHeight);
-
-        potion7.setWidth(buttonWidth);
-        potion7.setHeight(buttonHeight);
-
-        potion8.setWidth(buttonWidth);
-        potion8.setHeight(buttonHeight);
-
-        returnToGameButton.setWidth(buttonWidth);
-        returnToGameButton.setHeight(buttonHeight);
-
-        potion1.add(potion1ImageWidget).width(100).height(64);
-        potion2.add(potion2ImageWidget).width(100).height(64);
-        potion3.add(potion3ImageWidget).width(100).height(64);
-        potion4.add(potion4ImageWidget).width(100).height(64);
-        potion5.add(potion5ImageWidget).width(100).height(64);
-        potion6.add(potion6ImageWidget).width(100).height(64);
-        potion7.add(potion7ImageWidget).width(100).height(64);
-        potion8.add(potion8ImageWidget).width(100).height(64);
-        potion8.add(potion8ImageWidget).width(100).height(64);
-
-        buttonTable.top();
-        buttonTable.add(potion1).padTop(70).padLeft(40);
-        buttonTable.add(potion2).padTop(70).padLeft(40);
-        buttonTable.add(potion3).padTop(70).padLeft(40);
-        buttonTable.add(potion4).padTop(70).padLeft(40);
-        buttonTable.row();
-        buttonTable.add(potion5).padTop(70).padLeft(40);
-        buttonTable.add(potion6).padTop(70).padLeft(40);
-        buttonTable.add(potion7).padTop(70).padLeft(40);
-        buttonTable.add(potion8).padTop(70).padLeft(40);
-        buttonTable.row(); //Move to the next row
-//        buttonTable.add(returnToGameButton).padTop(100).padLeft(600);
-        addActor(buttonTable);
+        exitButtonTable.add(returnToGameButton).padBottom(20).padRight(20);
+        addActor(exitButtonTable);
         //addActor(exit);
-
-//
-
-        Table button2Table = new Table();
-        button2Table.setFillParent(true);
-        button2Table.add(returnToGameButton).padTop(400).padLeft(860);
-        addActor(button2Table);
-
-
         returnToGameButton.addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
@@ -147,69 +92,69 @@ public class LabWindow extends Window {
             }
         });
 
-        potion1.addListener(new ChangeListener() {
-            @Override
-            public void changed(ChangeEvent event, Actor actor) {
-                ServiceLocator.getEntityService().getCompanion().getEvents().trigger("SpawnPowerup",PowerupType.DEATH_POTION);
-                failLaboratory();
-            }
-        });
-        potion2.addListener(new ChangeListener() {
-            @Override
-            public void changed(ChangeEvent event, Actor actor) {
-                ServiceLocator.getEntityService().getCompanion().getEvents().trigger("SpawnPowerup",PowerupType.SPEED_BOOST);
-                failLaboratory();
-            }
-        });
-        potion3.addListener(new ChangeListener() {
-            @Override
-            public void changed(ChangeEvent event, Actor actor) {
-                ServiceLocator.getEntityService().getCompanion().getEvents().trigger("SpawnPowerup",PowerupType.HEALTH_BOOST);
-                failLaboratory();
-            }
-        });
+        for (int i = 0; i < potionImages.length; i++) {
+            potionImage = potionImages[i];
+            potionName = potionNames[i];
 
-        potion4.addListener(new ChangeListener() {
-            @Override
-            public void changed(ChangeEvent event, Actor actor) {
-                ServiceLocator.getEntityService().getCompanion().getEvents().trigger("SpawnPowerup",PowerupType.TEMP_IMMUNITY);
-                failLaboratory();
-            }
-        });
 
-        potion5.addListener(new ChangeListener() {
-            @Override
-            public void changed(ChangeEvent event, Actor actor) {
-                ServiceLocator.getEntityService().getCompanion().getEvents().trigger("SpawnPowerup",PowerupType.DOUBLE_DAMAGE);
-                failLaboratory();
-            }
-        });
+            Image potionImageWidget = new Image(potionImage);
+            potionButton[i] = new TextButton(potionName + " - " + potionCosts[i] + " points", skin);
+            buttonTable.add(potionImageWidget).width(100).height(64);;
+            buttonTable.add(potionButton[i]).pad(10);
+            int finalI = i;
+            potionButton[i].addListener(new ClickListener() {
+                @Override
+                public void clicked(InputEvent event, float x, float y) {
+                    // Deduct points when purchasing a potion and update the pointsLabel
+                    if (points >=potionCosts[finalI]) {
+                        update(potionCosts[finalI]);
+                        pointsLabel.setText("Points: " + points); // Update pointsLabel text
+                        PowerupType powerupType = getPowerupTypeForPotionName(potionNames[finalI]);
+                        ServiceLocator.getEntityService().getCompanion().getEvents().trigger("SpawnPowerup", powerupType);
+                    }
+                }
+            });
 
-        potion6.addListener(new ChangeListener() {
-            @Override
-            public void changed(ChangeEvent event, Actor actor) {
-                ServiceLocator.getEntityService().getCompanion().getEvents().trigger("SpawnPowerup",PowerupType.EXTRA_LIFE);
-                failLaboratory();
+            if(i==3) {
+                buttonTable.row();
             }
-        });
-        potion7.addListener(new ChangeListener() {
-            @Override
-            public void changed(ChangeEvent event, Actor actor) {
-                ServiceLocator.getEntityService().getCompanion().getEvents().trigger("SpawnPowerup",PowerupType.SNAP);
-                failLaboratory();
-            }
-        });
-        potion8.addListener(new ChangeListener() {
-            @Override
-            public void changed(ChangeEvent event, Actor actor) {
-                ServiceLocator.getEntityService().getCompanion().getEvents().trigger("SpawnPowerup",PowerupType.DOUBLE_CROSS);
-                failLaboratory();
-            }
-        });
+        }
+        addActor(buttonTable);
+
+
+
 
         // Override all normal user input
         inputOverrideComponent = new InputOverrideComponent();
         ServiceLocator.getInputService().register(inputOverrideComponent);
+
+    }
+
+    void update (int cost){
+        this.points-=cost;
+    }
+
+    private PowerupType getPowerupTypeForPotionName(String potionName) {
+        switch (potionName) {
+            case "Death":
+                return PowerupType.DEATH_POTION;
+            case "Speed":
+                return PowerupType.SPEED_BOOST;
+            case "Health":
+                return PowerupType.HEALTH_BOOST;
+            case "Invincibility":
+                return PowerupType.TEMP_IMMUNITY;
+            case "2x Damage":
+                return PowerupType.DOUBLE_DAMAGE;
+            case "Extra Life":
+                return PowerupType.EXTRA_LIFE;
+            case "Snap":
+                return PowerupType.SNAP;
+            case "Double Cross":
+                return PowerupType.DOUBLE_CROSS;
+            default:
+                return null;
+        }
     }
 
     /**
@@ -230,3 +175,4 @@ public class LabWindow extends Window {
         return super.remove();
     }
 }
+// updated with points

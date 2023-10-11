d8a1c15ebd602d11c710e9a117efdaafc3b36cae
Foref <s4679430@student.uq.edu.au>
2023-10-11 21:50:15 +1000

    initial setup for ShipActionstest

---
+++ source/core/src/test/com/csse3200/game/components/ships/ShipActionsTest.java
@@ -0,0 +1,41 @@
+package com.csse3200.game.components.ships;
+
+import com.badlogic.gdx.Application;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.math.Vector2;
+import com.csse3200.game.entities.Entity;
+import com.csse3200.game.entities.EntityService;
+import com.csse3200.game.events.EventHandler;
+import com.csse3200.game.services.ServiceLocator;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class ShipActionsTest {
+    @Mock
+    private EventHandler eventHandler;
+
+    @Mock
+    private EntityService entityService;
+
+    @Mock
+    private PhysicsComponent physicsComponent;
+
+    @Mock
+    private Body body;
+
+    @BeforeEach
+    void setUp() {
+        ServiceLocator.registerEntityService(entityService);
+
+        // Set up a ship entity with ShipActions component
+        ship = new Entity();
+        ship.addComponent(physicsComponent);
+        ship.addComponent(new ShipActions(100, 100, 1));
+        ship = ship.getComponent(ShipActions.class);
+    }
+
+}

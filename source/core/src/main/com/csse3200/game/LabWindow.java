f3b8387bbfe7243cf509fcbb559434771dbf09b1
atharvagupta2003 <atharvagupta2003@gmail.com>
2023-10-12 22:44:26 +1000

    Improved code of lab window and displayed number of health and speed powerups

--- source/core/src/main/com/csse3200/game/LabWindow.java
+++ source/core/src/main/com/csse3200/game/LabWindow.java
@@ -11,6 +11,7 @@ import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import com.csse3200.game.components.Companion.CompanionPowerupInventoryComponent;
 import com.csse3200.game.components.PowerupType;
 import com.csse3200.game.entities.Entity;
 import com.csse3200.game.input.InputOverrideComponent;
@@ -21,11 +22,14 @@ import com.csse3200.game.services.ServiceLocator;
  */
 public class LabWindow extends Window {
     private InputOverrideComponent inputOverrideComponent;
-    private int points = 100; // Initialize points with your starting value
-    private Label pointsLabel; // Label to display points
-
-    private TextButton[] potionButton=new TextButton[8];
-
+    private Label healthPowerupsLabel;
+    private Label speedPowerupsLabel;
+    private Image healthPowerupsImage;
+    private Image speedPowerupsImage;
+    private Entity companion = ServiceLocator.getEntityService().getCompanion();
+    private int Health_powerups = companion.getComponent(CompanionPowerupInventoryComponent.class).getPowerupInventoryCount(PowerupType.HEALTH_BOOST);
+    private int Speed_powerups = companion.getComponent(CompanionPowerupInventoryComponent.class).getPowerupInventoryCount(PowerupType.SPEED_BOOST);
+    private TextButton[] potionButton=new TextButton[6];
     private Texture potionImage;
     private String potionName;
 
@@ -41,22 +45,51 @@ public class LabWindow extends Window {
     }
 
     void create(){
-        // Here set up the window to be centered on the stage with 80% width and 65% height.
         Stage stage = ServiceLocator.getRenderService().getStage();
         setWidth((float) (stage.getWidth() * 0.70));
         setHeight((float) (stage.getHeight() * 0.50));
         setPosition(stage.getWidth() / 2 - getWidth() / 2 * getScaleX(), stage.getHeight() / 2 - getHeight() / 2 * getScaleY());
         Skin skin = new Skin(Gdx.files.internal("kenney-rpg-expansion/kenneyrpg.json"));
-        // Create a Table to hold the buttons and center them within the window
 
+        Texture healthPowerupTexture = new Texture("images/powerups/health_boost.png");
+        Texture speedPowerupTexture = new Texture("images/powerups/speed_boost.png"); // Replace with the actual image path
+
+        healthPowerupsImage = new Image(healthPowerupTexture);
+        speedPowerupsImage = new Image(speedPowerupTexture);
+
+        float imageSize = 32f;
+        healthPowerupsImage.setSize(imageSize, imageSize);
+        speedPowerupsImage.setSize(imageSize, imageSize);
+
+        // Position the Image widgets at the top of the window
+        healthPowerupsImage.setPosition(30, getHeight() - 70);
+        speedPowerupsImage.setPosition(healthPowerupsImage.getX() + healthPowerupsImage.getWidth() + 25, getHeight()- 70);
+
+        healthPowerupsLabel = new Label(": " + Health_powerups, skin);
+        speedPowerupsLabel = new Label(": " + Speed_powerups, skin);
+
+        float labelScale = 0.25f; // Adjust this value to the desired scale
+
+        healthPowerupsLabel.setFontScale(labelScale);
+        speedPowerupsLabel.setFontScale(labelScale);
+
+        // Position the labels
+        float labelOffsetX = 5; // Adjust the label offset in the X direction
+        float labelOffsetY = 3; // Adjust the label offset in the Y direction
+
+        healthPowerupsLabel.setPosition(healthPowerupsImage.getX() + healthPowerupsImage.getWidth() + labelOffsetX, getHeight() - 70 - labelOffsetY);
+        speedPowerupsLabel.setPosition(speedPowerupsImage.getX() + speedPowerupsImage.getWidth() + labelOffsetX, getHeight() - 70 - labelOffsetY);
+        // Add the Image widgets to the LabWindow
+        addActor(healthPowerupsImage);
+        addActor(speedPowerupsImage);
+        addActor(healthPowerupsLabel);
+        addActor(speedPowerupsLabel);
         Table buttonTable = new Table();
         buttonTable.setFillParent(true);
         // Fill the entire LabWindow
 
         Texture[] potionImages = {
                 new Texture("images/powerups/death_potion.png"),
-                new Texture("images/powerups/speed_potion.png"),
-                new Texture("images/powerups/health_potion.png"),
                 new Texture("images/powerups/temp_immunity.png"),
                 new Texture("images/powerups/double_damage.png"),
                 new Texture("images/powerups/extra_life.png"),
@@ -65,17 +98,8 @@ public class LabWindow extends Window {
         };
 
         String[] potionNames = {
-                "Death", "Speed", "Health", "Invincibility", "2x Damage", "Extra Life", "Snap", "Double Cross"
+                "Death", "Invincibility", "2x Damage", "Extra Life", "Snap", "Double Cross"
         };
-
-        int[] potionCosts = {
-                50, 60, 30, 40, 70, 80, 90, 100
-        };
-        // Create points label
-        pointsLabel = new Label("Points: " + points, skin);
-        pointsLabel.setFontScale(0.5f); // Adjust font scale as needed
-        pointsLabel.setPosition(40, 550); // Adjust position as needed
-        addActor(pointsLabel);
         // Create a separate table for the "EXIT" button and position it at the bottom-right
         Table exitButtonTable = new Table();
         exitButtonTable.setFillParent(true);
@@ -84,7 +108,7 @@ public class LabWindow extends Window {
         TextButton returnToGameButton = new TextButton("EXIT", skin);
         exitButtonTable.add(returnToGameButton).padBottom(20).padRight(20);
         addActor(exitButtonTable);
-        //addActor(exit);
+
         returnToGameButton.addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
@@ -98,50 +122,34 @@ public class LabWindow extends Window {
 
 
             Image potionImageWidget = new Image(potionImage);
-            potionButton[i] = new TextButton(potionName + " - " + potionCosts[i] + " points", skin);
+            potionButton[i] = new TextButton(potionName, skin);
             buttonTable.add(potionImageWidget).width(100).height(64);;
-            buttonTable.add(potionButton[i]).pad(10);
+            buttonTable.add(potionButton[i]).pad(20);
             int finalI = i;
             potionButton[i].addListener(new ClickListener() {
                 @Override
                 public void clicked(InputEvent event, float x, float y) {
-                    // Deduct points when purchasing a potion and update the pointsLabel
-                    if (points >=potionCosts[finalI]) {
-                        update(potionCosts[finalI]);
-                        pointsLabel.setText("Points: " + points); // Update pointsLabel text
                         PowerupType powerupType = getPowerupTypeForPotionName(potionNames[finalI]);
                         ServiceLocator.getEntityService().getCompanion().getEvents().trigger("SpawnPowerup", powerupType);
-                    }
+                        failLaboratory();
+
                 }
             });
 
-            if(i==3) {
+            if(i==2) {
                 buttonTable.row();
             }
         }
         addActor(buttonTable);
-
-
-
-
-        // Override all normal user input
         inputOverrideComponent = new InputOverrideComponent();
         ServiceLocator.getInputService().register(inputOverrideComponent);
 
-    }
-
-    void update (int cost){
-        this.points-=cost;
     }
 
     private PowerupType getPowerupTypeForPotionName(String potionName) {
         switch (potionName) {
             case "Death":
                 return PowerupType.DEATH_POTION;
-            case "Speed":
-                return PowerupType.SPEED_BOOST;
-            case "Health":
-                return PowerupType.HEALTH_BOOST;
             case "Invincibility":
                 return PowerupType.TEMP_IMMUNITY;
             case "2x Damage":
@@ -170,9 +178,7 @@ public class LabWindow extends Window {
 
     @Override
     public boolean remove() {
-        // Stop overriding input when exiting the Laboratory
         ServiceLocator.getInputService().unregister(inputOverrideComponent);
         return super.remove();
     }
 }
-// updated with points

305c6429d78355aecf8eb66ce7ad33798753c352
Manvi Narang <65333992+ManviNarang01@users.noreply.github.com>
2023-10-15 21:35:08 +1000

    Fine-tuned the Hell Map
    Added new tilesets and sprites

---
+++ source/core/assets/map/burnt_sand.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="burnt_sand" tilewidth="16" tileheight="16" tilecount="16384" columns="128">
+ <image source="../Downloads/burnt_sand.png" width="2048" height="2048"/>
+</tileset>

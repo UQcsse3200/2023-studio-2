4e2824fd147d23f503bd4cef5eae8ce7cffe27f7
henrybatt <hbatt1545@gmail.com>
2023-10-16 12:16:55 +1000

    PlanetScreen logging

--- source/core/src/main/com/csse3200/game/screens/PlanetScreen.java
+++ source/core/src/main/com/csse3200/game/screens/PlanetScreen.java
@@ -86,6 +86,7 @@ public class PlanetScreen extends ScreenAdapter {
      * @param name  The name of the planet to create a screen for.
      */
     public PlanetScreen(GdxGame game, String name) {
+        logger.info(String.format("Creating %s PlanetScreen", name));
         this.game = game;
         this.name = name;
         this.assets = FileLoader.readClass(AssetsConfig.class, "levels/global_assets.json");
@@ -104,6 +105,7 @@ public class PlanetScreen extends ScreenAdapter {
      */
     @Override
     public void show() {
+        logger.info(String.format("Displaying %s PlanetScreen", name));
         registerServices();
 
         populateGameState();
@@ -116,9 +118,8 @@ public class PlanetScreen extends ScreenAdapter {
         } else {
             ServiceLocator.getGameStateObserverService().trigger("updatePlanet", "gameArea", currentAreaName);
         }
-        this.allGameAreas.get(currentAreaName).create();
 
-        logger.debug((String.format("Initialising %s screen entities", this.name)));
+        this.allGameAreas.get(currentAreaName).create();
         this.player = allGameAreas.get(currentAreaName).getPlayer();
         if ("earth".equals(name)) {
             showTitleBox();
@@ -152,6 +153,7 @@ public class PlanetScreen extends ScreenAdapter {
      * @return      Whether the new game area was set correctly.
      */
     public boolean setCurrentArea(String name) {
+        logger.debug("Changing displayed area to " + name);
         if (this.allGameAreas.containsKey(name)) {
             this.allGameAreas.get(currentAreaName).dispose();
             this.currentAreaName = name;
@@ -195,6 +197,7 @@ public class PlanetScreen extends ScreenAdapter {
      * Generates all the appropriate game areas for the current planet based on its name.
      */
     private void generateGameAreas() {
+        logger.debug((String.format("Creating %s PlanetScreen areas", this.name)));
         String levelName = formatName(name);
 
         if (this.levelConfig != null) {
@@ -274,27 +277,21 @@ public class PlanetScreen extends ScreenAdapter {
      */
     @Override
     public void dispose() {
+        logger.debug(String.format("Disposing %s PlanetScreen", this.name));
         saveGame();
-        this.clear();
+        this.allGameAreas.get(currentAreaName).dispose();
+        renderer.dispose();
+        unloadAssets();
+        ServiceLocator.getGameStateObserverService().trigger("remove", "gameArea");
     }
 
     private void saveGame() {
+        logger.debug(String.format("Saved %s PlanetScreen", name));
         String path = String.format("%s/%s/%s/entities.json", SAVE_PATH, this.name, this.currentAreaName);
         ServiceLocator.getEntityService().saveCurrentArea(path);
 
         Map<String, Object> gameStateEntries = new HashMap<>(ServiceLocator.getGameStateObserverService().getFullStateData());
         FileLoader.writeClass(gameStateEntries, joinPath(List.of(SAVE_PATH, GAMESTATE_FILE)), FileLoader.Location.LOCAL);
-    }
-
-    /**
-     * Dispose of the entire game screen.
-     */
-    public void clear() {
-        logger.debug(String.format("Disposing %s screen", this.name));
-        this.allGameAreas.get(currentAreaName).dispose();
-        renderer.dispose();
-        unloadAssets();
-        ServiceLocator.getGameStateObserverService().trigger("remove", "gameArea");
     }
 
     /**

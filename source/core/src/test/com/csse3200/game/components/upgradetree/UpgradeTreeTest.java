50454ef18073f938753b05bf1bcdfba627b7fc0c
connor-golin <connor.golin@gmail.com>
2023-10-17 01:22:22 +1000

    Adds save/load to upgrades

--- source/core/src/test/com/csse3200/game/components/upgradetree/UpgradeTreeTest.java
+++ source/core/src/test/com/csse3200/game/components/upgradetree/UpgradeTreeTest.java
@@ -2,12 +2,10 @@ package com.csse3200.game.components.upgradetree;
 
 import com.badlogic.gdx.Application;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.audio.Sound;
-import com.csse3200.game.components.Weapons.WeaponType;
 import com.csse3200.game.components.resources.Resource;
 import com.csse3200.game.entities.Entity;
+import com.csse3200.game.entities.configs.PlayerConfig;
 import com.csse3200.game.entities.configs.WeaponConfig;
-import com.csse3200.game.entities.configs.WeaponConfigs;
 import com.csse3200.game.events.EventHandler;
 import com.csse3200.game.extensions.GameExtension;
 import com.csse3200.game.services.GameStateObserver;
@@ -15,13 +13,9 @@ import com.csse3200.game.services.ServiceLocator;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.ArrayList;
-
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.notNull;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -36,7 +30,7 @@ public class UpgradeTreeTest {
 
     @BeforeEach
     public void setUp() {
-        tree = new UpgradeTree();
+        tree = new UpgradeTree(mock(PlayerConfig.class));
         gameStateObserver = mock(GameStateObserver.class);
         mock(ServiceLocator.class);
         ServiceLocator.registerGameStateObserverService(gameStateObserver);

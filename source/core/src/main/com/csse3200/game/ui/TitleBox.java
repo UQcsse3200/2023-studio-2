f0abd614efe850b94b44718be8411ef77b15579d
YashMitttal <140570495+YashMitttal@users.noreply.github.com>
2023-10-16 10:21:56 +1000

    Added text by text animation and typing label flags to the title box class with generalisation

--- source/core/src/main/com/csse3200/game/ui/TitleBox.java
+++ source/core/src/main/com/csse3200/game/ui/TitleBox.java
@@ -9,11 +9,13 @@ import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.utils.Align;
 import com.csse3200.game.GdxGame;
 import com.csse3200.game.entities.Entity;
+import com.rafaskoberg.gdx.typinglabel.TypingLabel;
+
+import static java.awt.SystemColor.text;
 
 public class TitleBox extends Dialog {
 
     private GdxGame game;
-    private Label descriptionLabel;
 
     public TitleBox(GdxGame game, String title, String description, Skin skin) {
         super(title, skin);
@@ -21,21 +23,20 @@ public class TitleBox extends Dialog {
         setMovable(false);
         setResizable(true);
 
+
         Label titleLabel = getTitleLabel();
         titleLabel.setText(title);
         titleLabel.setAlignment(Align.center);
-        titleLabel.setFontScale(0.2f); // Adjust font scale as needed
-        titleLabel.setColor(Color.BLACK); // TitleBox Title Color can be changed here
+        titleLabel.setFontScale(0.2f);
+        titleLabel.setColor(Color.BLACK);
 
-        Label.LabelStyle labelStyle = new Label.LabelStyle(skin.get("small", Label.LabelStyle.class));
-        labelStyle.font.getData().setScale(0.3f); // Set the font scale to make it larger
+        TypingLabel descriptionLabel = new TypingLabel("", skin);
 
-        descriptionLabel = new Label(description, labelStyle);
+        // Initialize TypingLabel with your description
+        descriptionLabel = new TypingLabel(description, skin);
         descriptionLabel.setAlignment(Align.center);
-        descriptionLabel.setWrap(true); // Enable text wrapping
-        descriptionLabel.setColor(Color.BLACK); // Enable text wrapping
-
-
+        descriptionLabel.setWrap(true);
+        descriptionLabel.setColor(Color.BLACK);
 
         TextButton okButton = new TextButton("OK", skin);
         button(okButton, true);
@@ -54,18 +55,14 @@ public class TitleBox extends Dialog {
         Table buttonTable = new Table();
         buttonTable.add(okButton).pad(20f).expandX().center().row();
 
-
-
-//         Use a ScrollPane for description text if it exceeds the dialog's height
         ScrollPane scrollPane = new ScrollPane(descriptionLabel, skin);
-        scrollPane.setFadeScrollBars(false); // Disable fading
-        scrollPane.setScrollingDisabled(true, false); // Disable horizontal scrolling
+        scrollPane.setFadeScrollBars(false);
+        scrollPane.setScrollingDisabled(true, false);
         getContentTable().add(scrollPane).width(Gdx.graphics.getWidth() * 0.8f).height(100f).pad(20f).center().row();
 
         getContentTable().add(buttonTable).expandX().center().center();
-        descriptionLabel.setPosition(0, 300f);
 
-        setSize(Gdx.graphics.getWidth(), 550f); // Adjust the height as needed
+        setSize(Gdx.graphics.getWidth(), 550f);
     }
 
     public void showDialog(Stage stage) {
@@ -73,6 +70,6 @@ public class TitleBox extends Dialog {
     }
 
     private void onOK() {
-       hide();
+        hide();
     }
 }

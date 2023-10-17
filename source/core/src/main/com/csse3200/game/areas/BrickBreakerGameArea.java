1711fedccdcc7d3aa75d0e95e310c65347865810
Tarushi Gera <t.gera@uqconnect.edu.au>
2023-10-17 12:08:41 +1000

    Removed unused imports

--- source/core/src/main/com/csse3200/game/areas/BrickBreakerGameArea.java
+++ source/core/src/main/com/csse3200/game/areas/BrickBreakerGameArea.java
@@ -1,7 +1,6 @@
 package com.csse3200.game.areas;
 
 import com.badlogic.gdx.math.GridPoint2;
-import com.badlogic.gdx.math.Vector2;
 import com.csse3200.game.areas.terrain.TerrainFactory;
 import com.csse3200.game.components.gamearea.GameAreaDisplay;
 import com.csse3200.game.entities.Entity;

d96bb10549990637691e7a2d107d795a426dc8df
Rowan Gray <github@rowangray.au>
2023-10-17 11:15:25 +1000

    Added testing to ToolTest for the new range functionality

--- source/core/src/test/com/csse3200/game/components/structures/tools/ToolTest.java
+++ source/core/src/test/com/csse3200/game/components/structures/tools/ToolTest.java
@@ -1,8 +1,10 @@
 package com.csse3200.game.components.structures.tools;
 
 import com.badlogic.gdx.math.GridPoint2;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.ObjectMap;
 import com.csse3200.game.entities.Entity;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -11,6 +13,8 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class ToolTest {
 
@@ -47,6 +51,48 @@ class ToolTest {
             prevOrder = tool.getOrdering();
         }
     }
+
+    @Test
+    void TestCanPlaceOutOfRange() {
+        ObjectMap<String, Integer> cost = new ObjectMap<>();
+        var tool = new MockTool(cost, 5f, "image.png", 0);
+
+        var player = mock(Entity.class);
+        when(player.getCenterPosition()).thenReturn(new Vector2(0,0));
+
+        // out of range by one tile
+        Assertions.assertFalse(tool.canInteract(player, new GridPoint2(6, 0)).isValid());
+        // out of range on the diagonal
+        Assertions.assertFalse(tool.canInteract(player, new GridPoint2(4, 4)).isValid());
+
+        var player2 = mock(Entity.class);
+        when(player2.getCenterPosition()).thenReturn(new Vector2(10,10));
+
+        // test being converted to world tile position before distance is calculated.
+        Assertions.assertFalse(tool.canInteract(player, new GridPoint2(10, 10)).isValid());
+    }
+
+    @Test
+    void TestCanPlaceInRange() {
+        ObjectMap<String, Integer> cost = new ObjectMap<>();
+        var tool = new MockTool(cost, 5f, "image.png", 0);
+
+        var player = mock(Entity.class);
+        when(player.getCenterPosition()).thenReturn(new Vector2(0,0));
+
+        // test right in the middle
+        Assertions.assertTrue(tool.canInteract(player, new GridPoint2(0, 0)).isValid());
+        // test right on the edge
+        Assertions.assertTrue(tool.canInteract(player, new GridPoint2(0, 5)).isValid());
+        // test right on the edge diagonally
+        Assertions.assertTrue(tool.canInteract(player, new GridPoint2(3, 4)).isValid());
+
+        var player2 = mock(Entity.class);
+        when(player2.getCenterPosition()).thenReturn(new Vector2(10,10));
+
+        // test being converted to world tile position before distance is calculated.
+        Assertions.assertTrue(tool.canInteract(player2, new GridPoint2(20, 20)).isValid());
+    }
 }
 
 class MockTool extends Tool {
@@ -57,10 +103,5 @@ class MockTool extends Tool {
     @Override
     protected void performInteraction(Entity player, GridPoint2 position) {
 
-    }
-
-    @Override
-    protected ToolResponse canInteract(Entity player, GridPoint2 position) {
-        return null;
     }
 }

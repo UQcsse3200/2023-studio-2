1db3a33a80737d1bd25fd8c59a31aed659a0ac6a
JivalX <kjival@gmail.com>
2023-10-17 20:02:31 +1000

    Created ControlsScreenDisplayTest and fixed ControlsScreenTest to working condition

--- source/core/src/test/com/csse3200/game/components/spacenavigation/NavigationBackgroundTest.java
+++ source/core/src/test/com/csse3200/game/components/spacenavigation/NavigationBackgroundTest.java
@@ -1,7 +1,6 @@
 package com.csse3200.game.components.spacenavigation;
 
 import com.badlogic.gdx.Application;
-import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Vector2;

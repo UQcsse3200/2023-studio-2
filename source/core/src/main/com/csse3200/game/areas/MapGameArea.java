9c8ee9c453b707dc0895de7a56a9f11303e24bdc
Mitchell Hall <s4697858@student.uq.edu.au>
2023-10-18 23:48:47 +1000

    Fixed formatting in .json files by adding quotations ("") around tags. Replaced safe zone sprite on Cryoheim with an animated campfire sprite. Removed commented out code from MapGameArea.java

--- source/core/src/main/com/csse3200/game/areas/MapGameArea.java
+++ source/core/src/main/com/csse3200/game/areas/MapGameArea.java
@@ -99,9 +99,6 @@ public class MapGameArea extends GameArea{
 
         spawnAstronaut();
 
-        //spawnEnvironmentDamage();
-//        spawnFreezingArea();
-//        spawnEnvironmentDamage();
         spawnSafeZone(player);
 
         displayUI();
@@ -170,20 +167,6 @@ public class MapGameArea extends GameArea{
         spawnEntityAt(safeZone, safeZoneConfig.position, false, false);
     }
 
-//    private void spawnEnvironmentDamage() {
-//        if (mapConfig.areaEntityConfig == null) return;
-//
-//        Entity envDamage = EnvironmentalDamageFactory.createDamage();
-//        spawnEntityAt(envDamage, new GridPoint2(45, 45), false, false);
-//    }
-//
-//    private void spawnFreezingArea() {
-//        if (mapConfig.areaEntityConfig == null) return;
-//
-//        Entity freezeArea = FreezingAreaFactory.createFreezingArea();
-//        spawnEntityAt(freezeArea, new GridPoint2(40, 60), false, false);
-//    }
-
     public static float getSpeedMult() {
         TiledMapTileLayer collisionLayer = (TiledMapTileLayer) terrain.getMap().getLayers().get("Base");
         Vector2 playerPos = getPlayer().getPosition();
@@ -201,14 +184,6 @@ public class MapGameArea extends GameArea{
 
         return onIce != null && (boolean) onIce;
     }
-
-//    public static boolean isFreezing() {
-//        return freezing;
-//    }
-//
-//    public static void toggleFreezing(Entity player) {
-//        freezing = !freezing;
-//    }
 
     /**
      * Checks if the player should be taking damage from standing on fire or lava

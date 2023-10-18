c8b26b08b8e8e0fd0281ed044825d10cfb954fe7
JivalX <kjival@gmail.com>
2023-10-18 18:23:56 +1000

    improvements to test classes

--- source/core/src/test/com/csse3200/game/components/initialsequence/InitialScreenDisplayTest.java
+++ source/core/src/test/com/csse3200/game/components/initialsequence/InitialScreenDisplayTest.java
@@ -18,6 +18,9 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * Unit tests for the {@link InitialScreenDisplay} class.
+ */
 public class InitialScreenDisplayTest {
 
     private Set<String> loadedAssets;
@@ -55,22 +58,34 @@ public class InitialScreenDisplayTest {
         });
     }
 
+    /**
+     * Creates an instance of the {@link InitialScreenDisplay} for testing.
+     *
+     * @return An instance of the {@link InitialScreenDisplay}.
+     */
     private InitialScreenDisplay createInitialScreenDisplay() {
-        // Replace the following list parameters with the actual asset paths and text list
         ArrayList<String> assetPaths = new ArrayList<>();
         ArrayList<String> textList = new ArrayList<>();
         return new InitialScreenDisplay(game, assetPaths, textList);
     }
 
+    /**
+     * Verify that a specific asset has been loaded.
+     *
+     * @param assetPath The path to the asset to be verified.
+     */
     private void verifyAssetLoaded(String assetPath) {
-        // Replace with the actual logic to verify asset loading
         ResourceService resourceService = ServiceLocator.getResourceService();
         boolean isLoaded = resourceService.containsAsset(assetPath, Texture.class);
         assertTrue(isLoaded, "Asset not loaded: " + assetPath);
     }
 
+    /**
+     * Verify that a specific asset has been unloaded.
+     *
+     * @param assetPath The path to the asset to be verified.
+     */
     private void verifyAssetUnloaded(String assetPath) {
-        // Replace with the actual logic to verify asset unloading
         ResourceService resourceService = ServiceLocator.getResourceService();
         boolean isUnloaded = !resourceService.containsAsset(assetPath, Texture.class);
         assertTrue(isUnloaded, "Asset not unloaded: " + assetPath);
--- source/core/src/test/com/csse3200/game/components/maingame/ReturnToPlanetDisplayTest.java
+++ source/core/src/test/com/csse3200/game/components/maingame/ReturnToPlanetDisplayTest.java
@@ -3,12 +3,14 @@ package com.csse3200.game.components.maingame;
 import com.badlogic.gdx.Application;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.headless.HeadlessApplication;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.csse3200.game.GdxGame;
 import com.csse3200.game.services.ServiceLocator;
 import com.csse3200.game.services.ResourceService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
 import java.util.HashSet;
 import java.util.Set;
 
@@ -16,6 +18,9 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * Unit tests for the {@link ReturnToPlanetDisplay} class.
+ */
 public class ReturnToPlanetDisplayTest {
 
     private Set<String> loadedAssets;
@@ -36,10 +41,10 @@ public class ReturnToPlanetDisplayTest {
     }
 
     /**
-     * Test asset loading in {@link ReturnToPlanetDisplay}.
+     * Test the assets related to the skin used for buttons in {@link ReturnToPlanetDisplay}.
      */
     @Test
-    void testReturnToPlanetDisplayAssetLoading() {
+    void testReturnToPlanetDisplaySkinAssets() {
         Gdx.app.postRunnable(() -> {
             ReturnToPlanetDisplay returnToPlanetDisplay = createReturnToPlanetDisplay();
 
@@ -47,27 +52,29 @@ public class ReturnToPlanetDisplayTest {
             returnToPlanetDisplay.create();
             returnToPlanetDisplay.dispose();
 
-            // Verify that specific assets have been loaded and unloaded
-            verifyAssetLoaded("your_asset_path_here");
-            verifyAssetUnloaded("your_asset_path_here");
+            // Verify that specific assets related to the skin have been loaded and unloaded
+            verifyAssetLoaded("your_skin_atlas_path_here"); // Modify with the actual atlas path used in the skin
+            verifyAssetUnloaded("your_skin_atlas_path_here"); // Modify with the actual atlas path used in the skin
         });
     }
 
     private ReturnToPlanetDisplay createReturnToPlanetDisplay() {
+        // To access the skin, you might need to modify the ReturnToPlanetDisplay class
+        // to provide a method to retrieve the skin or use reflection to access it.
         return new ReturnToPlanetDisplay();
     }
 
     private void verifyAssetLoaded(String assetPath) {
-        // Replace with the actual logic to verify asset loading
+        // Verify asset loading
         ResourceService resourceService = ServiceLocator.getResourceService();
-        boolean isLoaded = resourceService.containsAsset(assetPath, Texture.class);
-        assertTrue(isLoaded, "Asset not loaded: " + assetPath);
+        boolean isLoaded = resourceService.containsAsset(assetPath, Skin.class);
+        assertTrue(isLoaded, "Skin asset not loaded: " + assetPath);
     }
 
     private void verifyAssetUnloaded(String assetPath) {
-        // Replace with the actual logic to verify asset unloading
+        // Verify asset unloading
         ResourceService resourceService = ServiceLocator.getResourceService();
-        boolean isUnloaded = !resourceService.containsAsset(assetPath, Texture.class);
-        assertTrue(isUnloaded, "Asset not unloaded: " + assetPath);
+        boolean isUnloaded = !resourceService.containsAsset(assetPath, Skin.class);
+        assertTrue(isUnloaded, "Skin asset not unloaded: " + assetPath);
     }
 }

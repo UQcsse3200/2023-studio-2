5272fe1db0a57a719489c95d5acbd573f1c79689
Aniket Gupta <aniketgupta1707@gmail.com>
2023-10-18 12:39:00 +1000

    Added a QuestBox class

--- source/core/src/main/com/csse3200/game/areas/MapGameArea.java
+++ source/core/src/main/com/csse3200/game/areas/MapGameArea.java
@@ -28,11 +28,16 @@ import com.csse3200.game.files.UserSettings;
 import com.csse3200.game.services.ResourceService;
 import com.csse3200.game.services.ServiceLocator;
 import com.csse3200.game.services.TerrainService;
+import com.csse3200.game.ui.QuestBox;
 import com.csse3200.game.utils.math.GridPoint2Utils;
+import com.badlogic.gdx.scenes.scene2d.Stage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.List;
+
+import static com.csse3200.game.ui.DialogComponent.stage;
+import static com.csse3200.game.ui.UIComponent.skin;
 
 /**
  * A Base Game Area for any level.
@@ -141,6 +146,7 @@ public class MapGameArea extends GameArea{
         //ui.addComponent(new GameAreaDisplay(map_config.mapName));
         ui.addComponent(new PlanetHudDisplay(mapConfig.mapName, mapConfig.planetImage))
                 .addComponent(new InventoryDisplayComponent());
+        QuestBox questBox=new QuestBox("Rescue Astro",skin,stage);
         spawnEntity(ui);
     }
 

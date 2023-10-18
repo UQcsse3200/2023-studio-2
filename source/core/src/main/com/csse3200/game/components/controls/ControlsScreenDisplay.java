011a03cf68c535462fb03b8836d34df2e4f08f89
Tarushi Gera <t.gera@uqconnect.edu.au>
2023-10-18 14:46:38 +1000

    Code Smells

--- source/core/src/main/com/csse3200/game/components/controls/ControlsScreenDisplay.java
+++ source/core/src/main/com/csse3200/game/components/controls/ControlsScreenDisplay.java
@@ -1,7 +1,5 @@
 package com.csse3200.game.components.controls;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.ui.*;

34093d7892033fb3507accd632b8a4efc965e23b
DRG31 <dev.gupta@uqconnect.edu.au>
2023-10-18 12:59:37 +1000

    Fixed dialogue box

--- source/core/src/main/com/csse3200/game/ui/DialogueBox.java
+++ source/core/src/main/com/csse3200/game/ui/DialogueBox.java
@@ -2,27 +2,22 @@ package com.csse3200.game.ui;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.Pixmap;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.NinePatch;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
-import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
-import com.badlogic.gdx.scenes.scene2d.utils.NinePatchDrawable;
-import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.Align;
 import com.csse3200.game.entities.Entity;
 import static com.csse3200.game.screens.MainMenuScreen.logger;
+import com.rafaskoberg.gdx.typinglabel.TypingLabel;
 
 /**
  * A custom dialog box for displaying in-game dialogues with an OK button.
  */
 public class DialogueBox extends Dialog {
 
-    private Label dialogueLabel; // Change TypingLabel to Label
-    private Label titleLabel; // Remove TypingLabel for title
+    private TypingLabel dialogueLabel;
+    private Label titleLabel;
     private String[] titles;
     private String[] messages;
     TextButton info;
@@ -38,72 +33,70 @@ public class DialogueBox extends Dialog {
     public DialogueBox(String[] title, String[] message, Skin skin) {
         super("", skin);
         this.skin = skin;
-        titleLabel = new Label(title[0], skin); // Create a Label for the title
-        this.dialogueLabel = new Label(message[0], skin); // Create a Label for the dialogue
-        this.titles = title;
-        this.messages = message;
+        titleLabel = new TypingLabel(title[0], skin);
+        this.dialogueLabel = new TypingLabel(message[0], skin);
+        this.titles=title;
+        this.messages=message;
         create();
     }
 
-    public void create() {
-        // Create a Pixmap for the dialog background
-        Pixmap bgPixmap = new Pixmap(900, 600, Pixmap.Format.RGBA8888);
-        bgPixmap.setColor(0.5f, 0.5f, 0.5f, 0.8f); // Gray color with alpha
-        bgPixmap.fill();
 
-        Texture bgTexture = new Texture(bgPixmap);
-
-
+    public void create(){
         titleLabel.setAlignment(Align.top);
-        titleLabel.setFontScale(0.3f);
-        titleLabel.setColor(Color.BLACK);
+        titleLabel.setFontScale(0.3f); // Adjust font scale as needed
+        titleLabel.setColor(Color.BLACK); // TitleBox Title Colour can be changed here
 
-        getContentTable().add(titleLabel).padTop(20f).row();
 
-        // Create a Label for the dialog text
-        Label dialogueLabel = new Label("", skin);
-        dialogueLabel.setColor(1, 1, 1, 0); // Set the background color of the Label to transparent
-        getContentTable().add(dialogueLabel).width(500f).height(-15f).pad(20f).center();
+        this.getContentTable().add(titleLabel).padTop(20f).row();
+        this.getContentTable().add(dialogueLabel).width(500f).height(-15f).pad(20f).center(); // Adjust width and height as needed
+
+
+        TypingLabel.LabelStyle labelStyle = new Label.LabelStyle(skin.get("small", Label.LabelStyle.class));
+        labelStyle.font.getData().setScale(0.8f); // Set the font scale to make it larger
+
 
         TextButton startButton = new TextButton("OK", skin);
         button(startButton, true);
         Entity entity = new Entity();
         entity.getEvents().addListener("ok", this::onOK);
-        startButton.addListener(new ChangeListener() {
-            @Override
-            public void changed(ChangeEvent changeEvent, Actor actor) {
-                logger.debug("OK button clicked");
-                entity.getEvents().trigger("ok");
-            }
-        });
+        startButton.addListener(
+                new ChangeListener() {
+                    @Override
+                    public void changed(ChangeEvent changeEvent, Actor actor) {
+                        logger.debug("OK button clicked");
+                        entity.getEvents().trigger("ok");
+                    }
+                });
 
         Table customButtonTable = new Table();
-        customButtonTable.add(startButton).pad(20f);
-
+        customButtonTable.add(startButton).pad(20f); // Add padding as needed
         info = new TextButton("Next", skin);
         button(info, true);
         entity.getEvents().addListener("info", this::oninfo);
-        info.addListener(new ChangeListener() {
-            @Override
-            public void changed(ChangeEvent changeEvent, Actor actor) {
-                logger.debug("Next button clicked");
-                entity.getEvents().trigger("info");
-            }
-        });
+        info.addListener(
+                new ChangeListener() {
+                    @Override
+                    public void changed(ChangeEvent changeEvent, Actor actor) {
+                        logger.debug("Next button clicked");
+                        entity.getEvents().trigger("info");
+
+
+                    }
+                });
+
+        customButtonTable.add(info).pad(25f); // Add padding as needed
+
 
         // Add the custom button table to the dialog
-        getContentTable().row();
-        getContentTable().add(customButtonTable).center();
+        this.getContentTable().row();
+        this.getContentTable().add(customButtonTable).center();
 
-        // Set the background using the Texture
-        getContentTable().background(new Image(bgTexture).getDrawable());
 
-        setSize(900f, 600f);
-        setPosition((Gdx.graphics.getWidth() - getWidth()) / 2, Gdx.graphics.getHeight() - getHeight() - 500);
+        // Size and positioning of the dialog
+        setSize(900f, 600f); // Adjust the size as needed
+        setPosition((Gdx.graphics.getWidth() - getWidth()) / 2, Gdx.graphics.getHeight() - getHeight() - 500); // Adjust the Y position as needed
+
     }
-
-
-
     /**
      * Sets the text to be displayed in the dialogue box.
      *
@@ -123,7 +116,7 @@ public class DialogueBox extends Dialog {
     }
 
     public void onOK() {
-        logger.info("Back to the game");
+        logger.info("Back to game");
         hide();
     }
 
@@ -137,7 +130,12 @@ public class DialogueBox extends Dialog {
         // Check if there are more dialogues to show
         if (nextIndex < nextTitles.length) {
             titleLabel.setText(nextTitles[nextIndex]);
+
+            // Set the text of the TypingLabel
             dialogueLabel.setText(nextMessages[nextIndex]);
+
+            // Start the TypingLabel animation
+            dialogueLabel.restart();
 
             getContentTable().clear();
             create();
@@ -146,4 +144,5 @@ public class DialogueBox extends Dialog {
             onOK();
         }
     }
+
 }

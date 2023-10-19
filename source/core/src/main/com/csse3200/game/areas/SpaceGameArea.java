31f65455f9025906738060e55f5c253aab8b4006
Foref <s4679430@student.uq.edu.au>
2023-10-19 15:57:53 +1000

    Fixed code smell isPositionOccupied SpaceGameArea

--- source/core/src/main/com/csse3200/game/areas/SpaceGameArea.java
+++ source/core/src/main/com/csse3200/game/areas/SpaceGameArea.java
@@ -36,6 +36,7 @@ public class SpaceGameArea extends GameArea {
     private ArrayList<Entity> targetTables;
     private Random random;
     int randomX, randomY;
+    private Vector2 goalPosition;
 
     /**
      * Constructor for initializing terrain area
@@ -147,16 +148,14 @@ public class SpaceGameArea extends GameArea {
      */
     public Entity spawnGoal() {
         random = new Random();
-        //int randomX, randomY;
-        GridPoint2 position;
         // Continue generating random positions until an unoccupied position is found.
         do {
             randomX = random.nextInt(terrain.getMapBounds(0).x);
             randomY = random.nextInt(terrain.getMapBounds(0).y);
-            position = new GridPoint2(randomX, randomY);
-        } while (isPositionOccupied(position));
+            goalPosition = new Vector2((float) randomX, (float) randomY);
+        } while (isPositionOccupied(goalPosition));
         Entity newGoal = MinigameObjectFactory.createObstacleGameGoal(WORMHOLE_SIZE, WORMHOLE_SIZE);
-        spawnEntityAt(newGoal, position, false, false);
+        spawnEntityAtVector(newGoal, goalPosition);
         goal = newGoal;
         return goal;
     }
@@ -164,9 +163,10 @@ public class SpaceGameArea extends GameArea {
     /**
      * Check if a position is occupied by an entity.
      */
-    public boolean isPositionOccupied(GridPoint2 position) {
+    public boolean isPositionOccupied(Vector2 position) {
         // Loop through the list of existing entities and check if any entity occupies the given position.
         for (Entity entity : targetTables) {
+            //GridPoint2 and Vector2 is unrelated
             if (entity != null && entity.getPosition().equals(position)) {
                 return true; // Position is occupied.
             }

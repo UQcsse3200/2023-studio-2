2a6a3302b847178ee955e196f53d30fa3033603e
Foref <s4679430@student.uq.edu.au>
2023-10-19 16:01:55 +1000

    Changed SpaceGameAreaTest

--- source/core/src/test/com/csse3200/game/areas/SpaceGameAreaTest.java
+++ source/core/src/test/com/csse3200/game/areas/SpaceGameAreaTest.java
@@ -49,18 +49,15 @@ public class SpaceGameAreaTest {
 
         SpaceGameArea spaceGameArea = mock(SpaceGameArea.class);
 
-
-
-
         // Define an unoccupied position
-        GridPoint2 unoccupiedPosition = new GridPoint2(1, 1);
+        GridPoint2 unoccupiedPosition = new Vector2(1f, 1f);
 
 
         when(spaceGameArea.isPositionOccupied(unoccupiedPosition)).thenReturn(false);
         assertFalse(spaceGameArea.isPositionOccupied(unoccupiedPosition));
 
         // Define an occupied position
-        GridPoint2 occupiedPosition = new GridPoint2(2, 2);
+        GridPoint2 occupiedPosition = new Vector2(2f, 2f);
 
         when(spaceGameArea.isPositionOccupied(occupiedPosition)).thenReturn(true);
         assertTrue(spaceGameArea.isPositionOccupied(occupiedPosition));
